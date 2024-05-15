45215598cac175aa83cb36b23c6abbdf539d6324
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-05-16 06:58:46 +0700

    Add FixedPathMovement

---
+++ Scripts/Movement/FixedPathMovement.gd
@@ -0,0 +1,54 @@
+## Applies a set of relative movements to a [Node2D]
+
+extends Node2D
+
+
+#region Parameters
+
+@export_range(0, 1000, 5) var speed: float = 50.0
+
+@export var vectors: Array[Vector2]
+
+@export_range(0, 10, 0.1, "seconds") var delayBetweenMoves: float = 0.0
+
+@export var shouldLoop: bool = true
+
+@export var isEnabled: bool = true
+#endregion
+
+
+#region State
+var currentMoveIndex: int = 0
+var currentDestination: Vector2
+#endregion
+
+
+func _ready():
+	setCurrentDestination()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float):
+	if not isEnabled: return
+	self.position = self.position.move_toward(currentDestination, speed * delta)
+
+	if self.position.is_equal_approx(currentDestination):
+		setNextMove()
+
+
+func setNextMove():
+	currentMoveIndex += 1
+
+	if currentMoveIndex >= vectors.size():
+		if shouldLoop:
+			currentMoveIndex = 0
+		else:
+			self.isEnabled = false
+
+	setCurrentDestination()
+
+
+func setCurrentDestination() -> Vector2:
+	var currentVector: Vector2 = vectors[currentMoveIndex] # TODO: Checks
+	self.currentDestination = self.position + currentVector
+	return currentDestination
