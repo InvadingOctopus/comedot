01b35db1acf05350d07059f6ab9b8b68b82b4531
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-05-29 03:34:38 +0700

    Change PlatformerControlParameters ranges

--- Components/Parameters/PlatformerControlParameters.gd
+++ Components/Parameters/PlatformerControlParameters.gd
@@ -8,36 +8,36 @@ extends Resource
 
 @export_subgroup("Movement on Floor")
 
-@export_range(50,  1000, 5) var speedOnFloor:			float = 100
+@export_range(0, 1000, 5) var speedOnFloor:			float = 100
 
 ## If `false`, then [member speed] is applied directly.
-@export var shouldApplyAccelerationOnFloor:				bool  = true
-@export_range(50,  1000, 5) var accelerationOnFloor:	float = 800
+@export var shouldApplyAccelerationOnFloor:			bool  = true
+@export_range(0, 1000, 5) var accelerationOnFloor:	float = 800
 
-@export var shouldApplyFrictionOnFloor:					bool  = true
-@export_range(100, 2000, 5) var frictionOnFloor:		float = 1000
+@export var shouldApplyFrictionOnFloor:				bool  = true
+@export_range(5, 5000, 5) var frictionOnFloor:		float = 1000
 
 
 @export_subgroup("Movement in Air")
 
-@export var shouldAllowMovementInputInAir:				bool  = true
+@export var shouldAllowMovementInputInAir:			bool  = true
 
-@export_range(0,   1000, 5) var speedInAir:				float = 100.0
+@export_range(0, 1000, 5) var speedInAir:			float = 100.0
 
 ## If `false`, then [member speed] is applied directly.
-@export var shouldApplyAccelerationInAir:				bool  = true
-@export_range(50,  1000, 5) var accelerationInAir:		float = 400.0
+@export var shouldApplyAccelerationInAir:			bool  = true
+@export_range(0, 1000, 5) var accelerationInAir:	float = 400.0
 
-@export var shouldApplyFrictionInAir:					bool  = true ## Applies horizontal friction when not on a floor (not gravity).
-@export_range(0,   2000, 5) var frictionInAir:			float = 200.0 ## Applies horizontal friction when not on a floor (not gravity).
+@export var shouldApplyFrictionInAir:				bool  = true ## Applies horizontal friction when not on a floor (not gravity).
+@export_range(0, 5000, 5) var frictionInAir:		float = 200.0 ## Applies horizontal friction when not on a floor (not gravity).
 
 
 @export_subgroup("Jump")
 
 @export_range(0, 5, 1) var maxNumberOfJumps: int = 2
 
-@export_range(-1000, -100, 5) var jumpVelocity1stJump:		float = -350.0 ## NOTE: This should be a NEGATIVE value because a positive Y axis value means downwards.
-@export_range(-1000, -100, 5) var jumpVelocity1stJumpShort:	float = -175.0 ## NOTE: This should be a NEGATIVE value because a positive Y axis value means downwards.
-@export_range(-1000, -100, 5) var jumpVelocity2ndJump:		float = -300.0 ## NOTE: This should be a NEGATIVE value because a positive Y axis value means downwards.
+@export_range(-1000, -10, 5) var jumpVelocity1stJump:		float = -350.0 ## NOTE: This should be a NEGATIVE value because a positive Y axis value means downwards.
+@export_range(-1000, -10, 5) var jumpVelocity1stJumpShort:	float = -175.0 ## NOTE: This should be a NEGATIVE value because a positive Y axis value means downwards.
+@export_range(-1000, -10, 5) var jumpVelocity2ndJump:		float = -300.0 ## NOTE: This should be a NEGATIVE value because a positive Y axis value means downwards.
 
 #endregion
