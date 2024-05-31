7a5795017ac781a7c9edd3f4a5f810a11ec08361
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-05-31 11:04:57 +0700

    Add basic plugin prototype

---
+++ Addons/Godoctopus/Godoctopus.gd
@@ -0,0 +1,35 @@
+## Godoctopus Plugin Prototype
+
+@tool
+extends EditorPlugin
+
+
+#region Constants
+const entityTypeName	:= &"Godoctopus Entity"
+const componentTypeName	:= &"Godoctopus Component"
+#endregion
+
+
+func _enter_tree() -> void:
+	addCustomTypes()
+
+
+func _exit_tree() -> void:
+	removeCustomTypes()
+
+
+func addCustomTypes():
+	# Entity
+	const entityScript	:= preload("res://Entities/Entity.gd")
+	const entityIcon	:= preload("res://Assets/Icons/entity.svg")
+	add_custom_type(entityTypeName, "Node2D", entityScript, entityIcon)
+
+	# Component
+	const componentScript	:= preload("res://Components/Component.gd")
+	const componentIcon		:= preload("res://Assets/Icons/component.svg")
+	add_custom_type(componentTypeName, "Node", componentScript, componentIcon)
+
+
+func removeCustomTypes():
+	remove_custom_type(entityTypeName)
+	remove_custom_type(componentTypeName)
