d0a86044b4856d4310a7cfc4dacb84464c3fe7c1
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-06-02 09:36:40 +0700

    Add MouseTrackingComponent

---
+++ Components/Movement/MouseTrackingComponent.gd
@@ -0,0 +1,19 @@
+## Moves the entity to the mouse position, immediately or gradually.
+
+class_name MouseTrackingComponent
+extends Component
+
+
+#region Parameters
+@export var shouldRepositionImmediately: bool = true
+@export_range(0, 1000, 5) var speed: float = 100
+@export var isEnabled := true
+#endregion
+
+
+func _physics_process(delta: float):
+	if not isEnabled: return
+	if shouldRepositionImmediately:
+		parentEntity.global_position = parentEntity.get_global_mouse_position()
+	else:
+		parentEntity.global_position = parentEntity.global_position.move_toward(parentEntity.get_global_mouse_position(), speed * delta)
