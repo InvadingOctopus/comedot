1aa9652f44222d465335b0de5edca65f1a72096d
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-06-07 16:08:35 +0700

    Add PlayerInputComponent

---
+++ Components/Control/PlayerInputComponent.gd
@@ -0,0 +1,52 @@
+## Monitors and stores the player's input axes for other "Control" components to act upon.
+## NOTE: To improve performance, small independent control components may do their own input polling. Therefore, this [PlayerInputComponent] makes most sense when a chain of multiple components depend upon it, such as [TurningControlComponent] + [ThrustControlComponent].
+## Requirements: Should be BEFORE all "Control" components
+
+class_name PlayerInputComponent
+extends Component
+
+
+#region Parameters
+@export var isEnabled: bool = true
+#endregion
+
+
+#region State
+var inputDirection:		Vector2
+var lastInputDirection:	Vector2
+
+var horizontalInput:	float
+var verticalInput:		float
+#endregion
+
+
+#region Signals
+# TBD: Should we add signals for actions like Jump and Fire?
+#endregion
+
+
+func _input(event: InputEvent):
+	if not isEnabled: return
+	pass # Handle one-shot input events such as jumping or firing.
+
+
+func _process(delta: float):
+	# TBD: Should we do this in [_process] or [_physics_process]?
+	if not isEnabled: return
+
+	self.inputDirection		= Input.get_vector(GlobalInput.Actions.moveLeft, GlobalInput.Actions.moveRight, GlobalInput.Actions.moveUp, GlobalInput.Actions.moveDown)
+	self.verticalInput		= Input.get_axis(GlobalInput.Actions.moveUp, GlobalInput.Actions.moveDown)
+	self.horizontalInput	= Input.get_axis(GlobalInput.Actions.moveLeft, GlobalInput.Actions.moveRight)
+
+	if inputDirection: lastInputDirection = inputDirection
+
+	# TODO: CHECK: Does this work for joystick input?
+
+	#debugInfo()
+
+
+func debugInfo():
+	Debug.watchList.inputDirection		= inputDirection
+	Debug.watchList.lastInputDirection	= lastInputDirection
+	Debug.watchList.verticalInput		= verticalInput
+	Debug.watchList.horizontalInput		= horizontalInput
