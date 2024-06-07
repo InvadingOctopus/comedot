f719d0620210db7dc65e11373f87e6c018fbc550
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-06-07 17:17:59 +0700

    Update TurningControlComponent
    - Use PlayerInputComponent
    - Add isEnabled
    - Hide debug info
    - Refactor text

--- Components/Control/TurningControlComponent.gd
+++ Components/Control/TurningControlComponent.gd
@@ -1,14 +1,28 @@
 ## Allows the player to rotate a node with left & right input actions.
-## Also known as "tank controls"
+## May be combined with the [ThrustControlComponent] to provide "tank-like" controls, similar to Asteroids.
+## Requirements: [PlayerInputComponent]
 
 class_name TurningControlComponent
 extends Component
 
 
+#region Parameters
+@export_range(0.1, 20, 0.1) var rotationSpeed: float = 5.0
+
 ## Override this to rotate a different node instead of the parent [Entity], such as a [GunComponent].
 @export var nodeToRotate: Node2D = null
 
-@export_range(0.1, 20, 0.1) var rotationSpeed: float = 5.0
+@export var isEnabled: bool = true
+#endregion
+
+
+#region State
+var playerInputComponent: PlayerInputComponent:
+	get:
+		if not playerInputComponent:
+			playerInputComponent = self.findCoComponent(PlayerInputComponent)
+		return playerInputComponent
+#endregion
 
 
 func _ready():
@@ -17,8 +31,10 @@ func _ready():
 
 
 func _physics_process(delta: float):
-	var rotationDirection := Input.get_axis(GlobalInput.Actions.turnLeft, GlobalInput.Actions.turnRight)
-	if not rotationDirection: return
-	Debug.watchList.rotationDirection = rotationDirection
+	if not isEnabled: return
 
-	nodeToRotate.rotation += (rotationSpeed * rotationDirection) * delta
+	var rotationDirection: float = playerInputComponent.turnInput
+
+	if rotationDirection: nodeToRotate.rotation += (rotationSpeed * rotationDirection) * delta
+
+	#Debug.watchList.rotationDirection = rotationDirection
