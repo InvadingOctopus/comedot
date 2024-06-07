6cbf7ec93b217dccd2516f21b6e8824c2357ed33
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-06-07 19:12:42 +0700

    Add AsteroidsControlComponent
    + Add AsteroidsMovementParameters

---
+++ Components/Control/AsteroidsControlComponent.gd
@@ -0,0 +1,116 @@
+## Lets the player move by rotating left or right, and thrust forward, and brake.
+## Similar to Asteroids; a common control scheme for spaceships or "tank"-like movement.
+## NOTE: This component is an independent and more advanced alternative to combining [TurningControlComponent] + [ThrustControlComponent], and it does not depend on [PlayerInputComponent].
+## Requirements: [CharacterBody2D]
+
+class_name AsteroidsControlComponent
+extends BodyComponent
+
+
+#region Parameters
+@export var isEnabled:  bool = true
+@export var parameters: AsteroidsMovementParameters = AsteroidsMovementParameters.new()
+#endregion
+
+
+#region State
+var horizontalInput:	float ## Turning
+var verticalInput:		float ## Thrust
+
+var inputDirection:		Vector2
+var lastInputDirection:	Vector2
+var lastDirection:		Vector2 ## Normalized
+var lastVelocity:		Vector2
+#endregion
+
+
+func _ready():
+	if parentEntity.body:
+		printLog("parentEntity.body.motion_mode → Floating")
+		parentEntity.body.motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
+	else:
+		printWarning("Missing parentEntity.body: " + parentEntity.logName)
+
+
+func _physics_process(delta: float):
+	if not isEnabled: return
+	processInput(delta)
+	parentEntity.callOnceThisFrame(body.move_and_slide)
+	lastVelocity = body.velocity # TBD: Should this come last?
+
+	# Avoid the "glue effect" where the character sticks to a wall until the velocity changes to the opposite direction.
+	parentEntity.callOnceThisFrame(Global.resetBodyVelocityIfZeroMotion, [body])
+
+	#showDebugInfo()
+
+
+## Get the input direction and handle the movement/deceleration.
+func processInput(delta: float):
+	if not isEnabled: return
+
+	self.inputDirection = Input.get_vector(GlobalInput.Actions.moveLeft, GlobalInput.Actions.moveRight, GlobalInput.Actions.moveUp, GlobalInput.Actions.moveDown)
+	self.horizontalInput = Input.get_axis(GlobalInput.Actions.turnLeft, GlobalInput.Actions.turnRight)
+	self.verticalInput = Input.get_axis(GlobalInput.Actions.moveBackward, GlobalInput.Actions.moveForward)
+
+	if inputDirection: lastInputDirection = inputDirection
+
+	var bodyDirection: Vector2 = Vector2.from_angle(body.rotation) # No need for [.normalized()]
+
+	# Turn
+
+	if horizontalInput:
+		body.rotation += (parameters.turningSpeed * horizontalInput) * delta
+
+	# Thrust
+
+	if parameters.shouldApplyAcceleration:
+		body.velocity = body.velocity.move_toward(bodyDirection * verticalInput * parameters.thrust, parameters.acceleration * delta)
+	else:
+		body.velocity = bodyDirection * verticalInput * parameters.thrust
+
+	# TODO: ? Compare setting vector components separately vs together
+
+	# Friction?
+
+	if parameters.shouldApplyFriction:
+
+		if is_zero_approx(verticalInput):
+			body.velocity = body.velocity.move_toward(Vector2.ZERO, parameters.friction * delta)
+
+	# Disable friction by maintaining velcoty fron the previous frame?
+
+	if parameters.shouldMaintainPreviousVelocity and not inputDirection:
+		body.velocity = lastVelocity
+	return
+	# Minimum velocity?
+
+	if parameters.shouldMaintainMinimumVelocity:
+		if body.velocity.length() < parameters.minimumSpeed:
+			if body.velocity.is_zero_approx():
+				body.velocity = self.lastVelocity.normalized() * parameters.minimumSpeed
+			else:
+				body.velocity = body.velocity.normalized() * parameters.minimumSpeed
+
+	# Last direction
+
+	if not body.velocity.is_zero_approx():
+		#if currentState == State.idle: currentState = State.walk
+		lastDirection = body.velocity.normalized()
+
+	#Debug.watchList.lastInputDirection = lastInputDirection
+	#Debug.watchList.lastDirection = lastDirection
+
+
+## NOTE: Not implemented
+func processFriction(delta: float):
+	pass
+
+
+func showDebugInfo():
+	Debug.watchList.velocity = body.velocity
+	Debug.watchList.wallNormal = body.get_wall_normal()
+	Debug.watchList.lastMotion = body.get_last_motion()
+
+	Debug.watchList.bodyDirection = Vector2.from_angle(body.rotation)
+	Debug.watchList.horizontalInput = horizontalInput
+	Debug.watchList.verticalInput = verticalInput
---
+++ Components/Parameters/AsteroidsMovementParameters.gd
@@ -0,0 +1,34 @@
+## Set of movement physics parameters for [AsteroidsControlComponent].
+
+class_name AsteroidsMovementParameters
+extends Resource
+
+#region Parameters
+
+@export_subgroup("Movement") # ----
+
+@export_range(50, 1000, 5) var thrust:			float = 300
+
+@export var shouldApplyAcceleration:			bool  = true
+@export_range(50, 2000, 5) var acceleration:	float = 800
+
+## Completely disables slowdown from friction by reapplying the velocity from the previous frame.
+## Use for scenarios like slippery surfaces such as ice.
+@export var shouldMaintainPreviousVelocity:		bool  = false
+
+@export var shouldMaintainMinimumVelocity:		bool  = false
+@export_range(10, 1000, 50) var minimumSpeed:	float = 100
+
+@export_subgroup("Rotation") # ----
+
+@export_range(0, 20, 0.1) var turningSpeed:		float = 5
+
+@export_subgroup("Friction") # ----
+
+## Slow the velocity down each frame.
+@export var shouldApplyFriction:				bool  = true
+@export_range(10, 2000, 10) var friction:		float = 1000
+
+@export var shouldResetVelocityOnCollision:		bool  = true
+
+#endregion
