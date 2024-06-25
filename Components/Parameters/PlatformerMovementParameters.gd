c45c3283894506e82e16ee5a35ca7ad85a1a60f3
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-06-25 09:45:23 +0700

    Add gravityScale to PlatformerMovementParameters
    + Improve comments

--- Components/Parameters/PlatformerMovementParameters.gd
+++ Components/Parameters/PlatformerMovementParameters.gd
@@ -20,24 +20,34 @@ extends Resource
 
 @export_subgroup("Movement in Air")
 
+## 1.0 is normal gravity as defined in Project Settings/Physics/2D
+@export_range(-10, 10, 0.05) var gravityScale:		float = 1
+
 @export var shouldAllowMovementInputInAir:			bool  = true
 
-@export_range(0, 1000, 5) var speedInAir:			float = 100.0
+@export_range(0, 1000, 5) var speedInAir:			float = 100
 
 ## If `false`, then [member speed] is applied directly.
 @export var shouldApplyAccelerationInAir:			bool  = true
-@export_range(0, 1000, 5) var accelerationInAir:	float = 400.0
+@export_range(0, 1000, 5) var accelerationInAir:	float = 400
 
 @export var shouldApplyFrictionInAir:				bool  = true ## Applies horizontal friction when not on a floor (not gravity).
-@export_range(0, 5000, 5) var frictionInAir:		float = 200.0 ## Applies horizontal friction when not on a floor (not gravity).
+@export_range(0, 5000, 5) var frictionInAir:		float = 200 ## Applies horizontal friction when not on a floor (not gravity).
 
 
 @export_subgroup("Jump")
 
-@export_range(0, 5, 1) var maxNumberOfJumps: int = 2
+@export_range(0, 5, 1) var maxNumberOfJumps:		int = 2
 
-@export_range(-1000, -10, 5) var jumpVelocity1stJump:		float = -350.0 ## NOTE: This should be a NEGATIVE value because a positive Y axis value means downwards.
-@export_range(-1000, -10, 5) var jumpVelocity1stJumpShort:	float = -175.0 ## NOTE: This should be a NEGATIVE value because a positive Y axis value means downwards.
-@export_range(-1000, -10, 5) var jumpVelocity2ndJump:		float = -300.0 ## NOTE: This should be a NEGATIVE value because a positive Y axis value means downwards.
+## NOTE: This should be a NEGATIVE value because a positive Y axis value means downwards.
+@export_range(-1000, -10, 5) var jumpVelocity1stJump:		float = -350 
+
+## A shorter maximum velocity for the 1st jump if the player releases the Jump button quickly.
+## NOTE: This should be a NEGATIVE value because a positive Y axis value means downwards.
+@export_range(-1000, -10, 5) var jumpVelocity1stJumpShort:	float = -175
+
+## The velocity of the 2nd and later jumps.
+## NOTE: This should be a NEGATIVE value because a positive Y axis value means downwards.
+@export_range(-1000, -10, 5) var jumpVelocity2ndJump:		float = -300
 
 #endregion
