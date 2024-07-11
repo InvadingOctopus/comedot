fa13c5bb737b897851324c816eb955d0a1482365
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-07-11 06:30:49 +0400

    Add CharacterBodyManipulatingComponentBase
    TODO: Better name? :)

---
+++ Components/Physics/CharacterBodyManipulatingComponentBase.gd
@@ -0,0 +1,34 @@
+## Base class for components which depend on a [CharacterBodyComponent] to manipulate a [CharacterBody2D] before and after it moves during each frame.
+## Components which need to perform updates AFTER [method CharacterBody2D.move_and_slide] must connect to the [signal CharacterBodyComponent.didMove] signal.
+## NOTE: This is NOT the base class for the [CharacterBodyComponent] itself.
+## @experimental
+
+class_name CharacterBodyManipulatingComponentBase
+extends Component
+
+
+#region Parameters
+#endregion
+
+
+#region State
+
+var characterBodyComponent: CharacterBodyComponent:
+	get:
+		if not characterBodyComponent: characterBodyComponent = findCoComponent(CharacterBodyComponent)
+		return characterBodyComponent
+
+var body: CharacterBody2D:
+	get:
+		if not body: body = characterBodyComponent.body
+		return body
+		
+#endregion
+
+
+# Called whenever the node enters the scene tree.
+func ready():
+	#super._enter_tree()
+	
+	if not self.characterBodyComponent:
+		printError("Missing CharacterBody2D in parent Entity: \n" + parentEntity.logFullName)
