135a1b6244a3956b1d58c49fd35087b57d1f8fb5
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-07-22 10:58:37 +0400

    Overhaul DebugComponent
    + Add charting and watchlist for arbitrary properties

--- Components/DebugComponent.gd
+++ Components/DebugComponent.gd
@@ -1,10 +1,62 @@
-## Displays debugging information about the entity and other sibling components.
+## Displays debugging information about the entity and other components or nodes.
 
 class_name DebugComponent
-extends BodyComponent
+extends Component
+
+
+#region Parameters
+@export var propertiesToChart: Array[NodePath]
+@export var propertiesToWatch: Array[NodePath]
+#endregion
+
+#region State
+@onready var label: Label = %DebugLabel
+var propertiesToChartAbsolutePaths: Array[NodePath]
+var propertiesToWatchAbsolutePaths: Array[NodePath]
+#endregion
+
+
+func _ready() -> void:
+	propertiesToChartAbsolutePaths = convertPathsToAbsolute(propertiesToChart)
+	propertiesToWatchAbsolutePaths = convertPathsToAbsolute(propertiesToWatch)
+	createCharts()
+
+
+## Convert the `./` paths to the absolute representation (from `/root/`) including the property paths `:`
+func convertPathsToAbsolute(relativePaths: Array[NodePath]) -> Array[NodePath]:
+	var absolutePath:	NodePath
+	var absolutePaths:	Array[NodePath]
+	
+	for relativePath: NodePath in relativePaths:
+		absolutePath = Global.convertRelativePathToAbsolute(self, relativePath)
+		absolutePaths.append(absolutePath)
+	
+	return absolutePaths
+
+
+func createCharts() -> void:
+	var nodeAndPropertyPaths: Array[NodePath]
+	
+	for path: NodePath in propertiesToChartAbsolutePaths:
+		nodeAndPropertyPaths = Global.splitPathIntoNodeAndProperty(path)
+		# DEBUG: Debug.printLog(str("path: ", path, ", nodePath: ", nodeAndPropertyPaths[0], ", propertyPath: ", nodeAndPropertyPaths[1]))
+		Debug.createChartWindow(nodeAndPropertyPaths[0], nodeAndPropertyPaths[1])
 
 
 func _physics_process(delta: float):
-	%PositionLabel.text = str("@:", parentEntity.position, "\n@G:", parentEntity.global_position)
-	%VelocityLabel.text = str("V:", body.velocity)
+	updateLabel()
+
+
+func updateLabel() -> void:
+	# TODO: PERFORMANCE: More efficient lookups
+	var nodeAndPropertyPaths: Array[NodePath]
+	var nodeToWatch: Node
+	var labelText: String
 	
+	for path: NodePath in propertiesToWatchAbsolutePaths:
+		nodeAndPropertyPaths = Global.splitPathIntoNodeAndProperty(path)
+		# DEBUG: Debug.printLog(str("path: ", path, ", nodePath: ", nodeAndPropertyPaths[0], ", propertyPath: ", nodeAndPropertyPaths[1]))
+		nodeToWatch = self.get_node(nodeAndPropertyPaths[0]) # TBD: Should it be `self` or the SceneTree root?
+		labelText += str(nodeAndPropertyPaths[1].get_subname(nodeAndPropertyPaths[1].get_subname_count() - 1), ": ", nodeToWatch.get_indexed(nodeAndPropertyPaths[1]), "\n")
+	
+	label.text = labelText
