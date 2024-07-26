69048788924bda97ab4be7f558e6ccbd73d38cb6
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-07-26 05:34:36 +0400

    Add TextBubble prototype

---
+++ Scenes/Decorations/TextBubble.gd
@@ -0,0 +1,23 @@
+## A text [Label] that floats up and disappears.
+## Call the static method [method TextBubble.create] to add a text bubble to a node.
+## Useful for showing health values etc. over a sprite.
+
+class_name TextBubble
+extends Label
+
+@onready var animationPlayer: AnimationPlayer = $AnimationPlayer
+
+
+static func create(parentNode: Node2D, text: String) -> TextBubble:
+	var scenePath := Global.getScenePathFromClass(TextBubble)
+	var newBubble: TextBubble = (load(scenePath) as PackedScene).instantiate()
+	newBubble.text = text
+	parentNode.add_child(newBubble)
+	newBubble.owner = parentNode
+	return newBubble
+
+
+func _ready() -> void:
+	animationPlayer.play(&"bubble") # TBD: Remove hardcoding?
+	await animationPlayer.animation_finished
+	self.queue_free()
