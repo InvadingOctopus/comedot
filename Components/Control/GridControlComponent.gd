9185f98e168236912e0b6fd10eb1a910e713de17
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-08-03 04:50:43 +0400

    Improve GridControlComponent
    + Add continuous movement
    + Add diagonal movement
    + Add a delay Timer between steps
    * More reliable input handling

--- Components/Control/GridControlComponent.gd
+++ Components/Control/GridControlComponent.gd
@@ -4,9 +4,14 @@
 class_name GridControlComponent
 extends Component
 
+# TODO: Allow movement on input `is_just_released`
+
 
 #region Parameters
-@export var isEnabled: bool = true
+@export var isEnabled:				bool = true
+@export var shouldAllowDiagonals:	bool = false
+@export var shouldMoveContinuously:	bool = true
+@export var shouldShowDebugInfo:	bool = true
 #endregion
 
 
@@ -18,6 +23,8 @@ var tileBasedPositionComponent: TileBasedPositionComponent:
 		return tileBasedPositionComponent
 
 var recentInputVector: Vector2i
+
+@onready var timer: Timer = $Timer
 
 #endregion
 
@@ -33,13 +40,40 @@ func getRequiredcomponents() -> Array[Script]:
 
 func _input(event: InputEvent) -> void:
 	if not isEnabled or not event.is_action_type(): return
+		
+	if GlobalInput.hasActionTransitioned(GlobalInput.Actions.moveLeft) \
+	or GlobalInput.hasActionTransitioned(GlobalInput.Actions.moveRight) \
+	or GlobalInput.hasActionTransitioned(GlobalInput.Actions.moveUp) \
+	or GlobalInput.hasActionTransitioned(GlobalInput.Actions.moveDown):
+		
+		var inputVectorFloat: Vector2 = Input.get_vector(
+			GlobalInput.Actions.moveLeft, GlobalInput.Actions.moveRight, \
+			GlobalInput.Actions.moveUp, GlobalInput.Actions.moveDown)
+		
+		if shouldAllowDiagonals:
+			self.recentInputVector = Vector2i(signf(inputVectorFloat.x), signf(inputVectorFloat.y))
+		else: # Fractional axis values will get zeroed in the conversion to integers
+			self.recentInputVector = Vector2i(inputVectorFloat)	
+			
+		if shouldShowDebugInfo: printDebug(str(recentInputVector))
+		
+		move()
+		
+
+
+func _physics_process(delta: float) -> void:
+	if not isEnabled or not shouldMoveContinuously: return	
 	
-	if event.is_action_pressed(GlobalInput.Actions.moveLeft) \
-	or event.is_action_pressed(GlobalInput.Actions.moveRight) \
-	or event.is_action_pressed(GlobalInput.Actions.moveUp) \
-	or event.is_action_pressed(GlobalInput.Actions.moveDown):
-		
-		self.recentInputVector = Vector2i(Input.get_vector(GlobalInput.Actions.moveLeft, GlobalInput.Actions.moveRight, GlobalInput.Actions.moveUp, GlobalInput.Actions.moveDown))
-		
-		if not is_zero_approx(recentInputVector.length()):
-			tileBasedPositionComponent.processMovementInput(self.recentInputVector)
+	move()
+
+
+## Tells the [TileBasedPositionComponent] to move to the [member recentInputVector].
+## Uses a [Timer] to add a delay between each step.
+## NOTE: Does not depend on [member isEnabled]
+func move() -> void:
+	if is_zero_approx(recentInputVector.length()) or not is_zero_approx(timer.time_left):
+		return
+	
+	tileBasedPositionComponent.inputVector = self.recentInputVector
+	tileBasedPositionComponent.processMovementInput()
+	timer.start()
