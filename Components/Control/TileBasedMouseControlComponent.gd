55e8a695c428dd1241a47e076bbd4c840b81c75b
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-08-03 09:23:50 +0400

    Add TileBasedMouseControlComponent

---
+++ Components/Control/TileBasedMouseControlComponent.gd
@@ -0,0 +1,37 @@
+## Snaps the [TileBasedPositionComponent]'s coordinates to the mouse pointer.
+## Used for displaying selection cursors etc.
+## Requirements: [TileBasedPositionComponent]
+
+class_name TileBasedMouseControlComponent
+extends Component
+
+# TODO: Only update on mouse movement
+
+
+#region Parameters
+@export var isEnabled: bool = true
+#endregion
+
+
+#region State
+
+var tileBasedPositionComponent: TileBasedPositionComponent:
+	get:
+		if not tileBasedPositionComponent: tileBasedPositionComponent = self.getCoComponent(TileBasedPositionComponent)
+		return tileBasedPositionComponent
+
+#endregion
+
+
+## Returns a list of required component types that this component depends on.
+func getRequiredcomponents() -> Array[Script]:
+	return [TileBasedPositionComponent]
+
+
+#func _input(event: InputEvent) -> void:
+func _process(delta: float) -> void:
+	if not isEnabled: return
+	
+	var tileMap: TileMapLayer = tileBasedPositionComponent.tileMap
+	tileBasedPositionComponent.currentTileCoordinates = tileMap.local_to_map(tileMap.get_local_mouse_position())
+	tileBasedPositionComponent.snapEntityPositionToTile(tileBasedPositionComponent.currentTileCoordinates)
