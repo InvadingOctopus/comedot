86f71f1452e7ca251e8e0c420337bb3684b43b33
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-08-04 10:31:01 +0400

    Add shouldKeepTryingUntilValidMove
    * Refactor functions

--- Components/Movement/TileBasedRandomMovementComponent.gd
+++ Components/Movement/TileBasedRandomMovementComponent.gd
@@ -11,6 +11,11 @@ extends TileBasedControlComponent
 
 ## An array of steps to take from randomly on every [signal TImer.timeout] of the [member timer].
 @export var verticalMovesSet: Array[int] = [-1, 0, 1]
+
+@export var shouldKeepTryingUntilValidMove: bool = true
+
+const maximumTries: int = 10
+
 #endregion
 
 
@@ -23,20 +28,39 @@ func _ready() -> void:
 	tileBasedPositionComponent.didArriveAtNewTile.connect(self.onTileBasedPositionComponent_didArriveAtNewTile)
 
 
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	pass # Supress TileBasedControlComponent
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	pass # Supress TileBasedControlComponent
 
 
-func onTileBasedPositionComponent_didArriveAtNewTile(newDestination: Vector2i):
+func onTileBasedPositionComponent_didArriveAtNewTile(_newDestination: Vector2i) -> void:
 	if not isEnabled: return
 	pass #stepTimer.start() # Unneeded if Timer is not `one_shot`
 
 
 func onStepTimer_timeout() -> void:
 	if not isEnabled: return
-	self.recentInputVector = Vector2i(horizontalMovesSet.pick_random(), verticalMovesSet.pick_random())
+	moveRandomly()
+
+
+func moveRandomly() -> void:
+	self.recentInputVector = getRandomVector()
+	
+	# Should we keep rerolling until we find a vacant tile to move to?
+	
+	var tries: int = 0
+	
+	if shouldKeepTryingUntilValidMove:
+		while not tileBasedPositionComponent.validateCoordinates(tileBasedPositionComponent.currentTileCoordinates + self.recentInputVector) \
+		or tries < maximumTries:
+			self.recentInputVector = getRandomVector()
+			tries += 1
+		 
 	self.move()
+
+
+func getRandomVector() -> Vector2i:
+	return Vector2i(horizontalMovesSet.pick_random(), verticalMovesSet.pick_random())
