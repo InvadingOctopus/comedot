fdcae06fc9674ead3e1faa8e49ec860c86cbc38e
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-08-04 19:33:31 +0400

    Add DynamicArrayDictionary

---
+++ Scripts/DynamicArrayDictionary.gd
@@ -0,0 +1,27 @@
+## Stores data for arbitrary (X,Y) coordinates, as a Dictionary of the form: `{ 2D Array Index/Coordinates : Value }`
+## May be used for building maps and other grid-like structures with "dynamic" or "lazily" generated content.
+
+class_name DynamicArrayDictionary
+extends Node
+
+
+#region Parameters & State
+## A Dictionary representing a grid: `{Coordinates : Value}`
+## Each (x,y) [Vector2i] coordinate key contains an arbitrary value.
+@export var gridDictionary := { Vector2i(0, 0): null }
+
+@export var shouldShowDebugInfo: bool = false
+#endregion
+
+
+func setCellData(x: int, y: int, value: Variant) -> void:
+	var coordinates: Vector2i = Vector2i(x, y)
+	if shouldShowDebugInfo: Debug.printDebug(str("setCellData() @", coordinates, " = ", value), str(self))
+	gridDictionary[coordinates] = value
+
+
+func getCellData(x: int, y: int) -> Variant:
+	var coordinates: Vector2i = Vector2i(x, y)
+	var value: Variant = gridDictionary.get(coordinates)
+	if shouldShowDebugInfo: Debug.printDebug(str("getCellData() @", coordinates, ": ", value), str(self))
+	return value
