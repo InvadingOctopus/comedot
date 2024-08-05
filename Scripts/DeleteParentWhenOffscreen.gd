e3288f9b267e7f1e779c7e02fe27f63e7a2cd933
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-08-05 12:57:53 +0400

    Silence a mysteriously changed file
    This file kept showing as changed without any visible changes in any difference viewer. What the spooky heck?

--- Scripts/DeleteParentWhenOffscreen.gd
+++ Scripts/DeleteParentWhenOffscreen.gd
@@ -1,10 +1,7 @@
 extends VisibleOnScreenNotifier2D
 
 func onScreenExited() -> void:
-    # Delete parent node when offscreen
-    # TODO: Get topmost/root parent node?
-    var parent: Node = get_parent()
-    parent.queue_free()
-
-    
-    
+	# Delete parent node when offscreen
+	# TODO: Get topmost/root parent node?
+	var parent: Node = get_parent()
+	parent.queue_free()
