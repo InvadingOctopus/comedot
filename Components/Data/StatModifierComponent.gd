07ee4710b801f9800172cd7fa397ee7a8aae61aa
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-08-07 16:19:21 +0400

    Refactor text

--- Components/Data/StatModifierComponent.gd
+++ Components/Data/StatModifierComponent.gd
@@ -1,12 +1,15 @@
 ## Modifies a [Stat] over time at the interval specified on the child [Timer].
+
 class_name StatModifierComponent
 extends Component
 
+
+#region Parameters
 @export var stat: Stat
-
 @export_range(-10.0, 10.0, 0.5, "or_greater", "or_less") var modifier: int # HACK: A step of 0.5 to fordibly show the slider :')
+@export var isEnabled: bool = true
+#endregion
 
-@export var isEnabled := true
 
 func onTimerTimeout() -> void:
 	if not isEnabled: return
