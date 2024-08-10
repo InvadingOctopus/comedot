68b3462f2121a219fd55825eb99d0d1d9a2f4977
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-08-11 02:28:16 +0400

    Connect to signal on _ready()
    Saves the time of using the UI whenever adding this script.

--- Scripts/DeleteParentWhenOffscreen.gd
+++ Scripts/DeleteParentWhenOffscreen.gd
@@ -1,5 +1,10 @@
 extends VisibleOnScreenNotifier2D
 
+
+func _ready() -> void:
+	self.screen_exited.connect(onScreenExited)
+
+
 func onScreenExited() -> void:
 	# Delete parent node when offscreen
 	# TODO: Get topmost/root parent node?
