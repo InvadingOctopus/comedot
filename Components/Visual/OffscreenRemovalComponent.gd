8f10926cfd2d776b491a3c3ffae67119d0dcf050
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-08-11 02:43:49 +0400

    Add removalDelay for offscreen removal

--- Components/Visual/OffscreenRemovalComponent.gd
+++ Components/Visual/OffscreenRemovalComponent.gd
@@ -1,21 +1,15 @@
-## Deletes the parent Entity when the child `VisibleOnScreenNotifier2D` goes off screen.
+## Calls [method Entity.requestDeletion] → [method Node.queue_free()] on the parent [Entity] when the bounding [member VisibleOnScreenNotifier2D.rect] of the [VisibleOnScreenNotifier2D] goes off screen.
 
 class_name OffscreenRemovalComponent
 extends Component
 
-
-@export var rectangle: Rect2:
-	get: return onScreenNotifier.rect
-	set(newValue):
-		rectangle = newValue
-		if onScreenNotifier: onScreenNotifier.rect = rectangle
-
-@onready var onScreenNotifier: VisibleOnScreenNotifier2D = %OnScreenNotifier
+## The time in seconds to wait for before removing the Entity after it goes offscreen. If 0 the removal is instant.
+@export var removalDelay: float = 0
 
 
-func _ready() -> void:
-	onScreenNotifier.rect = self.rectangle
-
-
+## Delete parent node when offscreen
 func onScreenExited() -> void:
-	self.parentEntity.queue_free()
+	if removalDelay > 0:
+		await self.get_tree().create_timer(removalDelay).timeout
+
+	parentEntity.requestDeletion()
--- Scripts/DeleteParentWhenOffscreen.gd
+++ Scripts/DeleteParentWhenOffscreen.gd
@@ -1,12 +1,20 @@
+## Calls [method Node.queue_free()] on the parent [Node] when the bounding [member VisibleOnScreenNotifier2D.rect] of this [VisibleOnScreenNotifier2D] goes off screen.
+
 extends VisibleOnScreenNotifier2D
+
+
+## The time in seconds to wait for before removing the parent after it goes offscreen. If 0 the removal is instant.
+@export var removalDelay: float = 0
 
 
 func _ready() -> void:
 	self.screen_exited.connect(onScreenExited)
 
 
+## Delete parent node when offscreen
 func onScreenExited() -> void:
-	# Delete parent node when offscreen
-	# TODO: Get topmost/root parent node?
-	var parent: Node = get_parent()
-	parent.queue_free()
+	if removalDelay > 0:
+		await self.get_tree().create_timer(removalDelay).timeout
+
+	# TBD: Get topmost/root parent node?
+	get_parent().queue_free()
