064ae7fc60ec06a088eac627fb1f5479719d3783
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-08-16 14:34:26 +0400

    Improve PauseButton
    * Fix mouse events passthrough in Debug.tscn to prevent PauseButton from being blocked
    * Fix size
    * Change SF Symbols character to standard text
    * Implement visual update

--- Scripts/UI/PauseButton.gd
+++ Scripts/UI/PauseButton.gd
@@ -1,6 +1,22 @@
+## The Pause Button
+## NOTE: The [member Node.process_mode] should be [constant Node.ProcessMode.PROCESS_MODE_ALWAYS] so that the button can remain interactive while the gameplay is paused.
+
 extends Button
+
+
+func _ready() -> void:
+	updateState()
+
+
+func updateState() -> void:
+	# TBD: The Great Conundrum: Should the visual of a toggle-able control represent the CURRENT state or the NEXT state that will happen upon using the control?
+	# i.e. Should the Pause Button show the "PLAY >" icon when it's PAUSED? or should it show the "PAUSED ||" icon?
+	if self.get_tree().paused: self.text = ">"
+	else: self.text = "||"
 
 
 func onToggled(toggled_on: bool) -> void:
 	Global.setPause(toggled_on)
+	updateState()
+
 
