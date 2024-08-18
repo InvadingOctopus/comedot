613a06af27c06051c1b0daacdc791df37a0caf05
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-08-18 10:59:39 +0400

    Add StatsComponent.statsDictionary & getStat
    * Improve performance
    + Add cacheStats()
    + Cache stat during findStat()
    - Remove statNames :')

--- Components/Data/StatsComponent.gd
+++ Components/Data/StatsComponent.gd
@@ -5,7 +5,6 @@ extends Component
 
 
 #region Parameters
-@export var statNames: Array[StringName] # TBD: What is this used for? lol
 @export var stats: Array[Stat]
 
 ## May be used for automatically resetting stats in situations like restarting the game etc.
@@ -13,8 +12,24 @@ extends Component
 #endregion
 
 
+#region State
+var statsDictionary: Dictionary = {} ## Caches stats accessed by [StringName] keys.
+#endregion
+
+
 func _ready() -> void:
 	if shouldResetResourcesOnReady: resetStats()
+	cacheStats()
+
+
+## Saves each [Stat] in the [member stats] array to the [member statsDictionary] with the [member Stat.name] as its key, for quicker access.
+## TIP: Use [method getStat] to quickly access cached stats.
+## WARNING: May override previously cached stats.
+## Returns: The number of stats saved in the dictionary.
+func cacheStats() -> int:
+	for stat in self.stats:
+		statsDictionary[stat.name] = stat
+	return statsDictionary.size()
 
 
 func resetStats() -> void:
@@ -22,20 +37,30 @@ func resetStats() -> void:
 		stat = stat.duplicate() # TBD: Is there a better way?
 
 
-## Returns the first matching [Stat] found in [member stats].
-func findStat(nameToSearch: StringName) -> Stat:
+## Searches the [member statsDictionary] for the [param name] key. If no matching [Stat] is found, calls [method findStat].
+func getStat(statName: StringName) -> Stat:
+	var stat: Stat = statsDictionary.get(statName)
+	if not stat: stat = findStat(statName)
+	return stat
 
-	if stats.size() <= 0: return null
+
+## Returns the first matching [Stat] found in [member stats].
+## NOTE: This has a slower performance than [method getStat] if [param nameToSearch] has already been cached in the [member statsDictionary].
+func findStat(nameToSearch: StringName) -> Stat:
+	if stats.is_empty(): return null
 
 	for stat: Stat in stats:
-		if stat.name == nameToSearch: return stat
+		if stat.name == nameToSearch: 
+			statsDictionary[stat.name] = stat # Cache for quicker future access
+			return stat
 	# else:
 	printWarning("Cannot find Stat: " + nameToSearch)
 	return null
 
 
 ## Returns the value of the matching [Stat] found in [member stats], otherwise a `0` if no match is found.
+## Queries the [member statsDictionary] first or calls [method findStat].
 func getStatValue(statName: StringName) -> int:
-	var stat: Stat = self.findStat(statName)
+	var stat: Stat = self.getStat(statName)
 	if not stat: return 0
 	else: return stat.value
