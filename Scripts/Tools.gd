972aa13292d24b1f0df1896b9982298ddca00907
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-08-18 11:00:15 +0400

    Add Tools.addChildAndSetOwner()
    TODO: Replace previous pairs of `add_child` + `owner` calls

--- Scripts/Tools.gd
+++ Scripts/Tools.gd
@@ -75,6 +75,13 @@ static func addSceneInstance(scene: PackedScene, parent: Node, position: Vector2
 
 #region Node Management
 
+## Calls [param parent].[method Node.add_child] and sets the [param child].[member Node.owner].
+## This is necessary for persistence to a [PackedScene] for save/load.
+static func addChildAndSetOwner(child: Node, parent: Node) -> void: # DESIGN: TBD: Should `parent` be the 1st argument or 2nd? All global functions operate on the 1st argument, the parent [Node], but this method's name has "child" as the first word, so the `child` should be the 1st argument, right? :')
+	parent.add_child(child)
+	child.owner = parent
+
+
 static func findFirstChildOfType(parentNode: Node, type: Variant) -> Node:
 	var children: Array[Node] = parentNode.get_children()
 	for child in children:
