3809ad1f71639460a7a8e5fa914ab1766d4f1512
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-08-18 10:58:05 +0400

    Add Stat signals & remove @tool
    + Add didMax, didMin, didZero
    - Remove @tool and Engine.is_editor_hint() to improve performance
    * TODO: Verify signals :')

--- Resources/Stat.gd
+++ Resources/Stat.gd
@@ -1,9 +1,11 @@
 ## Represents a statistic on a character or object, such as the player's health or an enemy's attack power.
 ## May be optionally tied to a UI HUD element to update the display whenever the value changes.
 
-@tool # To clamp values when editing stats in the editor. # WARNING: This is causing errors on editor launch because of the GameState signal access.
+#@tool # To clamp values when editing stats in the editor. # WARNING: This is causing errors on editor launch because of the GameState signal access. It doesn't seem to provide much usage benefit, so it's disabled instead of using a potentially expensive `Engine.is_editor_hint()` check during each change.
 class_name Stat
 extends Resource
+
+# TODO: Support float?
 
 
 #region Parameters
@@ -43,13 +45,25 @@ extends Resource
 				# NOTE: PERFORMANCE: We don't use `Debug.printChange()` because we already checked for changes.
 				Debug.printDebug(str(self) + " " + str(name) + ": " + str(previousValue) + " → " + str(value) + " (" + str(previousChange) + ")")
 
+			# Signals
+			# TBD: CHECK: PERFORMANCE: Are signals expensive for frequently updated stats?
+
 			emit_changed()
 
-			if not Engine.is_editor_hint(): # TBD: Is this check too expensive for frequently updated stats?
-				GameState.HUDStatUpdated.emit(self) # TBD: Should this be optional? CHECK: Are signals expensive for frequently updated stats?
+			# NOTE: We don't use `elif` because more than one signal may be emitted during a single change, if min/max/0 are equal.
+			
+			if previousChange > 0: # Were we rising?
+				if value >= max: didMax.emit()
+				if value >= 0:	 didZero.emit()
+
+			if previousChange < 0: # Were we falling?
+				if value <= min: didMin.emit()
+				if value <= 0:	 didZero.emit()
+			
+			GameState.HUDStatUpdated.emit(self) # TBD: Should this be optional?
 
 
-@export var logChanges := false
+@export var logChanges: bool = false
 
 #endregion
 
@@ -62,4 +76,15 @@ var previousChange: int ## [member value] - [member previousValue] so a decrease
 var percentage: float: ## The current [member value] as a percentage of the [member max] limit.
 	get: return float(value) / float(max) * 100.0
 
+#endregion
+
+
+
+#region Signals
+
+# NOTE: More than one signal may be emitted during a single change, if [member min] & [member max] are equal, or also equal to 0.
+
+signal didMax  ## Only emitted when [member previousChange] is >= +1
+signal didMin  ## Only emitted when [member previousChange] is <= -1
+signal didZero ## Emitted whether the value is rising or falling. See [member previousChange] to check the direction of approach to 0.
 #endregion
