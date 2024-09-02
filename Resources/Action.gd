1cecfc1892ad37fbc44c3878b44cb4dc18c211d2
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-09-02 13:54:24 +0400

    Add Action prototype

---
+++ Resources/Action.gd
@@ -0,0 +1,13 @@
+## Represents an action that may be performed by the player or any other character. May have a cost and may require a target to be chosen.
+## It may be a special skill/spell such as "Fireball", an innate ability such as "Fly", or a trivial command like "Examine".
+## @experimental
+
+class_name Action
+extends CostableResourceBase
+
+
+#region Parameters
+@export var requiresTarget: bool 
+#endregion
+
+
