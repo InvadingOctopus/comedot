694097348b5d44ead0dce13a7f2cee25f5fb50bd
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-09-06 21:50:01 +0400

    Rename CharacterToRigidCollisionComponent
    to PushRigidBodyComponent
    
    + Add isEnabled

--- Components/Physics/PushRigidBodyComponent.gd
+++ Components/Physics/PushRigidBodyComponent.gd
@@ -1,6 +1,7 @@
 ## Allows a [CharacterBody2D] to push [RigidBody2D]s.
-## NOTE: This component should be processed AFTER all components which call `CharacterBody2D.move_and_slide()`
-class_name CharacterToRigidCollisionComponent
+## NOTE: This component should be processed AFTER all other components which call [method CharacterBody2D.move_and_slide]
+
+class_name PushRigidBodyComponent
 extends CharacterBodyManipulatingComponentBase
 
 # CREDIT: KidsCanCode@YouTube https://www.youtube.com/watch?v=SJuScDavstM
@@ -8,10 +9,12 @@ extends CharacterBodyManipulatingComponentBase
 
 #region Parameters
 @export_range(10.0, 100.0, 10.0) var pushingForce: float = 100.0
+@export var isEnabled: bool = true
 #endregion
 
 
 func _physics_process(_delta: float) -> void:
+	if not isEnabled: return
 	for collisionIndex in body.get_slide_collision_count():
 		var collision: KinematicCollision2D = body.get_slide_collision(collisionIndex)
 		if collision.get_collider() is RigidBody2D:
