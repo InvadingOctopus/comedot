98ca1c9ee0f8991930225b3179aa01b8b9e632be
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-09-08 05:17:13 +0400

    OCD alignment

--- AutoLoad/TurnBasedCoordinator.gd
+++ AutoLoad/TurnBasedCoordinator.gd
@@ -34,9 +34,9 @@ extends Node # + TurnBasedObjectBase
 ## but a healing-over-time component may increase the health at the START of a turn.
 enum TurnBasedState { # TBD: Should this be renamed to "Phase"?
 	turnInvalid	= -1,
-	turnBegin	= 0,
-	turnUpdate	= 1,
-	turnEnd		= 2
+	turnBegin	=  0,
+	turnUpdate	=  1,
+	turnEnd		=  2
 	}
 
 #endregion
