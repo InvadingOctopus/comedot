7f862af7043ac9e4d496648f991f3955fd986042
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-09-11 07:48:56 +0400

    Add comments

--- Resources/Action.gd
+++ Resources/Action.gd
@@ -1,9 +1,12 @@
-## Represents an action that may be performed by the player or any other character. May have a cost and may require a target to be chosen.
+## Represents an "special" action that a player or another character may choose to per. May have a cost and may require a target to be chosen.
 ## It may be a special skill/spell such as "Fireball", an innate ability such as "Fly", or a trivial command like "Examine".
+## NOTE: In most games this does NOT include the very basic common actions such as movement, jumping, shooting etc.
 ## @experimental
 
 class_name Action
 extends StatDependentResourceBase
+
+# TBD: A less ambiguous name? Because "action" is a Godot term for all input events.
 
 
 #region Parameters
