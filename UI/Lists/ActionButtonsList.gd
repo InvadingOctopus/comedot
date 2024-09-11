58ee5a94d372afa8d36badd0a16310d84e64902b
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-09-11 06:14:57 +0400

    Add ActionButtonsList prototype

---
+++ UI/Lists/ActionButtonsList.gd
@@ -0,0 +1,83 @@
+## Creates an [ActionButton] for each [Action] in an [ActionsComponent].
+## Attach this script to any [Container] [Control] such as a [GridContainer] or [HBoxContainer].
+## @experimental
+
+class_name ActionButtonsList
+extends Container
+
+
+#region Parameters
+
+## An [Entity] with an [ActionsComponent] and a [StatsComponent] which will be used for validating and displaying each [Action] button.
+## If `null`, the first [member GameState.players] Entity will be used.
+@export var entity: Entity
+
+@export var shouldShowDebugInfo: bool = false
+#endregion
+
+
+#region State
+var lastActionChosen: Action
+#endregion
+
+
+#region Signals
+signal didChooseAction(action: Action)
+#endregion
+
+
+#region Dependencies
+
+const actionButtonScene: PackedScene = preload("res://UI/Buttons/ActionButton.tscn") # TBD: load or preload?
+
+var actionsComponent: ActionsComponent:
+	get:
+		if not actionsComponent: actionsComponent = entity.getComponent(ActionsComponent)
+		return actionsComponent
+
+var targetStatsComponent: StatsComponent:
+	get:
+		if not targetStatsComponent: targetStatsComponent = entity.getComponent(StatsComponent)
+		return targetStatsComponent
+
+var player: PlayerEntity:
+	get: return GameState.players.front()
+
+#endregion
+
+
+func _ready() -> void:
+	if not entity:
+		entity = player
+		if not entity: Debug.printWarning("Missing entity", str(self))
+
+	if actionsComponent: readdAllActions()
+	else: Debug.printWarning("Missing actionsComponent", str(self))
+
+
+## Removes all children and adds all choices again.
+func readdAllActions() -> void:
+	Tools.removeAllChildren(self)
+	for action in actionsComponent.actions:
+		createActionButton(action)
+
+
+func createActionButton(action: Action) -> Button:
+	var newActionButton: ActionButton = actionButtonScene.instantiate()
+	newActionButton.shouldShowDebugInfo  = self.shouldShowDebugInfo
+
+	newActionButton.entity = self.entity
+	newActionButton.action = action
+
+	Tools.addChildAndSetOwner(newActionButton, self)
+
+	newActionButton.updateUI() # TBD: Update before adding or after?
+	newActionButton.pressed.connect(self.onActionButton_pressed.bind(action))
+
+	return newActionButton
+
+
+func onActionButton_pressed(action: Action) -> void:
+	if shouldShowDebugInfo: Debug.printDebug(str("onActionButton_pressed() ", action.logName), str(self))
+	self.lastActionChosen = action
+	self.didChooseAction.emit(action)
