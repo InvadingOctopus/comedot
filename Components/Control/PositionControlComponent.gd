339b4edeb97cef593a9ea7a5c022e34e120f9144
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-09-19 01:37:27 +0400

    Add PositionControlComponent

---
+++ Components/Control/PositionControlComponent.gd
@@ -0,0 +1,43 @@
+## Sets the [Entity]'s position directly on player input, without any physics.
+## May optionally use a secondary input axis such as the right gamepad joystick, to control the camera or an aiming cursor etc.
+
+class_name PositionControlComponent
+extends Component
+
+# TBD: Optional features like inertia for a better feeling of control? 
+
+
+#region Parameters
+
+@export_range(0.0, 1000.0, 10.0) var speed: float = 200
+
+## If `true`, uses the "look" input actions such as [const GlobalInput.Actions.lookLeft] etc.,
+## which default to a secondary input axis such as the right gamepad joystick,
+## that may be used to control a camera angle or an aiming cursor etc.
+@export var shouldUseSecondaryAxis: bool = false
+
+@export var isEnabled: bool = true
+
+#endregion
+
+
+func _process(delta: float) -> void: # TBD: Should this be `_physics_process()` or `_process()`?
+	# NOTE: Cannot use `_input()` because `delta` is needed.
+	if not isEnabled: return
+
+	var input: Vector2
+	
+	if shouldUseSecondaryAxis:
+		input = Input.get_vector(
+			GlobalInput.Actions.moveLeft,
+			GlobalInput.Actions.moveRight,
+			GlobalInput.Actions.moveUp,
+			GlobalInput.Actions.moveDown)
+	else:
+		input = Input.get_vector(
+			GlobalInput.Actions.lookLeft,
+			GlobalInput.Actions.lookRight,
+			GlobalInput.Actions.lookUp,
+			GlobalInput.Actions.lookDown)
+
+	parentEntity.position += input * speed * delta
