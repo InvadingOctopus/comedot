2e20febd84d66efa269f405bcbf2649e15595b47
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-09-18 06:01:34 +0400

    Add ?

--- Templates/Scripts/Resource/ScriptPayloadScriptTemplate.gd
+++ Templates/Scripts/Resource/ScriptPayloadScriptTemplate.gd
@@ -2,7 +2,7 @@
 
 ## A script for an [ScriptPayload] to execute.
 
-extends Resource # TBD: Should the base be a `ScriptPayloadScript` :)
+extends Resource # TBD: Should the base be a `ScriptPayloadScript`? :)
 
 
 static func onPayload_didExecute(source: Variant, target: Variant) -> Variant:
