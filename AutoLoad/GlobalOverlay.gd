23c757c17d99ba900398f32fe143b4c384b6180b
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-09-26 21:30:24 +0400

    Add Audio Player Pool in GlobalOverlay
    * Prevent playback and automatic removal in createAudioPlayer() if `stream` is `null` to allow node creation only
    + Add createAudioPlayerPool(), playAudioPlayerPool(), audioPlayers, currentAudioPlayerIndex
    ? TODO: Compare performance between playAudioPlayerPool() vs. createAudioPlayer()
    ! THANKS: kasdf@Discord

--- AutoLoad/GlobalOverlay.gd
+++ AutoLoad/GlobalOverlay.gd
@@ -14,6 +14,8 @@ var maximumNumberOfSounds: int = 10 # TBD
 
 #region State
 var pauseOverlay: PauseOverlay
+var audioPlayers: Array[AudioStreamPlayer2D]
+var currentAudioPlayerIndex: int
 #endregion
 
 
@@ -63,9 +65,11 @@ func createTemporaryLabel(text: String) -> Label:
 	return labelsList.createTemporaryLabel(text)
 
 
-## Creates and returns an [AudioStreamPlayer2D], plays it, then deletes it.
+## Creates an [AudioStreamPlayer2D], plays the specified sound, then deletes the node.
 ## Used for playing sound effects for nodes and entities that may be deleted before the audio finishes playing,
 ## such as enemy destruction or collectible pickups etc.
+## If [param stream] is `null`, then a [AudioStreamPlayer2D] node will be created only, but not played or automatically removed later.
+## Returns: The newly created [AudioStreamPlayer2D] node.
 func createAudioPlayer(
 	stream:   AudioStream,
 	position: Vector2 = Vector2.ZERO,
@@ -87,11 +91,63 @@ func createAudioPlayer(
 	sounds.add_child(audioPlayer, true) # force_readable_name
 	audioPlayer.owner = sounds # Necessary for persistence to a [PackedScene] for save/load.
 	audioPlayer.add_to_group(Global.Groups.audio, true) # persistent 
-	audioPlayer.play() # TBD: Add playback position argument? TBD: Find a way to move along with node and continue playing after the node is destroyed?
 	
-	audioPlayer.finished.connect(audioPlayer.queue_free)
+	if stream:
+		audioPlayer.play() # TBD: Add playback position argument? TBD: Find a way to move along with node and continue playing after the node is destroyed?
+		audioPlayer.finished.connect(audioPlayer.queue_free)
+	
 	return audioPlayer
 
+
+## Creates a "pool" of reusable [AudioStreamPlayer2D] nodes for [method playAudioPlayerPool].
+## May provide better performance compared to [method createAudioPlayer].
+## NOTE: Deletes all existing children already in the `Sounds` node.
+func createAudioPlayerPool() -> Array[AudioStreamPlayer2D]:
+	# Delete existing children
+	self.audioPlayers.clear()
+	Tools.removeAllChildren(sounds)
+
+	# Fill the pool
+	for count in maximumNumberOfSounds:
+		var newAudioPlayer: AudioStreamPlayer2D = createAudioPlayer(null)
+		self.audioPlayers.append(newAudioPlayer)
+	
+	currentAudioPlayerIndex = 0 # Reset the index
+	return self.audioPlayers
+
+
+## Plays a sound via one of the existing [AudioStreamPlayer2D] nodes from the "pool" in the `Sounds` parent node.
+## May provide better performance compared to [method createAudioPlayer].
+## Used for playing sound effects for nodes and entities that may be deleted before the audio finishes playing,
+## such as enemy destruction or collectible pickups etc.
+## Returns: The [AudioStreamPlayer2D] node which was used.
+func playAudioPlayerPool(
+	stream:   AudioStream,
+	position: Vector2 = Vector2.ZERO,
+	_bus:	  StringName = Global.AudioBuses.sfx) -> AudioStreamPlayer2D:
+	
+	# Cycle through the available AudioStremPlayer2D nodes,
+	# so we can have a pool of simultaneous sounds up to a limit.
+
+	var audioPlayer: AudioStreamPlayer2D
+
+	if audioPlayers.size() <= 0:
+		createAudioPlayerPool()
+		# TBD: Debug.printWarning("No AudioStreamPlayer2D in audioPlayers pool", str(self))
+		# return null
+	
+	audioPlayer = audioPlayers[currentAudioPlayerIndex]
+	
+	currentAudioPlayerIndex += 1
+	if currentAudioPlayerIndex >= audioPlayers.size():
+		currentAudioPlayerIndex = 0
+
+	# Configure the new sound
+	audioPlayer.stream = stream
+	audioPlayer.position = position
+	audioPlayer.play() # TBD: Add playback position argument? TBD: Find a way to move along with node and continue playing after the node is destroyed?
+	
+	return audioPlayer
 
 #region Animations
 
