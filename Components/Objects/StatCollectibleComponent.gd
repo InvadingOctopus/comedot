f4bae9eff58d527943e0f891e770865e58a003bf
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-10-06 17:05:31 +0400

    Check Stat.max in StatCollectibleComponent
    Add preventCollectionIfStatIsMax

--- Components/Objects/StatCollectibleComponent.gd
+++ Components/Objects/StatCollectibleComponent.gd
@@ -10,6 +10,8 @@ extends CollectibleComponent
 @export var statModifierMinimum: int = 1 ## The minimum amount of change, inclusive. To always apply a fixed amount, set both minimum and maximum to the same number.
 @export var statModifierMaximum: int = 1 ## The maximum amount of change, inclusive. To always apply a fixed amount, set both minimum and maximum to the same number.
 
+@export var preventCollectionIfStatIsMax: bool = true # TBD: Better name? :')
+
 @export var shouldDisplayIndicator: bool = true
 #endregion
 
@@ -23,6 +25,16 @@ func _ready() -> void:
 ## WARNING: Calling this function repeatedly may introduce gameplay bugs; Get a random value once and store it to a variable or property.
 func getRandomModifier() -> int:
 	return randi_range(statModifierMinimum, statModifierMaximum) if statModifierMinimum != statModifierMaximum else statModifierMaximum
+
+
+## Prevent collection if `preventCollectionIfStatIsMax` and the Stat is already at its [member Stat.max].
+func checkCollectionConditions(collectorEntity: Entity, collectorComponent: CollectorComponent) -> bool:
+	if not super.checkCollectionConditions(collectorEntity, collectorComponent): return false
+	if preventCollectionIfStatIsMax and stat.value >= stat.max: 
+		if shouldShowDebugInfo: printDebug(str("preventCollectionIfStatIsMax and stat.value ", stat.value, " >= stat.max ", stat.max))
+		return false
+	else:
+		return true
 
 
 ## Returns: The randomized stat modifier value.
