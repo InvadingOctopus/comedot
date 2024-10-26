5ac74f65680c4b90c8ed73c602abafcdc1d2f0d0
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-10-26 10:02:12 +0700

    Update NamedResourceUI
    * TODO: Better abstraction when GDScript supports overriding properties :')

--- UI/Views/NamedResourceUI.gd
+++ UI/Views/NamedResourceUI.gd
@@ -1,37 +1,91 @@
-## Displays the name and icon of a [NamedResourceBase] and uses the [member NamedResourceBase.description] as the tooltip.
+## An abstract base cass for [Container] [Controls] that represent a [NamedResourceBase]-derived Resource.
+## Displays the name and icon, and uses the [member NamedResourceBase.description] as the tooltip.
 
 @tool
 class_name NamedResourceUI
 extends Container
 
-# TBD: Should it be named "-Label" or "-UI"?
+# TODO: Better abstraction when GDScript supports overriding properties :')
 
 
 #region Parameters
-@export var resource: NamedResourceBase:
+
+#@export var resource: NamedResourceBase
+
+@export var shouldAnimate:   bool = true
+
+@export var shouldShowText:  bool = true: 
 	set(newValue):
-		if newValue != resource:
-			resource = newValue
-			updateUI()
+		if newValue != shouldShowText:
+			shouldShowText = newValue
+			if label: updateText(false) # Update the label, without animation
+
+@export var shouldUppercase: bool = false:
+	set(newValue):
+		if newValue != shouldUppercase:
+			shouldUppercase = newValue
+			if label: label.uppercase = shouldUppercase
+
+@export var shouldShowIcon:  bool = true:
+	set(newValue):
+		if newValue != shouldShowIcon:
+			shouldShowIcon = newValue
+			if icon: icon.visible = shouldShowIcon
+
+@export var shouldShowIconAfterText: bool = false:
+	set(newValue):
+		if newValue != shouldShowIconAfterText:
+			shouldShowIconAfterText = newValue
+			arrangeControls()
+
 #endregion
 
 
 #region State
-@onready var label: Label = $Label
-@onready var icon:  TextureRect = $Icon
+@onready var label:	Label		= $Label
+@onready var icon:	TextureRect	= $Icon
 #endregion
 
 
+## May be overridden in subclass.
 func _ready() -> void:
-	updateUI()
+	applyInitialFlags()
 
 
-func updateUI() -> void:
-	if is_instance_valid(resource): 
-		if label: label.text   = resource.displayName
-		if icon:  icon.texture = resource.icon
-		self.tooltip_text	   = resource.description
+func applyInitialFlags() -> void:
+	if label: label.uppercase	= shouldUppercase
+	if icon:  icon.visible		= shouldShowIcon
+	if shouldShowIconAfterText: arrangeControls()
+
+
+func arrangeControls() -> void:
+	# TBD: Account for other controls in between?
+	if not shouldShowIconAfterText:
+		self.move_child(icon,  0)
+		self.move_child(label, 1)
 	else:
-		if label: label.text   = ""
-		if icon:  icon.texture = null # TBD:
-		self.tooltip_text	   = ""
+		self.move_child(label, 0)
+		self.move_child(icon,  1)
+
+
+## May be overridden in subclass.
+func updateUI(animate: bool = self.shouldAnimate) -> void:
+	# Update the text first in case its length changes and affects the icon.
+	updateText(animate)
+	updateIcon(animate)
+
+
+## IMPORTANT: Abstract; MUST be overridden in subclass
+@warning_ignore("unused_parameter")
+func updateIcon(animate: bool = self.shouldAnimate) -> void:
+	pass # icon.texture = resource.icon
+
+
+## IMPORTANT: Abstract; MUST be overridden in subclass
+@warning_ignore("unused_parameter")
+func updateText(animate: bool = self.shouldAnimate) -> void:
+	pass
+	# self.label.text = resource.displayName
+	# self.tooltip_text = resource.description
+
+
