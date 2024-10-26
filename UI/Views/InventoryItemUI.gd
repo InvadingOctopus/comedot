bccd0157f4ee106eac5491b0aab77a134bc2430c
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-10-26 14:54:29 +0700

    Add InventoryItemUI

---
+++ UI/Views/InventoryItemUI.gd
@@ -0,0 +1,39 @@
+## A [Container] with an icon and other [Control]s to represent an [InventoryItem].
+
+class_name InventoryItemUI
+extends NamedResourceUI
+
+
+
+#region Parameters
+
+@export var inventoryItem: InventoryItem
+#endregion
+
+
+#region State
+#endregion
+
+
+func _ready() -> void:
+	applyInitialFlags()
+
+	if inventoryItem:
+		updateUI(false) # Display the initial value, without animation
+		inventoryItem.changed.connect(self.onInventoryItem_changed)
+	else:
+		Debug.printWarning("Missing inventoryItem", self)
+
+
+func onInventoryItem_changed() -> void:
+	updateText()
+
+
+## TIP: May be overridden in subclass to customize the icon, for example, show different icons or colors for different ranges of the [member Stat.value].
+func updateIcon(_animate: bool = self.shouldAnimate) -> void:
+	icon.texture = inventoryItem.icon
+
+
+func updateText(_animate: bool = self.shouldAnimate) -> void:
+	self.label.text   = inventoryItem.displayName
+	self.tooltip_text = inventoryItem.description
