100e53fda8c2d8ccfef34a69913888b6327283a4
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-11-16 14:18:05 +0700

    Refactor comments & text

--- Components/Visual/HealthVisualComponent.gd
+++ Components/Visual/HealthVisualComponent.gd
@@ -1,4 +1,7 @@
 ## Adds visual effects and indicators based on the state of a [HealthComponent].
+## NOTE: The visuals are played when the [Stat]'s value is changed,
+## so the effects may occur even when the Stat is modified elsewhere WITHOUT any damage happening to this component's parent entity,
+## for example if sharing the same "Health" Stat on multiple Entities!
 ## @experimental
 
 class_name HealthVisualComponent
@@ -40,7 +43,7 @@ func onHealthComponent_healthChanged(difference: int) -> void:
 func animate(difference: int) -> void:
 	if difference < 0:
 		Animations.blinkNode(self.parentEntity, 3)
-	
+
 	if shouldTint:
 		var health: Stat  = healthComponent.health
 		var red:	float = (1.0 - (health.percentage / 100.0)) * 5.0 # Increase redness as health gets lower
