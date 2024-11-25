fc102b330fecd74618b21bcd05d71e9a21a867c6
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-11-25 07:02:36 +0700

    Add TurnBasedTileBasedPlatformerControlComponent
    Prototype
    May Goom forgive me for these names (; - ;)

---
+++ Components/TurnBased/TurnBasedTileBasedPlatformerControlComponent.gd
@@ -0,0 +1,37 @@
+## Provides platformer pseudo-"physics" for a turn-based game with tile-based positioning.
+## TIP: Use [TurnBasedTileBasedGravityComponent] for gravity.
+## Requirements: [TurnBasedEntity], [TileBasedPositionComponent]
+## @experimental
+
+class_name TurnBasedTileBasedPlatformerControlComponent
+extends TurnBasedTileBasedControlComponent
+
+# TBD: A better name...?
+
+
+#region Parameters
+#endregion
+
+
+#region State
+#endregion
+
+
+#region Signals
+#endregion
+
+
+func _input(event: InputEvent) -> void:
+	if not isEnabled or not event.is_action_type(): return
+
+	# NOTE: Only accept horizontal move input; only allow vertical movement via jumps and gravity.
+
+	if event.is_action_pressed(GlobalInput.Actions.moveLeft) \
+	or event.is_action_pressed(GlobalInput.Actions.moveRight):
+
+		if shouldShowDebugInfo: printLog(str(parentEntity.logName, " ", event))
+
+		self.recentInputVector = Input.get_vector(GlobalInput.Actions.moveLeft, GlobalInput.Actions.moveRight, GlobalInput.Actions.moveUp, GlobalInput.Actions.moveDown)
+		self.recentInputVector.y = 0 # NOTE: Negate the direct Y input; only allow vertical movement via jumps and gravity.
+
+		validateMove() # May start the turn
