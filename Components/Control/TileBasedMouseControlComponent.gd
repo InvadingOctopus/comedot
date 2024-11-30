019eda5c29912a17f25776d102a5660090161497
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-11-30 19:39:21 +0700

    Add TileBasedMouseControl…shouldProcessEveryFrame

--- Components/Control/TileBasedMouseControlComponent.gd
+++ Components/Control/TileBasedMouseControlComponent.gd
@@ -9,7 +9,17 @@ extends Component
 
 
 #region Parameters
+
+## If `true`, then the position is updated every frame via the [method Node._process] function. This allows accurate updates when a [TileMapLayer] or [Camera2D] is moving.
+## If `false` (default), then the position is only updated when a mouse motion event is received via the [method Node._input] function. This improves performance.
+@export var shouldProcessEveryFrame: bool = false:
+	set(newValue):
+		if newValue != shouldProcessEveryFrame:
+			shouldProcessEveryFrame = newValue
+			self.set_process(shouldProcessEveryFrame)
+
 @export var isEnabled: bool = true
+
 #endregion
 
 
@@ -21,9 +31,21 @@ func getRequiredComponents() -> Array[Script]:
 #endregion
 
 
-func _input(event: InputEvent) -> void:
-	if not isEnabled or not event is InputEventMouseMotion: return
+func _ready() -> void:
+	self.set_process(self.shouldProcessEveryFrame)
 
+
+func _input(event: InputEvent) -> void:
+	if not isEnabled or shouldProcessEveryFrame or not event is InputEventMouseMotion: return
+	updatePosition()
+
+
+func _process(_delta: float) -> void:
+	if not isEnabled or not shouldProcessEveryFrame: return
+	updatePosition()
+
+
+func updatePosition() -> void:
 	var tileMap: TileMapLayer = tileBasedPositionComponent.tileMap
 	tileBasedPositionComponent.currentCellCoordinates = tileMap.local_to_map(tileMap.get_local_mouse_position())
 	tileBasedPositionComponent.snapEntityPositionToTile(tileBasedPositionComponent.currentCellCoordinates)
