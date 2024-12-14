3cb3374264ca99d08515ddc0bd99554eb243b061
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-12-14 11:25:55 +0700

    Refactor KnockbackOnHitComponent
    + Add knockback()
    * Refactor code & text

--- Components/Combat/KnockbackOnHitComponent.gd
+++ Components/Combat/KnockbackOnHitComponent.gd
@@ -1,4 +1,4 @@
-## Pushes the entity back when a [DamageReceivingComponent] takes damage.
+## Pushes the entity back when its [DamageReceivingComponent] takes damage.
 ## TIP: Use a [VelocityClampComponent] to prevent the entity from "rocketing" away.
 ## WARNING: The knockback may not be applied if [member PlatformerMovementParameters.shouldStopInstantlyOnFloor] or [member PlatformerMovementParameters.shouldStopInstantlyInAir] is `true`.
 ## Requirements: [CharacterBodyComponent], [DamageReceivingComponent], AFTER [PlatformerPhysicsComponent]
@@ -27,11 +27,10 @@ extends CharacterBodyManipulatingComponentBase
 
 #region Dependencies
 @onready var damageReceivingComponent: DamageReceivingComponent = coComponents.DamageReceivingComponent # TBD: Static or dynamic?
-#endregion
-
 
 func getRequiredComponents() -> Array[Script]:
 	return [CharacterBodyComponent, DamageReceivingComponent] # Cannot easily join with `super.getRequiredComponents()` because Godot goes dumb and treats it as an untyped Array
+#endregion
 
 
 func _ready() -> void:
@@ -53,6 +52,12 @@ func onDamageReceivingComponent_didReceiveDamage(damageComponent: DamageComponen
 	# TBD: Use velocities?
 
 	var damageDirection: Vector2 = self.body.global_position.direction_to(damageComponent.global_position)
+	self.knockback(damageDirection)
+
+
+## Applies and returns a knockback force from the source of damage.
+func knockback(damageDirection: Vector2) -> Vector2:
+	if not isEnabled: return Vector2.ZERO
 
 	# Should we ensures that the knockback is always noticeable even if the player is moving at a high speed towards the damage source?
 	if shouldZeroCurrentVelocity:
@@ -61,12 +66,11 @@ func onDamageReceivingComponent_didReceiveDamage(damageComponent: DamageComponen
 	# WARNING: The knockback force may not be applied consistently
 	# because of `PlatformerPhysicsComponent.processAllFriction()` if `PlatformerMovementParameters.shouldStopInstantly…` is true.
 
-	# Apply force in the opposite direction
-	body.velocity += -damageDirection * knockbackForce
-
-	# Any more? For example, a jump when taking damage in a platform game.
-	body.velocity += additionalVector
+	# Apply force in the opposite direction + any other vector, such as a upwards jump when taking damage in a platform game.
+	var totalForce: Vector2 = (-damageDirection * knockbackForce) + additionalVector
+	body.velocity += totalForce
 
 	if shouldShowDebugInfo: printDebug(str("-damageDirection: ", -damageDirection, ", knockbackForce: ", knockbackForce, ", additionalVector: ", additionalVector, ", body.velocity: ", body.velocity))
 
 	characterBodyComponent.queueMoveAndSlide()
+	return totalForce
