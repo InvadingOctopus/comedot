f7aececa0b7b84f066f9ace70d91d468b11f0afc
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-12-20 17:42:58 +0700

    Fix PlayerSpawnPosition to wait for player ready
    * Improve comments

--- Scenes/Gameplay/PlayerSpawnPosition.gd
+++ Scenes/Gameplay/PlayerSpawnPosition.gd
@@ -1,13 +1,16 @@
-## Sets the position of the specified player [Entity] to itself on [method _ready]
+## Sets the position of the specified player [Entity] to itself on [method _ready].
 
 extends Marker2D
+
 
 #region Parameters
 
 ## The index into the [member GameState.players] array.
+## NOTE: Ignored if [member playerOverride] is set.
 @export var playerIndex: int = 0
 
 ## A custom [Entity] instead of a member of the [member GameState.players] array.
+## NOTE: Overrides [member playerIndex].
 @export var playerOverride: Entity
 
 @onready var playerSpawnPosition: Marker2D = self
@@ -17,14 +20,20 @@ extends Marker2D
 
 var player: PlayerEntity:
 	get:
-		if playerOverride:
-			return playerOverride
-		elif playerIndex == 0:
-			return GameState.players.front()
-		else:
-			return GameState.players[playerIndex]
+		if playerOverride: return playerOverride
+		elif playerIndex == 0: return GameState.players.front()
+		else: return GameState.players[playerIndex]
 
 
 func _ready() -> void:
-	if player:
-		player.global_position = playerSpawnPosition.global_position
+	if player: setPlayerPosition()
+	else: GameState.playerReady.connect(self.gameState_playerReady) # If there is no player yet, wait for one to be ready
+	
+
+func gameState_playerReady(newPlayer: Entity) -> void:
+	# Make sure that the player that just became ready is the one that we were asked to position
+	if newPlayer == self.player: self.setPlayerPosition()
+
+
+func setPlayerPosition() -> void:
+	if player: player.global_position = playerSpawnPosition.global_position
