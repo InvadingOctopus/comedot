f9392a7264538b1ff9da893283bdf9c6844a9ab4
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2024-12-27 11:23:25 +0700

    Add TurnBasedPlayerEntity

---
+++ Entities/Characters/TurnBasedPlayerEntity.gd
@@ -0,0 +1,50 @@
+## A subclass of [Entity] specialized for turn-based player-controlled characters.
+## ALERT: Game-specific subclasses which extend [TurnBasedPlayerEntity] MUST call `_super.ready()` etc. if those methods are overridden.
+
+class_name TurnBasedPlayerEntity
+extends TurnBasedEntity
+
+# TODO: Some way to reduce code duplication between PlayerEntity
+
+
+#region Shortcuts
+# Quick access to common components
+# NOTE: Not cached, because components may change during runtime.
+# INFO: Use direct access on Dictionary for better performance instead of `getComponent()`
+
+var bodyComponent: CharacterBodyComponent:
+	get: return self.components.get(&"CharacterBodyComponent")
+
+var healthComponent: HealthComponent:
+	get: return self.components.get(&"HealthComponent")
+
+var statsComponent: StatsComponent:
+	get: return self.components.get(&"StatsComponent")
+
+var inventoryComponent: InventoryComponent:
+	get: return self.components.get(&"InventoryComponent")
+
+var actionsComponent: ActionsComponent:
+	get: return self.components.get(&"ActionsComponent")
+
+var upgradesComponent: UpgradesComponent:
+	get: return self.components.get(&"UpgradesComponent")
+
+#endregion
+
+
+func _enter_tree() -> void:
+	super._enter_tree()
+	self.add_to_group(Global.Groups.players,   true) # persistent
+	self.add_to_group(Global.Groups.turnBased, true) # persistent
+
+
+func _ready() -> void:
+	printLog("_ready()")
+	GameState.addPlayer(self)
+	GameState.playerReady.emit(self)
+
+
+func _exit_tree() -> void:
+	super._exit_tree()
+	GameState.removePlayer(self)
