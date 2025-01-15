87a19e947760358e67119cdc643d0653a4cbfb36
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-01-16 01:12:14 +0700

    Overhaul SceneManager
    ! Change sceneStack to String path array: Improve performance
    + Add pushCurrentSceneToStack(), pushCurrentSceneAndTransition()
    + Cache sceneTree property
    * Improve documentation & logging

--- AutoLoad/SceneManager.gd
+++ AutoLoad/SceneManager.gd
@@ -14,8 +14,15 @@ const logName: String = "SceneManager" # Because we can't have class_name :')
 #region State
 
 ## A first-in=last-out stack of scenes that can be navigated within via [method pushSceneToStack] and [method popSceneFromStack].
-## NOTE: New items are "pushed" and "popped" from the END/BACK of the array (i.e. the last elements), NOT the front, to improve performance by not rearranging the rest of the array.
-static var sceneStack: Array[PackedScene]
+## Stores paths instead of [PackedScene] to save memory and improve performance.
+## IMPORTANT: The END of the array (i.e. the last element) is the TOP of the "stack" and the PREVIOUS scene; [member SceneTree.current_scene] is not stored on the stack.
+## New items are "pushed" and "popped" from the end/back of the array, NOT the front, to improve performance by not rearranging the rest of the array.
+static var sceneStack: Array[String]
+
+var sceneTree: SceneTree:
+	get:
+		if not sceneTree: sceneTree = self.get_tree()
+		return sceneTree
 
 #endregion
 
@@ -23,12 +30,14 @@ static var sceneStack: Array[PackedScene]
 #region Scene Management Methods
 
 ## Transitions to the specified scene with an optional animation.
-## NOTE: Does NOT use the [member sceneStack]; see [method pushSceneToStack] and [method popSceneFromStack].
+## NOTE: Does NOT use the [member sceneStack]; see [method pushCurrentSceneAndTransition] and [method popSceneFromStack].
 func transitionToScene(nextScene: PackedScene, pauseSceneTree: bool = true, animate: bool = true) -> void: # NOTE: Cannot be `static` because of `self.get_tree()`
-	var sceneTree: SceneTree = self.get_tree()
-	var sceneBeforeTransition: Node = sceneTree.current_scene
+	if not is_instance_valid(nextScene):
+		Debug.printError(str("transitionToScene(): Invalid scene: ", nextScene), logName)
+		return
 
-	Debug.printLog(str("transitionToScene(): ", sceneBeforeTransition, " → ", nextScene),  logName)
+	var sceneBeforeTransition: Node = sceneTree.current_scene
+	Debug.printAutoLoadLog(str("transitionToScene(): ", sceneBeforeTransition, " → ", nextScene, " ", nextScene.resource_path))
 
 	# Pause
 	sceneTree.paused = pauseSceneTree
@@ -42,50 +51,81 @@ func transitionToScene(nextScene: PackedScene, pauseSceneTree: bool = true, anim
 	sceneTree.paused = false
 
 
-## Transitions to a new scene, adds it to the [member sceneStack] and returns resulting stack size.
-func pushSceneToStack(nextScene: PackedScene, pauseSceneTree: bool = true) -> int:  # NOTE: Cannot be `static` because of `self.get_tree()`
-	# Push the current scene on the stack first
-	var sceneBeforeTransition: Node = self.get_tree().current_scene
-	sceneStack.push_back(sceneBeforeTransition) # NOTE: PERFORMANCE: Don't use push_front() because of slower performance.
+## Shortcut for calling [method pushCurrentSceneToStack] then [method transitionToScene].
+## Call [method popSceneFromStack] from the [param nextScene] to return to the previous scene.
+func pushCurrentSceneAndTransition(nextScene: PackedScene, pauseSceneTree: bool = true, animate: bool = true) -> void: # NOTE: Cannot be `static` because of `self.get_tree()`
+	self.pushCurrentSceneToStack()
+	await self.transitionToScene(nextScene, pauseSceneTree, animate) # IMPORTANT: await for animations
 
-	# Transition to the specified next scene
-	self.transitionToScene(nextScene, pauseSceneTree)
-	var sceneAfterTransition: Node = self.get_tree().current_scene
 
-	if sceneAfterTransition == nextScene:	
-		Debug.printLog(str("sceneStack.size: ", sceneStack.size()),  logName)
-	else:
-		Debug.printWarning(str("SceneTree.current_scene: ", sceneAfterTransition, " != nextScene: ", nextScene), logName)
+## Adds a scene path to the [member sceneStack] and returns the resulting stack size.
+static func pushSceneToStack(scenePath: String) -> int:
+	if scenePath.is_empty():
+		Debug.printWarning("pushSceneToStack(): Path empty!", logName)
+		return sceneStack.size()
+
+	# Check if we're pushing the same scene more than once
+	if not sceneStack.is_empty() and sceneStack.back() == scenePath:
+		Debug.printWarning("pushSceneToStack(): Scene already on top of stack: " + scenePath, logName)
+
+	sceneStack.push_back(scenePath) # NOTE: PERFORMANCE: Don't use push_front() because of slower performance.
+
+	if Debug.shouldPrintDebugLogs:
+		Debug.printDebug(str("pushSceneToStack(): ", scenePath, " → ", sceneStack.size(), ": ", sceneStack), logName)
 
 	return sceneStack.size()
 
 
-## Transitions to the PREVIOUS scene from the [member sceneStack], if any, and returns it.
+## Pushes the current scene's path to [member sceneStack] and returns the stack size.
+func pushCurrentSceneToStack() -> int:  # NOTE: Cannot be `static` because of `self.get_tree()`
+	var currentScene: Node = sceneTree.current_scene
+
+	if not is_instance_valid(currentScene):
+		Debug.printWarning("pushCurrentSceneToStack(): No valid current scene", logName)
+		return sceneStack.size()
+
+	var currentScenePath: String = currentScene.scene_file_path
+
+	if not currentScenePath.is_empty():
+		pushSceneToStack(currentScenePath)
+	else:
+		Debug.printWarning("pushCurrentSceneToStack(): Cannot get path for current scene", logName)
+
+	return sceneStack.size()
+
+
+## Transitions to the PREVIOUS scene from the top/end of the [member sceneStack], if any, and returns it.
 ## NOTE: Returns the previous scene from the stack EVEN IF the transition was NOT successful.
-func popSceneFromStack(pauseSceneTree: bool = true) -> PackedScene:  # NOTE: Cannot be `static` because of `self.get_tree()`
-	if sceneStack.size() <= 1: # Can't pop if there is only 1 or fewer scenes on the stack.
-		Debug.printWarning(str("popSceneFromStack(): No previous scene on sceneStack! size: ", sceneStack.size()), logName)
+func popSceneFromStack(pauseSceneTree: bool = true, animate: bool = true) -> PackedScene:  # NOTE: Cannot be `static` because of `self.get_tree()`
+
+	if sceneStack.is_empty(): # Can't pop if there are no scenes on the stack.
+		Debug.printWarning("popSceneFromStack(): sceneStack is empty!", logName)
 		return null
 
-	# Get the last scene from the stack
-	var previousSceneFromStack: PackedScene = sceneStack.pop_back() # NOTE: PERFORMANCE: Don't use pop_front() because of slower performance.
-	self.transitionToScene(previousSceneFromStack, pauseSceneTree)
+	# Get the previous scene from the top of the stack
+
+	var previousScenePathFromStack: String  = sceneStack.pop_back() # NOTE: PERFORMANCE: Don't use pop_front() because of slower performance.
+	var previousSceneFromStack: PackedScene = load(previousScenePathFromStack)
+
+	if previousSceneFromStack:
+		Debug.printAutoLoadLog(str("popSceneFromStack() → ", previousScenePathFromStack, " → stack size: ", sceneStack.size()))
+	else:
+		Debug.printError("popSceneFromStack(): Cannot load path: " + previousScenePathFromStack, logName)
+
+	await self.transitionToScene(previousSceneFromStack, pauseSceneTree, animate)# IMPORTANT: await for animations
 
 	# Verify the transition
-	
-	var sceneAfterTransition: Node = self.get_tree().current_scene
 
-	if sceneAfterTransition == previousSceneFromStack:
-		Debug.printLog(str("sceneStack.size: ", sceneStack.size()),  logName)
-	else:
-		Debug.printWarning(str("SceneTree.current_scene: ", sceneAfterTransition, " != previousSceneFromStack: ", previousSceneFromStack), logName)
-	
+	var scenePathAfterTransition: String = sceneTree.current_scene.scene_file_path
+
+	if not scenePathAfterTransition == previousScenePathFromStack:
+		Debug.printWarning(str("SceneTree.current_scene.scene_file_path: ", scenePathAfterTransition, " != previousScenePathFromStack: ", previousScenePathFromStack), logName)
+
 	return previousSceneFromStack
 
 
 ## Sets [member SceneTree.paused] and returns the resulting paused status.
 func setPause(paused: bool) -> bool: # NOTE: Cannot be `static` because of `self.get_tree()`
-	var sceneTree: SceneTree = self.get_tree()
 	sceneTree.paused = paused
 
 	GlobalOverlay.showPauseVisuals(sceneTree.paused)
@@ -95,6 +135,6 @@ func setPause(paused: bool) -> bool: # NOTE: Cannot be `static` because of `self
 ## Toggles [member SceneTree.paused] and returns the resulting paused status.
 func togglePause() -> bool: # NOTE: Cannot be `static` because of `self.get_tree()`
 	# TBD: Should this be more efficient instead of so many function calls?
-	return setPause(not self.get_tree().paused)
+	return setPause(not sceneTree.paused)
 
 #endregion
