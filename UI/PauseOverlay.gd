4fb940093f6d2135c7d55b8d6d70f9116c8e34a5
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-02-05 18:23:14 +0700

    Add Restart button to PauseOverlay

--- UI/PauseOverlay.gd
+++ UI/PauseOverlay.gd
@@ -10,9 +10,14 @@ extends Control
 
 
 func _ready() -> void:
-	%TitleLabel.text = ProjectSettings.get_setting("application/config/name", "COMEDOT")
-	%SubtitleLabel.text = ProjectSettings.get_setting("application/config/description", "")
+	%TitleLabel.text	= ProjectSettings.get_setting("application/config/name", "COMEDOT")
+	%SubtitleLabel.text	= ProjectSettings.get_setting("application/config/description", "")
 
 
 func _process(_delta: float) -> void:
 	%TimeLabel.text = str("Time Launched: %1.1f" % (Time.get_ticks_msec() / 1000.0))
+
+
+func onRestartButton_longPressed() -> void:
+	SceneManager.setPause(false)
+	GameState.restart()
