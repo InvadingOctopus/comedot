d40d1f3d5cb394f5d635d81a2b86d433a296c7ad
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-02-18 22:17:39 +0700

    Improve PlayerSpawnPosition & camera support
    * Fix default position. How did that happen??
    + Add shouldCreateCamera, setCamera()
    + Add class_name to support subclasses or dynamic creation
    + Add logging
    
    THANKS: @datouzhu125@GitHub

--- Scenes/Gameplay/PlayerSpawnPosition.gd
+++ Scenes/Gameplay/PlayerSpawnPosition.gd
@@ -1,5 +1,10 @@
 ## Sets the position of the specified player [Entity] to itself on [method _ready].
+## ALERT: If the player entity has a [Camera2D] node that is saved in the scene at a different position,
+## the camera will "jump" to this [PlayerSpawnPosition] if the player is not [method _ready] before this script runs.
+## To avoid, check the order of nodes in the scene tree, or enable [member Camera2D.position_smoothing_enabled] for a slower panning effect,
+## or delete the player's initial [Camera2D] and enable the [member shouldCreateCamera] option.
 
+class_name PlayerSpawnPosition
 extends Marker2D
 
 
@@ -13,15 +18,25 @@ extends Marker2D
 ## NOTE: Overrides [member playerIndex].
 @export var playerOverride: Entity
 
-@onready var playerSpawnPosition: Marker2D = self
+## If `true` and if the player entity doesn't have a [Camera2D] node, a new camera will be created and added to the player.
+## TIP: This option may resolve jumps between the difference in the player's initial position as saved in the scene, and this spawn point, if the player is not [method _ready] before this script.
+@export var shouldCreateCamera: bool = false
+
+@export var shouldShowDebugInfo: bool = false
 
 #endregion
 
+
+#region State
 
 var player: PlayerEntity:
 	get:
 		if playerOverride: return playerOverride
 		else: return GameState.getPlayer(playerIndex)
+
+@onready var playerSpawnPosition: Marker2D = self
+
+#endregion
 
 
 func _ready() -> void:
@@ -35,4 +50,19 @@ func onGameState_playerReady(newPlayer: Entity) -> void:
 
 
 func setPlayerPosition() -> void:
-	if player: player.global_position = playerSpawnPosition.global_position
+	if player:
+		if shouldShowDebugInfo: Debug.printDebug(str("setPlayerPosition(): ", player.global_position, " → ", playerSpawnPosition.global_position), self)
+		player.global_position = playerSpawnPosition.global_position
+		if shouldCreateCamera: setCamera()
+
+
+## Creates a [Camera2D] if the player entity does not already have one, or just returns the existing camera.
+## This avoids any jumps between different initial positions. See the [member shouldCreateCamera] flag.
+func setCamera() -> Camera2D:
+		var camera: Camera2D = player.findFirstChildOfType(Camera2D)
+		if shouldCreateCamera and camera == null:
+			camera = Camera2D.new()
+			Tools.addChildAndSetOwner(camera, player)
+			if shouldShowDebugInfo: Debug.printDebug(str("setCamera() new: ", camera), self)
+		
+		return camera
