5d2772a8f98ebcfc6a52d2f0c103ee6b5c657961
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-02-25 01:59:51 +0700

    Avoid annoying Godot Timer error
    * Add checks for validating minimum cooldown
    * Skip Timer if cooldown is too low

--- Components/Gameplay/CooldownComponent.gd
+++ Components/Gameplay/CooldownComponent.gd
@@ -11,12 +11,25 @@ extends Component
 
 #region Parameters
 
+## The value that [member Timer.wait_time] will be reset to if it goes <= 0
+## According to Godot documentation, it should be 0.05
+const minimumTimerWaitTime: float = 0.05
+
 ## Number of seconds after shooting before another bullet can be emitted.
-## WARNING: A value of 0.0 may cause issues with [Timer].
+## WARNING: A value of 0.0 may cause issues with [Timer]
 @export_range(0.05, 100, 0.05, "suffix:seconds") var cooldown: float = 3:
 	set(newValue):
-		cooldown = newValue
-		if cooldownTimer: cooldownTimer.wait_time = newValue
+		if is_zero_approx(newValue) or newValue < 0:
+			newValue = 0
+
+		if newValue != cooldown:
+			cooldown = newValue
+			if cooldownTimer: 
+				if newValue > 0 and not is_zero_approx(newValue): # Avoid the annoying Godot error: "Time should be greater than zero."
+					cooldownTimer.wait_time = newValue
+				else:
+					cooldownTimer.wait_time = minimumTimerWaitTime # HACK: TODO: Find a better way
+					cooldownTimer.stop()
 
 #endregion
 
@@ -39,9 +52,12 @@ signal didFinishCooldown
 func startCooldown(overrideTime: float = self.cooldown) -> void:
 	printDebug(str("startCooldown() cooldown: ", self.cooldown, ", previous Timer.wait_time: ", cooldownTimer.wait_time, " → overrideTime: ", overrideTime))
 	hasCooldownCompleted = false
-	cooldownTimer.wait_time = overrideTime
-	cooldownTimer.start(overrideTime)
-	didStartCooldown.emit()
+	if overrideTime > 0 and not is_zero_approx(overrideTime): # Avoid the annoying Godot error: "Time should be greater than zero."
+		cooldownTimer.wait_time = overrideTime
+		cooldownTimer.start(overrideTime)
+		didStartCooldown.emit()
+	else: # If the time is too low, just go straight to the finish
+		finishCooldown()
 
 
 func finishCooldown() -> void:
