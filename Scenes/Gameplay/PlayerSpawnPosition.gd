34202cebb589b75935ce1776c3a4ad396d571696
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-02-28 14:46:40 +0700

    Update PlayerSpawnPosition to use CameraComponent

--- Scenes/Gameplay/PlayerSpawnPosition.gd
+++ Scenes/Gameplay/PlayerSpawnPosition.gd
@@ -1,8 +1,8 @@
 ## Sets the position of the specified player [Entity] to itself on [method _ready].
-## ALERT: If the player entity has a [Camera2D] node that is saved in the scene at a different position,
+## ALERT: If the player entity has a [Camera2D] or [CameraComponent] node that is saved in the scene at a different position,
 ## the camera will "jump" to this [PlayerSpawnPosition] if the player is not [method _ready] before this script runs.
 ## To avoid, check the order of nodes in the scene tree, or enable [member Camera2D.position_smoothing_enabled] for a slower panning effect,
-## or delete the player's initial [Camera2D] and enable the [member shouldCreateCamera] option.
+## or delete the player's initial camera and enable the [member shouldCreateCamera] option.
 
 class_name PlayerSpawnPosition
 extends Marker2D
@@ -18,8 +18,8 @@ extends Marker2D
 ## NOTE: Overrides [member playerIndex].
 @export var playerOverride: Entity
 
-## If `true` and if the player entity doesn't have a [Camera2D] node, a new camera will be created and added to the player.
-## TIP: This option may resolve jumps between the difference in the player's initial position as saved in the scene, and this spawn point, if the player is not [method _ready] before this script.
+## If `true` and if the player entity doesn't have a [Camera2D] node, a new [CameraComponent] will be created and added to the player.
+## TIP: This option avoids the screen view jumping between the difference in the player's initial position as saved in the scene, and this spawn point, if the player is not [method _ready] before this script.
 @export var shouldCreateCamera: bool = false
 
 @export var debugMode: bool = false
@@ -39,6 +39,11 @@ var player: PlayerEntity:
 #endregion
 
 
+#region Dependencies
+const cameraComponentPath: String = "res://Components/Visual/CameraComponent.tscn"
+#endregion
+
+
 func _ready() -> void:
 	if player: setPlayerPosition()
 	else: GameState.playerReady.connect(self.onGameState_playerReady) # If there is no player yet, wait for one to be ready
@@ -51,18 +56,17 @@ func onGameState_playerReady(newPlayer: Entity) -> void:
 
 func setPlayerPosition() -> void:
 	if player:
-		if debugMode: Debug.printDebug(str("setPlayerPosition(): ", player.global_position, " → ", playerSpawnPosition.global_position), self)
+		if debugMode: Debug.printDebug(str("setPlayerPosition(): ", player.logName, " ", player.global_position, " → ", playerSpawnPosition.global_position), self)
 		player.global_position = playerSpawnPosition.global_position
 		if shouldCreateCamera: setCamera()
 
 
-## Creates a [Camera2D] if the player entity does not already have one, or just returns the existing camera.
+## Creates a [CameraComponent] if the player entity does not already have a camera, or just returns the existing camera.
 ## This avoids any jumps between different initial positions. See the [member shouldCreateCamera] flag.
 func setCamera() -> Camera2D:
-		var camera: Camera2D = player.findFirstChildOfType(Camera2D)
+		var camera: Node = player.findFirstChildOfAnyTypes([Camera2D, CameraComponent], false) # !returnEntityIfNoMatches
 		if shouldCreateCamera and camera == null:
-			camera = Camera2D.new()
-			Tools.addChildAndSetOwner(camera, player)
+			camera = player.addNewComponent(CameraComponent)
 			if debugMode: Debug.printDebug(str("setCamera() new: ", camera), self)
 		
 		return camera
