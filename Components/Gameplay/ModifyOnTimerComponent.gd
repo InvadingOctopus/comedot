4cfe9975c8bc952d619c602d4f3860dda6a03c19
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-03-04 05:17:30 +0700

    Add ModifyOnTimerComponent usage example
    TBD: Should this be part of the HowTo.md file?

--- Components/Gameplay/ModifyOnTimerComponent.gd
+++ Components/Gameplay/ModifyOnTimerComponent.gd
@@ -3,6 +3,10 @@
 ## [method removeEntity] if [member shouldRemoveEntity] → [method removeNodes] → [method removeComponents] → [method createComponents]
 ## To use a different time for each of those tasks, enable `Editable Children` and disable `$InternalTimer`, then connect any other [Timer] to any of those methods.
 ## TIP: To add/remove nodes based on physics collisions, use [ModifyOnCollisionComponent]
+## TIP: EXAMPLE: For entities like arrows that should get stuck in walls etc. then be removed after a few seconds,
+## use a [ModifyOnCollisionComponent] to add a [ModifyOnTimerComponent] to the entity,
+## then connect the [signal ModifyOnCollisionComponent.didEnterBody] to the $InternalTimer of [ModifyOnTimerComponent] and choose the Timer's [method Timer.start] method,
+## and UNBIND 1 signal argument so that the collision event's `body` argument does not get passed to the Timer's `time` float parameter.
 
 class_name ModifyOnTimerComponent
 extends Component
