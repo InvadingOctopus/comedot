015ba90d0a6fef51f86f17463a5dc035746109fd
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-03-07 02:10:27 +0700

    Allow multiple Stats in StatModifierComponent

--- Components/Data/StatModifierComponent.gd
+++ Components/Data/StatModifierComponent.gd
@@ -1,4 +1,4 @@
-## Modifies a [Stat] over time at the interval specified on the child [Timer].
+## Modifies [Stat] Resources over time at the interval specified on the child [Timer].
 ## TIP: Useful for regenerating a Health [Stat] or dealing poison damage-over-time, etc.
 ## For example, you may connect the [signal ShieldedHealthComponent.shieldDidDecrease] to [method Timer.start] on this [StatModifierComponent].
 ## TIP: Rename this component's node to "ManaRegenerationComponent" etc. to keep track of its purpose in the Scene Tree.
@@ -9,12 +9,16 @@ extends Component
 
 
 #region Parameters
-@export var stat: Stat
-@export_range(-10.0, 10.0, 0.5, "or_greater", "or_less") var modifier: int # HACK: A step of 0.5 to forcibly show the slider :')
+@export var statsToModify: Dictionary[Stat, int] ## A [Dictionary] where the keys are [Stat] Resources and the values are the positive or negative modifier to apply to that respective Stat.
 @export var isEnabled: bool = true
 #endregion
 
 
 func onTimerTimeout() -> void:
 	if not isEnabled: return
-	stat.value += modifier
+	modifyStats()
+
+
+func modifyStats() -> void:
+	for stat in statsToModify:
+		stat.value += statsToModify[stat]
