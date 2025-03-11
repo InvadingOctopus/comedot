16b6e3830ba70ce79201239675b0e749ab976881
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-03-11 07:37:01 +0700

    Refactor SpawnTimer to Spawner base class
    ! Change sceneToSpawn to String path
    * TBD: Is this too much abstraction? Will it cause performance or other issues?

--- Scenes/Gameplay/SpawnTimer.gd
+++ Scenes/Gameplay/SpawnTimer.gd
@@ -1,111 +1,46 @@
-## A [Timer] that creates copies of the specified Scene at regular intervals.
+## A [Timer] that creates copies of a specified Scene at regular intervals.
 
 class_name SpawnTimer
-extends Timer
+extends Spawner
 
 
 #region Parameters
 
-@export var sceneToSpawn: PackedScene
+## If `true` then a copy of [member sceneToSpawn] is spawned as soon as this spawner node os [method Node._ready] and then the [Timer] is started.
+@export var shouldSpawnOnReady: bool = false
 
-## The parent node to add the new spawns to. If `null`, the spawns will be added as children of this area.
-@export var parentOverride: Node
+## Stops the [Timer] when set to `false`
+# @export var isEnabled: bool = true: # UNUSED: sigh GDScript can't override setters/getters :(
+# 	set(newValue):
+# 		if newValue != isEnabled:
+# 			isEnabled = newValue
+# 			self.paused = not isEnabled
+# 			if self.is_node_ready():
+# 				if isEnabled: selfAsTimer.start()
+# 				else: selfAsTimer.stop()
 
-## An optional group to add the spawned nodes to.
-@export var groupToAddTo: StringName
-
-## Maintains a counter and stops spawning nodes when the maximum number is reached.
-## NOTE: Does NOT monitor the deletion of previous nodes; so the counter never decreases. Use [member maximumLimitInGroup] to maintain a specific amount of nodes currently in the scene.
-## If this value is -1 or any other negative number, then it is ignored. CAUTION: Spawning nodes infinitely will eventually cause system slowdown and a crash.
-## Supercedes [member maximumLimitInGroup]
-@export var maximumTotalToSpawn: int = -1
-
-## Stops spawning nodes if [member groupToAddTo] has the specified amount of members.
-## If this value is -1 or any other negative number, then it is ignored. CAUTION: Spawning nodes infinitely will eventually cause system slowdown and a crash.
-## NOTE: [member maximumTotalToSpawn] supersedes this value and is checked first.
-@export var maximumLimitInGroup: int = -1
-
-## Stops the [Timer] when set to `false`.
-@export var isEnabled: bool = true: 
-	set(newValue):
-		if newValue != isEnabled:
-			isEnabled = newValue
-			self.paused = not isEnabled
-			if not isEnabled: self.stop()
-
-@export var debugMode: bool = false
 #endregion
 
 
 #region State
-var totalNodesSpawned: int
+@onready var selfAsTimer: Timer = self.get_node(^".") as Timer
 #region
 
 
-#region Signals
-signal didSpawn(newSpawn: Node2D, parent: Node2D)
-#endregion
+func _ready() -> void:
+	if shouldSpawnOnReady:
+		selfAsTimer.stop() # Stop the Timer
+		spawn.call_deferred() # Defer to avoid the error: "Parent node is busy setting up children, `add_child()` failed."
+
+	Tools.reconnectSignal(selfAsTimer.timeout, self.onTimeout)
+	selfAsTimer.start() # Start the Timer after the initial spawn
 
 
 func onTimeout() -> void:
 	spawn()
 
 
-## Returns: The newly spawned node
 func spawn() -> Node2D:
 	if not isEnabled: return null
+	return super.spawn()
 
-	# Validate
-
-	# <0 is ignored
-	if maximumTotalToSpawn >= 0 \
-	and totalNodesSpawned >= maximumTotalToSpawn:
-		if debugMode: Debug.printDebug(str("totalNodesSpawned: ", totalNodesSpawned, " >= maximumTotalToSpawn: ", maximumTotalToSpawn), self)
-		return null
-
-	# <0 is ignored
-	if maximumLimitInGroup >= 0 \
-	and not groupToAddTo.is_empty():
-		var groupCount: int = self.get_tree().get_node_count_in_group(groupToAddTo)
-		if groupCount >= maximumLimitInGroup:
-			if debugMode: Debug.printDebug(str("maximumLimitInGroup: ", maximumLimitInGroup, " >= nodes in ", groupToAddTo, ": ", groupCount), self)
-			return null
-
-	if not sceneToSpawn:
-		Debug.printWarning("No sceneToSpawn", self)
-		return
-
-	# Load
-
-	var sceneResource   := load(sceneToSpawn.resource_path)
-	var newSpawn: Node2D = sceneResource.instantiate()
-
-	# Add the new node to the parent
-
-	var parent: Node
-
-	if not parentOverride: parent = self.get_parent() # The [Timer]'s parent because [Timer] is not a [Node2D]
-	else: parent = parentOverride
-
-	# Let the game-specific subclasses, if any, customize the new copies.
-
-	if validateNewNode(newSpawn, parent):
-
-		if not groupToAddTo.is_empty():
-			newSpawn.add_to_group(groupToAddTo, true)
-
-		parent.add_child(newSpawn)
-		if newSpawn.get_parent() == parent: # NOTE: Make sure the new node has not been reparented during its `_ready()`
-			newSpawn.owner = parent # INFO: Necessary for persistence to a [PackedScene] for save/load.
-		totalNodesSpawned += 1
-		didSpawn.emit(newSpawn, parent)
-		return newSpawn
-	else:
-		return null
-
-
-## A method for subclasses to override. Prepares newly spawned node with further game-specific logic.
-## May suppress the creation of a newly spawned node by checking additional conditions and returning `false`.
-@warning_ignore("unused_parameter")
-func validateNewNode(newSpawn: Node2D, parent: Node) -> bool:
-	return isEnabled
---
+++ Scenes/Gameplay/Spawner.gd
@@ -0,0 +1,119 @@
+## Node that creates copies of a specified Scene as new children of itself or another node as their parent.
+## NOTE: To actually spawn anything, some other script or Signal must call [member spawn]
+## TIP: Use subclasses such as [SpawnTimer] or [RandomSpawnTimer] to spawn monsters or collectibles etc. at regular intervals.
+
+class_name Spawner
+extends Node
+
+# TBD: Rename to SpawnerBase and designate as a base class?
+
+
+#region Parameters
+
+## The path of the Scene to spawn copies of.
+@export_file("*.tscn") var sceneToSpawn: String # DESIGN: A String instead of PackedScene to avoid loading until needed, right?
+
+## The parent node to add the new spawns to. If `null`, the spawns will be added as children of this area.
+@export var parentOverride:	Node
+
+## An optional group to add the spawned nodes to.
+@export var groupToAddTo:	StringName
+
+## Maintains a counter and stops spawning nodes when the maximum number is reached.
+## NOTE: Does NOT monitor the deletion of previous nodes; so the counter never decreases. Use [member maximumLimitInGroup] to maintain a specific amount of nodes currently in the scene.
+## If this value is -1 or any other negative number, then it is ignored. CAUTION: Spawning nodes infinitely will eventually cause system slowdown and a crash.
+## Supersedes [member maximumLimitInGroup]
+@export var maximumTotalToSpawn: int = -1
+
+## Stops spawning nodes if [member groupToAddTo] has the specified amount of members.
+## If this value is -1 or any other negative number, then it is ignored. CAUTION: Spawning nodes infinitely will eventually cause system slowdown and a crash.
+## NOTE: [member maximumTotalToSpawn] supersedes this value and is checked first.
+@export var maximumLimitInGroup: int = -1
+
+@export var debugMode: bool = false
+
+@export var isEnabled: bool = true
+
+#endregion
+
+
+#region State
+var totalNodesSpawned: int
+#region
+
+
+#region Signals
+
+## Emitted before [member sceneToSpawn] is loaded and a copy is instantiated.
+## TIP: This allows a signal handler to conditionally choose a different scene if needed.
+signal willSpawn(scenePathToSpawn: String)
+
+signal didSpawn(newSpawn: Node2D, parent: Node2D)
+
+#endregion
+
+
+## Creates and returns a new instance of [member sceneToSpawn]
+func spawn() -> Node2D:
+	# Validate first...
+
+	if sceneToSpawn.is_empty():
+		Debug.printWarning("No sceneToSpawn", self)
+		return null
+	
+	# NOTE: <0 is ignored
+	if maximumTotalToSpawn >= 0 \
+	and totalNodesSpawned >= maximumTotalToSpawn:
+		if debugMode: Debug.printDebug(str("totalNodesSpawned: ", totalNodesSpawned, " >= maximumTotalToSpawn: ", maximumTotalToSpawn), self)
+		return null
+
+	# NOTE: <0 is ignored
+	if maximumLimitInGroup >= 0 \
+	and not groupToAddTo.is_empty():
+		var groupCount: int = self.get_tree().get_node_count_in_group(groupToAddTo)
+		if groupCount >= maximumLimitInGroup:
+			if debugMode: Debug.printDebug(str("maximumLimitInGroup: ", maximumLimitInGroup, " >= nodes in ", groupToAddTo, ": ", groupCount), self)
+			return null
+
+	# NOTE: Emit the `will` signal before loading the scene path,
+	# in case a signal handler might want to modify `sceneToSpawn`
+	willSpawn.emit(sceneToSpawn)
+
+	# Load
+
+	var sceneResource   := load(sceneToSpawn)
+	var newSpawn: Node2D = sceneResource.instantiate()
+
+	if not newSpawn:
+		Debug.printWarning("Unable to instantiate scene: " + sceneToSpawn, self)
+		return null
+	
+	# Add the new node to the parent
+	
+	var parent: Node
+
+	if not parentOverride: parent = self.get_parent() # The [Timer]'s parent because [Timer] is not a [Node2D]
+	else: parent = parentOverride
+
+	# Let the game-specific subclasses, if any, customize the new copies.
+
+	if validateNewNode(newSpawn, parent):
+
+		if not groupToAddTo.is_empty():
+			newSpawn.add_to_group(groupToAddTo, true)
+
+		parent.add_child(newSpawn)
+		if newSpawn.get_parent() == parent: # NOTE: Make sure the new node has not been reparented during its `_ready()`
+			newSpawn.owner = parent # INFO: Necessary for persistence to a [PackedScene] for save/load.
+		totalNodesSpawned += 1
+		didSpawn.emit(newSpawn, parent)
+		return newSpawn
+	else:
+		return null
+
+
+## A method for subclasses to override. Prepares newly spawned node with further game-specific logic.
+## May suppress the creation of a newly spawned node by checking additional conditions and returning `false`.
+@warning_ignore("unused_parameter")
+func validateNewNode(newSpawn: Node2D, parent: Node) -> bool:
+	return isEnabled
