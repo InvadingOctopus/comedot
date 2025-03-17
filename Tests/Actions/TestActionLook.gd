cc12d7774142c4e61c4448cb59d2b8a2eb6f1212
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-03-17 07:59:35 +0700

    Remove !

--- Tests/Actions/TestActionLook.gd
+++ Tests/Actions/TestActionLook.gd
@@ -5,5 +5,5 @@ extends GDScript
 
 static func onPayload_didExecute(_payload: Payload, _source: Entity, target: Entity) -> Variant:
 	# Function entry logging done in ScriptPayload.executeImplementation()
-	GlobalUI.createTemporaryLabel(str("It looks like a ", target, "!"))
+	GlobalUI.createTemporaryLabel(str("It looks like a ", target))
 	return true
