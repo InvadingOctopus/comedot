5fba9cf3f02f8b42877419ec299cf480b06a66d9
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-03-20 00:34:29 +0700

    Overhaul ZoneComponent
    * Inherit from AreaContactComponent
    * Simplify & deduplicate code

--- Components/Gameplay/ZoneComponent.gd
+++ Components/Gameplay/ZoneComponent.gd
@@ -1,120 +1,62 @@
-## Monitors an [Area2D] when it enters or exits another [Area2D] belonging to the "zones" group, and maintains a list of overlapping zones.
+## A subclass of [AreaContactComponent] that only monitors [Area2D]s and adds them to separate "zones" list if they belong to the "zones" group.
+## NOTE: The zone [Area2D]s must also have a [CollisionObject2D.collision_layer] matching the [CollisionObject2D.collision_mask] of this component to be detected.
 ## Intended to be subclassed for game-specific functionality.
-## Examples: A "home zone" which heals the player, or an area of fire or poison which causes damage.
+## Examples: A "home zone" which heals the player, or a hazardous region of fire or poison which causes damage.
 
 class_name ZoneComponent
 extends AreaContactComponent
 
+# TODO: Option to choose any Group name.
+
 
 #region Signals
-signal didEnterZone(zoneArea: Area2D) ## Emitted AFTER [signal AreaCollisionComponent.didEnterArea]
-signal didExitZone(zoneArea:  Area2D) ## Emitted AFTER [signal AreaCollisionComponent.didExitArea]
+signal didEnterZone(zoneArea: Area2D) ## Emitted BEFORE [signal AreaCollisionComponent.didEnterArea]
+signal didExitZone(zoneArea:  Area2D) ## Emitted BEFORE [signal AreaCollisionComponent.didExitArea]
 signal didUpdateZones
 #endregion
 
 
 #region State
-## The list of [Area2D]s which belong to the "zones" group and overlap this component's area.
-var currentZones: Array[Area2D]
+var currentZones: Array[Area2D] ## The list of [Area2D]s which belong to the "zones" group and overlap this component's area.
 #endregion
 
 
 func _ready() -> void:
-	super._ready()
 	self.shouldMonitorAreas  = true
 	self.shouldMonitorBodies = false
-	connectSignals()
+	self.shouldConnectSignalsOnReady = true
+	super._ready()
 
 
-## Overrides [method AreaContactComponent.connectSignals] and only monitors for [Area2D]s, regardless of flags.
-func connectSignals() -> void:
-	# TBD: CHECK: Should it CONNECT_PERSIST?
-	Tools.connectSignal(area.area_entered, self.onAreaEntered, CONNECT_PERSIST)
-	Tools.connectSignal(area.area_exited,  self.onAreaExited,  CONNECT_PERSIST)
-	# NOTE: Ignore "bodies"
-
-
-## Overrides [method AreaContactComponent.readdAllContacts] and only adds "zones".
 func readdAllContacts() -> void:
-	# Clear the current lists.
-	areasInContact.clear()
-	bodiesInContact.clear()
+	# Clear our list.
 	currentZones.clear()
-	
-	# Ignore shouldMonitorAreas/shouldMonitorBodies flags 
-	# Allow signal observers to respond to zones already in contact.
-
-	for overlappingArea in selfAsArea.get_overlapping_areas():
-		areasInContact.append(overlappingArea)
-		self.didEnterArea.emit(overlappingArea)
-		
-		if overlappingArea.is_in_group(Global.Groups.zones):
-			currentZones.append(overlappingArea)
-			self.didEnterZone.emit(overlappingArea)
-
-	didUpdateZones.emit()
-	return currentZones.size() # TBD: How does this work with `void`?
+	super.readdAllContacts() # This will call onCollide() to add zones.
+	return currentZones.size() # CHECK: How does this work if super is `-> void` lol?
 
 
-## Overrides [method AreaContactComponent.onAreaEntered] and only adds "zones".
-func onAreaEntered(areaEntered: Area2D) -> void:
-	# Is the area a member of the "zones" group?
-	if not isEnabled or not areaEntered.is_in_group(Global.Groups.zones): return
+#region Events 
 
-	areasInContact.append(areaEntered)
-
-	# Add it to the [currentZones] array if it isn't already in it.
-	if self.currentZones.count(areaEntered) <= 0:
-		currentZones.append(areaEntered)
-		didUpdateZones.emit()
-
-	if debugMode: printDebug(str("onAreaEntered(): ", areaEntered, ", currentZones: ", currentZones.size()))
-	self.onCollide(areaEntered)
-	didEnterArea.emit(areaEntered)
-	didEnterZone.emit(areaEntered)
-
-
-## Overrides [method AreaContactComponent.readdonAreaExitedllContacts] and only adds "zones".
-## NOTE: This is NOT affected by `isEnabled`; zones that exit should ALWAYS be removed!
-func onAreaExited(areaExited: Area2D) -> void:
-	# Is the area a member of the "zones" group?
-	if not areaExited.is_in_group(Global.Groups.zones): return
-	
-	areasInContact.erase(areaExited)
-
-	# Remove it from the [currentZones] array.
-	if self.currentZones.count(areaExited) >= 1:
-		currentZones.erase(areaExited)
-		didUpdateZones.emit()
-
-	if debugMode: printDebug(str("onAreaExited(): ", areaExited, ", currentZones: ", currentZones.size()))
-	self.onExit(areaExited)
-	didExitArea.emit(areaExited)
-	didExitZone.emit(areaExited)
-
-
-#region Abstract Methods
-# NOTE: Ignore the AreaContactComponent implementation because here we only care about zones.
-
-## Abstract; Must be implemented by subclass.
-@warning_ignore("unused_parameter")
 func onCollide(collidingNode: Node2D) -> void:
-	pass
+	if debugMode: printDebug(str("onCollide(): ", collidingNode, ", currentZones: ", currentZones.size()))
+	if collidingNode is Area2D and collidingNode.is_in_group(Global.Groups.zones) and self.currentZones.count(collidingNode) <= 0: # Add it to the [currentZones] array if it isn't already in it.
+		currentZones.append(collidingNode)
+		didUpdateZones.emit()
+		didEnterZone.emit(collidingNode)
 
 
-## Abstract; Must be implemented by subclass.
-@warning_ignore("unused_parameter")
 func onExit(exitingNode: Node2D) -> void:
-	pass
+	# TBD: Check group during removal too? What if group changes after adding?
+	if exitingNode is Area2D and exitingNode.is_in_group(Global.Groups.zones) and self.currentZones.count(exitingNode) >= 1:
+		if debugMode: printDebug(str("onExit(): ", exitingNode, ", currentZones: ", currentZones.size()))
+		currentZones.erase(exitingNode)
+		didUpdateZones.emit()
+		didExitZone.emit(exitingNode)
 
 #endregion
 
 
-#region DEBUG
-
-# func _physics_process(_delta: float) -> void:
-# 	showDebugInfo()
-
+#region Debug
 
 func showDebugInfo() -> void:
 	if not debugMode: return
