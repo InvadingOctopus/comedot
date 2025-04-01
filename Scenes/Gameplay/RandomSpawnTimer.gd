9d198271f05bfe08b20138bfb7d4ceef7f4c8855
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-04-01 13:27:50 +0700

    Add note

--- Scenes/Gameplay/RandomSpawnTimer.gd
+++ Scenes/Gameplay/RandomSpawnTimer.gd
@@ -15,7 +15,7 @@ extends SpawnTimer
 @export_file("*.tscn") var randomScenesList: Array[String]
 
 ## The chance in percentage for an instance to be created whenever the Timer counts down.
-@export_range(0, 100, 1, "suffix:%") var spawnChance: int = 100
+@export_range(0, 100, 1, "suffix:%") var spawnChance: int = 100 # TBD: Should this be a float 0.0 to 1.0? or will that cause float comparison effery?
 
 #endregion
 
