f7c971d19c44ebebe337131fcc09d5567e427b7c
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-04-02 05:14:04 +0700

    Add TileMapCellData Resource
    * Change TileMapLayerWithCustomCellData to use TileMapCellData
    * TODO: Test

---
+++ Resources/TileMapCellData.gd
@@ -0,0 +1,54 @@
+## Stores custom data during runtime for each cell of a [TileMapLayer].
+## Used by components such as [TileBasedPositionComponent] e.g. to determine whether a cell is currently occupied or its remaining "health" if it's destructible.
+## NOTE: "CELLS" are the individual grid elements, NOT the "TILES";
+## Tiles are the permanent resources in a [TileSet]. A single Tile is used to paint multiple Cells of a [TileMapLayer].
+## [TileSet]s can specify custom data per Tile, but a [TileMapLayer] cannot add custom data per grid Cell without this script.
+## For a standalone [TileMapLayer] with built-in support for custom data, see [TileMapLayerWithCustomCellData]
+
+class_name TileMapCellData
+extends Resource
+
+
+#region Parameters & State
+
+## A Dictionary of Dictionaries. { CellCoordinates : {Key : Value} }
+## Each (x,y) [Vector2i] coordinate key contains a Dictionary of {[StringName]: [Variant]}.
+## NOTE: This data is set dynamically during gameplay by components such as [TileBasedPositionComponent] e.g. to determine whether a cell is currently occupied or not.
+@export_storage var dataDictionary: Dictionary[Vector2i, Dictionary]
+
+@export var debugMode: bool = false
+
+#endregion
+
+
+#region State
+var associatedTileMaps: Array[TileMapLayer] ## The list of [TileMapLayer]s that this data structure represents. Set by [TileBasedPositionComponent] etc.
+#endregion
+
+
+#region Data Interface
+
+func setCellData(coordinates: Vector2i, key: StringName, value: Variant) -> void:
+	if debugMode: Debug.printDebug(str("setCellData() @", coordinates, " ", key, " = ", value), self)
+
+	# NOTE: Do NOT assign an entire dictionary here or that will override all other keys!
+
+	# Get the data dictionary for the cell, or add an empty dictionary.
+	var cellData: Variant = dataDictionary.get_or_add(coordinates, {}) # Cannot type this as a `Dictionary` if the coordinate key is missing :(
+
+	cellData[key] = value
+
+
+func getCellData(coordinates: Vector2i, key: StringName) -> Variant:
+	var cellData: Variant = dataDictionary.get(coordinates) # Cannot type this as a `Dictionary` if the coordinate key is missing :(
+	var value: Variant
+
+	if cellData is Dictionary:
+		value = (cellData as Dictionary).get(key)
+	else:
+		value = null
+
+	if debugMode: Debug.printDebug(str("getCellData() @", coordinates, " ", key, ": ", value), self)
+	return value
+
+#endregion
--- Scripts/Data/TileMapLayerWithCustomCellData.gd
+++ Scripts/Data/TileMapLayerWithCustomCellData.gd
@@ -1,7 +1,5 @@
-## Stores custom data during runtime for the individual cells of a [TileMapLayer].
-## NOTE: "CELLS" are the individual grid elements, NOT the "TILES";
-## Tiles are the permanent resources in a [TileSet]. A single Tile is used to paint multiple Cells of a [TileMapLayer].
-## [TileSet]s can specify custom data per Tile, but a [TileMapLayer] cannot add custom data per grid Cell without this script.
+## A [TileMapLayer] with a [TileMapCellData] Resource to store custom data during runtime for individual cells,
+## such as whether a cell is occupied by an Entity or its "health" if it's destructible etc.
 
 class_name TileMapLayerWithCustomCellData
 extends TileMapLayer
@@ -9,34 +7,36 @@ extends TileMapLayer
 
 #region Parameters & State
 
-## A Dictionary of Dictionaries. { CellCoordinates : {Key : Value} }
-## Each (x,y) [Vector2i] coordinate key contains a Dictionary of {[StringName]: [Variant]}.
-@export var dataDictionary: Dictionary[Vector2i, Dictionary] = { Vector2i(0,0): {&"key": 0} }
+@export var cellData: TileMapCellData ## If `null` then a new structure is created on [method _ready]
 
-@export var debugMode: bool = false
+@export var debugMode: bool = false:
+	set(newValue):
+		if newValue != debugMode:
+			debugMode = newValue
+			if cellData: cellData.debugMode = self.debugMode
 
 #endregion
 
 
+func _ready() -> void:
+	if not cellData:
+		if debugMode: Debug.printDebug("No TileMapCellData, creating new.", self)
+		self.cellData = TileMapCellData.new()
+		cellData.debugMode = self.debugMode
+
+
+#region Data Interface
+
 func setCellData(coordinates: Vector2i, key: StringName, value: Variant) -> void:
-	if debugMode: Debug.printDebug(str("setCellData() @", coordinates, " ", key, " = ", value), self)
-
-	# NOTE: Do NOT assign an entire dictionary here or that will override all other keys!
-
-	# Get the data dictionary for the cell, or add an empty dictionary.
-	var cellData: Variant = dataDictionary.get_or_add(coordinates, {}) # Cannot type this as a `Dictionary` if the coordinate key is missing :(
-
-	cellData[key] = value
+	if cellData: cellData.setCellData(coordinates, key, value)
+	else: Debug.printWarning("setCellData(): No TileMapCellData!", self)
 
 
 func getCellData(coordinates: Vector2i, key: StringName) -> Variant:
-	var cellData: Variant = dataDictionary.get(coordinates) # Cannot type this as a `Dictionary` if the coordinate key is missing :(
-	var value: Variant
+	if cellData: 
+		return cellData.getCellData(coordinates, key)
+	else: 
+		Debug.printWarning("getCellData(): No TileMapCellData!", self)
+		return null
 
-	if cellData is Dictionary:
-		value = (cellData as Dictionary).get(key)
-	else:
-		value = null
-
-	if debugMode: Debug.printDebug(str("getCellData() @", coordinates, " ", key, ": ", value), self)
-	return value
+#endregion
