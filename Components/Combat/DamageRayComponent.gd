307ecc723581395110a774282c55f3bc22afc741
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-04-03 14:45:07 +0700

    Fix DamageRayComponent hitting missed hurtboxes

--- Components/Combat/DamageRayComponent.gd
+++ Components/Combat/DamageRayComponent.gd
@@ -8,6 +8,7 @@ class_name DamageRayComponent
 extends DamageComponent
 
 # TODO: Compare performance impact.
+# TODO: Remove excluded hurtboxes after leaving their area, so we can try to roll again when re-entering them.
 # TBD:  Also use [Area2D]-based signals and THEN check the [RayCast2D]?
 # TBD:  Ensure onAreaExited() is called? To remove `damageReceivingComponentsInContact`?
 
@@ -29,6 +30,7 @@ var recentCollidingObject:  Object # PERFORMANCE: Store as a class property to a
 
 func _ready() -> void:
 	super._ready()
+	self.didMiss.connect(onDidMiss)
 	self.set_physics_process(isEnabled)
 
 
@@ -46,3 +48,10 @@ func _physics_process(_delta: float) -> void:
 			selfAsRayCast.add_exception(recentCollidingObject)
 			selfAsRayCast.force_raycast_update()
 		if not shouldSearchForDamageReceiver: break
+
+
+func onDidMiss(damageReceivingComponent: DamageReceivingComponent, _totalChance: int, _roll: int) -> void:
+	# NOTE: Do not retry to hit the same DamageReceivingComponent we just missed our chance at, 
+	# otherwise the chance will be tried EVERY FRAME while this DamageRayComponent remains in the DamageReceivingComponent's hurtbox.
+	selfAsRayCast.add_exception(damageReceivingComponent.area) 
+	# TODO: Remove exception after exiting area
