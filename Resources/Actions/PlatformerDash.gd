cb80e1efdfcb87c4d1caa1a80ad2803f8de4c753
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-04-06 05:45:00 +0700

    Add PlatformerDash Action prototype

---
+++ Resources/Actions/PlatformerDash.gd
@@ -0,0 +1,31 @@
+## A basic "dash" [Action] [ScriptPayload] script for a platformer game.
+## Adds horizontal velocity for a single frame to the [CharacterBodyComponent],
+## based on the recent input direction of a [PlatformerControlComponent].
+## @experimental
+
+extends GDScript
+
+# TODO: Some way to set parameters for static functions :')
+# TODO: Optional invulnerability
+
+
+const force: float = 300
+const shouldApplyVisualEffect: bool = true
+
+
+@warning_ignore("unused_parameter")
+static func onPayload_didExecute(payload: Payload, source: Variant, target: Variant) -> Variant: # `Variant` may be replaced by specific types such as `Entity` or `Component`.
+	if source is Entity:
+		var characterBodyComponent: CharacterBodyComponent = source.components.CharacterBodyComponent
+		var platformerControlComponent: PlatformerControlComponent = source.components.PlatformerControlComponent
+
+		if characterBodyComponent and platformerControlComponent:
+			characterBodyComponent.body.velocity.x += platformerControlComponent.lastInputDirection * force 
+			if shouldApplyVisualEffect:
+				source.modulate = Color(10, 10, 10) # Super white
+				Animations.blinkNode(source, 1, 0.05)
+				Animations.tweenProperty(source, ^"modulate", Color.WHITE, 0.25)
+			return true
+
+	return false
+
