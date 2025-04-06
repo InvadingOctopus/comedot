a41ef7aa77752e91cf674f4a3b79aff1e9b2f0d7
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-04-06 19:21:52 +0700

    Improve OffscreenRemovalComponent
    + Add isEnabled, ignoreIfHidden

--- Components/Visual/OffscreenRemovalComponent.gd
+++ Components/Visual/OffscreenRemovalComponent.gd
@@ -4,12 +4,25 @@
 class_name OffscreenRemovalComponent
 extends Component
 
+
+#region Parameters
+
 ## The time in seconds to wait for before removing the Entity after it goes offscreen. If 0 the removal is instant.
 @export var removalDelay: float = 0
+
+## Ignores [member Node2D.visible] = `false`.
+## By default, a [VisibleOnScreenNotifier2D] considers a node as "exited" if its [member Node2D.visible] is `false`,
+## even if the node's position is still inside the screen.
+## TIP: Enable this flag to avoid unintended removals such as when using blinking animations etc.
+@export var ignoreIfHidden: bool = true
+
+@export var isEnabled: bool = true
+#endregion
 
 
 ## Delete parent node when offscreen
 func onScreenExited() -> void:
+	if not isEnabled or (ignoreIfHidden and not parentEntity.visible): return
 	if removalDelay > 0 and not is_zero_approx(removalDelay): # Just to be safe
 		await self.get_tree().create_timer(removalDelay).timeout
 
