a513a22f02290920b3fa4f15115a20289837940a
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-04-08 22:53:48 +0700

    Add FakeParallax prototype

---
+++ Scripts/Visual/FakeParallax.gd
@@ -0,0 +1,33 @@
+## Moves a node in the opposite horizontal direction of another node.
+## Used to simulate a basic parallax effect. For proper parallax, use [Parallax2D].
+## @experimental
+
+class_name FakeParallax
+extends Node2D
+
+
+#region Parameters
+@export var focalNode:	 Node2D ## The node which acts as the "camera" such as the player sprite. If omitted, the first [member GameState.players] Entity is used.
+@export var scrollScale: float = 1.0
+@export var isEnabled:	 bool  = true
+#endregion
+
+#region State
+var previousFocalPosition: Vector2
+#endregion
+
+
+func _ready() -> void:
+	if not focalNode: focalNode = GameState.getPlayer(0)
+	
+	if focalNode: self.previousFocalPosition = focalNode.global_position
+
+
+func _process(_delta: float) -> void:
+	if not isEnabled: return
+
+	if self.previousFocalPosition != focalNode.global_position:
+		var difference: float = previousFocalPosition.x - focalNode.global_position.x
+		self.position.x += (difference * scrollScale)
+	
+	self.previousFocalPosition = focalNode.global_position
