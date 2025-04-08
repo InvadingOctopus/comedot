a600459541d1e5062656816afd6c37352389ebf3
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-04-08 23:48:30 +0700

    Add Flicker

---
+++ Scripts/Visual/Flicker.gd
@@ -0,0 +1,40 @@
+# Randomly toggles a node's visibility on & off.
+## @experimental
+
+# class_name Flicker
+extends CanvasItem
+
+
+#region Parameters
+
+@export var interval:	float = 0.2 ## The time in seconds before the next attempt to toggle visibility.
+
+@export_range(0, 1.0, 0.01) var hideChance: float = 0.9 ## The chance to disappear every [member interval] seconds, where 1.0 = 100%
+@export_range(0, 1.0, 0.01) var showChance: float = 1.0 ## If hidden, the chance to appear every [member interval] seconds, where 1.0 = 100%
+
+@export var isEnabled:   bool = true:
+	set(newValue):
+		if newValue != isEnabled:
+			isEnabled = newValue
+			self.set_process(isEnabled)
+
+#endregion
+
+
+#region State
+var intervalElapsed: float
+#endregion
+
+
+func _process(delta: float) -> void:
+	# if not isEnabled: return # Set by property setter
+	
+	intervalElapsed += delta
+	if intervalElapsed < interval: return
+	intervalElapsed = 0
+
+	# If hidden, show and go wait for the next disappearance
+	if  self.visible and randf() <= hideChance:
+		self.visible = false
+	elif not self.visible and randf() <= showChance:
+		self.visible = true
