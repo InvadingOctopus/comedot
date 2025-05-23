234fc43e532e6a1f5552a01aa9098150068294a8
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-05-23 11:19:07 +0700

    Improve RidableComponent
    * Rename offset to riderPositionOffset & add default value
    + Add dismountInputEventName
    + Add shouldTogglePause

--- Components/Movement/RidableComponent.gd
+++ Components/Movement/RidableComponent.gd
@@ -1,4 +1,4 @@
-## Allows the parent Entity to be "mounted" and ridden by another Entity, such as a vehicle or horse etc. driven by the player character.
+## Allows the parent Entity to be "mounted" and "ridden" by another Entity, e.g. as a vehicle or horse etc. driven by the player character.
 ## For more basic "attachment" of any node to an Entity, see [AttachmentComponent].
 
 class_name RidableComponent
@@ -19,14 +19,34 @@ extends Component
 			if rider is Entity:
 				didMount.emit(rider)
 
-@export var offset: Vector2
+@export var riderPositionOffset: Vector2 = Vector2(0, -16)
 
-## The list of component types to toggle on the new rider Entity and this component's vehicle/mount Entity. When a rider dismounts, the components are toggled back.
-## [method Entity.toggleComponents] is called to flip the `isEnabled` flag found on most components, and their
-## May be used to hand over player control to a vehicle/mount.
+
+@export_group("Control")
+
+## The component types to toggle on the mount/vehicle and rider entities:
+## On the mount: When mounted, the components are ENABLED. When dismounted, the components are DISABLED.
+## On the rider: When mounted, the components are DISABLED. When the rider dismounts, the components are re-ENABLED.
+## [method Entity.toggleComponents] is called to flip the `isEnabled` flag on each component if available, and pause the disabled components if [member shouldTogglePause].
+## May be used to transfer player control to the vehicle/mount.
 @export var componentTypesToToggle: Array[Script] = [PlatformerControlComponent, JumpControlComponent, ActionsComponent, ActionControlComponent]
 
-@export var isEnabled: bool = true
+## If `true` then [method Entity.toggleComponents] also pauses each component in [member componentTypesToToggle] when it is disabled.
+@export var shouldTogglePause: bool = false
+
+## An optional [InputEventAction] name to let the player manually dismount.
+@export var dismountInputEventName: StringName = "":
+	set(newValue):
+		if newValue != dismountInputEventName:
+			dismountInputEventName = newValue
+			self.set_process_unhandled_input(isEnabled and not dismountInputEventName.is_empty())
+
+@export var isEnabled: bool = true:
+	set(newValue):
+		if newValue != isEnabled:
+			isEnabled = newValue
+			self.set_process_unhandled_input(isEnabled and not dismountInputEventName.is_empty())
+
 #endregion
 
 
@@ -59,6 +79,12 @@ func dismount() -> bool:
 	else:
 		return false
 
+
+func _unhandled_input(event: InputEvent) -> void:
+	if self.isMounted and isEnabled and not dismountInputEventName.is_empty() and event.is_action(dismountInputEventName) and Input.is_action_just_pressed(dismountInputEventName): # Check conditions in the order of most-likely to change
+		self.dismount()
+		self.get_viewport().set_input_as_handled()
+
 #endregion
 
 
@@ -70,28 +96,28 @@ func _ready() -> void:
 
 
 func onSelf_didMount(newRider: Entity) -> void:
-	if debugMode: printDebug(str("onSelf_didMount(): ", newRider, " componentsToToggle: ", componentTypesToToggle))
+	if debugMode: printDebug(str("onSelf_didMount(): ", newRider, " componentsToToggle: ", componentTypesToToggle, ", shouldTogglePause: ", shouldTogglePause))
 	
 	if not isEnabled or componentTypesToToggle.is_empty(): return
 	# Switch components from rider to mount
-	newRider.toggleComponents(componentTypesToToggle, false)
-	self.parentEntity.toggleComponents(componentTypesToToggle, true)
+	newRider.toggleComponents(componentTypesToToggle, false, shouldTogglePause)
+	self.parentEntity.toggleComponents(componentTypesToToggle, true, shouldTogglePause)
 
 
 func onSelf_didDismount(previousRider: Entity) -> void:
-	if debugMode: printDebug(str("onSelf_didDisount(): ", previousRider, " componentsToToggle: ", componentTypesToToggle))
+	if debugMode: printDebug(str("onSelf_didDisount(): ", previousRider, " componentsToToggle: ", componentTypesToToggle, ", shouldTogglePause: ", shouldTogglePause))
 	
 	if not isEnabled or componentTypesToToggle.is_empty(): return
 	# Switch components from mount back to previous rider
-	self.parentEntity.toggleComponents(componentTypesToToggle, false)
-	previousRider.toggleComponents(componentTypesToToggle, true)
+	self.parentEntity.toggleComponents(componentTypesToToggle, false, shouldTogglePause)
+	previousRider.toggleComponents(componentTypesToToggle, true, shouldTogglePause)
 
 
 func _physics_process(_delta: float) -> void: # TBD: _process() or _physics_process()?
 	if not isEnabled or not rider: return
-	# DESIGN: Set the attachee's position to this COMPONENT's position, NOT the Entity's position,
-	# so that there may be an additional constant offset.
-	rider.global_position = self.global_position + offset
+	# DESIGN: Set the attachee's position to this COMPONENT's position, NOT the mount/vehicle Entity's position,
+	# so that there may be an additional constant offset if needed.
+	rider.global_position = self.global_position + riderPositionOffset
 	if is_instance_of(rider, CollisionObject2D):
 		rider.reset_physics_interpolation() # CHECK: Is this necessary?
 
