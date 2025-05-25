a47944f230869ca9dc06b503efe5def7397d8541
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-05-25 16:46:29 +0700

    Overhaul DebugComponent
    * Rename propertiesToWatch to propertiesToLabel
    + Add shouldHideLabelsUntilHover
    + Add Entity name label
    + Improve visibility & readability

--- Components/DebugComponent.gd
+++ Components/DebugComponent.gd
@@ -9,34 +9,63 @@ extends Component
 
 #region Parameters
 
+@export var isEnabled: bool = true:
+	set(newValue):
+		if newValue != isEnabled:
+			isEnabled = newValue
+			self.set_process(isEnabled)
+			if self.is_node_ready(): updateLabelsVisibility()
+
+
+@export_group("Label Overlays")
+
+## A list of [NodePaths] of the nodes & their properties to show [Label]s for.
+## Example: `../HealthComponent:health:value` (use `/` for nodes and `:` for properties)
+@export_node_path var propertiesToLabel: Array[NodePath]:
+	set(newValue):
+		if newValue != propertiesToLabel:
+			propertiesToLabel = newValue
+			self.set_process(not propertiesToLabel.is_empty())
+			if self.is_node_ready(): %PropertiesLabel.visible = not propertiesToLabel.is_empty()
+
+@export var shouldHideLabelsUntilHover: bool = false:
+	set(newValue):
+		if newValue != shouldHideLabelsUntilHover:
+			shouldHideLabelsUntilHover = newValue
+			if self.is_node_ready(): updateLabelsVisibility()
+
+
+@export_group("Chart Windows")
+
 ## A list of [NodePaths] of the nodes & their properties to create [Chart] windows for.
 ## Example: `../CharacterBodyComponent:body:velocity:x` (use `/` for nodes and `:` for properties)
 @export_node_path var propertiesToChart: Array[NodePath]
 
-## A list of [NodePaths] of the nodes & their properties to show [Label]s for.
-## Example: `../HealthComponent:health:value` (use `/` for nodes and `:` for properties)
-@export_node_path var propertiesToWatch: Array[NodePath]
-
 @export_range(100, 200, 5) var chartVerticalHeight: float = 100
 @export_range(0.1, 2.0, 0.05) var chartValueScale:  float = 0.5
 
-@export var isEnabled: bool = true
 #endregion
 
 
 #region State
-@onready var label: Label = %DebugLabel
+@onready var entityLabel:		Label = %EntityLabel
+@onready var propertiesLabel:	Label = %PropertiesLabel
+
 var propertiesToChartAbsolutePaths: Array[NodePath]
-var propertiesToWatchAbsolutePaths: Array[NodePath]
+var propertiesToLabelAbsolutePaths: Array[NodePath]
 #endregion
 
 
 func _ready() -> void:
-	label.visible = isEnabled
+	%PropertiesLabel.self_modulate = self.debugBubbleColor # Differentiate from other DebugComponents
+	updateLabelsVisibility()
 	if not isEnabled: return
 
+	entityLabel.text = parentEntity.name
+	entityLabel.tooltip_text = parentEntity.logFullName
+
 	propertiesToChartAbsolutePaths = convertPathsToAbsolute(propertiesToChart)
-	propertiesToWatchAbsolutePaths = convertPathsToAbsolute(propertiesToWatch)
+	propertiesToLabelAbsolutePaths = convertPathsToAbsolute(propertiesToLabel)
 	createCharts()
 
 
@@ -63,21 +92,52 @@ func createCharts() -> void:
 		newChart.lineColor  = Color(0.2 * randi_range(1, 4), 0.2 * randi_range(1, 5), 0.2 * randi_range(1, 5), 0.5) # Give a distinct random color to each chart
 
 
-func _physics_process(_delta: float) -> void:
+#region Labels
+
+func registerEntity(newParentEntity: Entity) -> void:
+	super.registerEntity(newParentEntity)
+	if self.is_node_ready():
+		entityLabel.text = parentEntity.name
+		entityLabel.tooltip_text = parentEntity.logFullName
+
+
+func updateLabelsVisibility() -> void:
+	# DESIGN: The Entity/Component name labels can be set manually via "Editable Children"
+	$VisibilityToggleHotspot.visible = isEnabled and shouldHideLabelsUntilHover
+	%Labels.visible = isEnabled and not shouldHideLabelsUntilHover
+	%Labels.z_index = 2000 if shouldHideLabelsUntilHover else 100
+	%PropertiesLabel.visible = not propertiesToLabel.is_empty()
+
+
+func _process(_delta: float) -> void:
 	if not isEnabled: return
-	updateLabel()
+	if propertiesToLabel.is_empty(): # In case the array was modified at runtime
+		self.set_process(false)
+		return
+	$VisibilityToggleHotspot.modulate = Color(randf(), randf(), randf())
+	updatePropertiesLabel()
 
 
-func updateLabel() -> void:
+func updatePropertiesLabel() -> void:
 	# TODO: PERFORMANCE: More efficient lookups
 	var nodeAndPropertyPaths: Array[NodePath]
 	var nodeToWatch: Node
-	var labelText: String = ""
+	var propertiesText: String = ""
 
-	for path: NodePath in propertiesToWatchAbsolutePaths:
+	for path: NodePath in propertiesToLabelAbsolutePaths:
 		nodeAndPropertyPaths = Tools.splitPathIntoNodeAndProperty(path)
 		# DEBUG: Debug.printLog(str("path: ", path, ", nodePath: ", nodeAndPropertyPaths[0], ", propertyPath: ", nodeAndPropertyPaths[1]))
-		nodeToWatch = self.get_node(nodeAndPropertyPaths[0]) # TBD: Should it be `self` or the SceneTree root?
-		labelText += str(nodeAndPropertyPaths[1].get_subname(nodeAndPropertyPaths[1].get_subname_count() - 1), ": ", nodeToWatch.get_indexed(nodeAndPropertyPaths[1]), "\n")
+		nodeToWatch = self.get_tree().current_scene.get_node(nodeAndPropertyPaths[0]) # TBD: Should it be `self` or the SceneTree root?
+		propertiesText += str(nodeAndPropertyPaths[1].get_subname(nodeAndPropertyPaths[1].get_subname_count() - 1), ": ", nodeToWatch.get_indexed(nodeAndPropertyPaths[1]), "\n")
 
-	label.text = labelText
+	propertiesLabel.text = propertiesText
+
+
+func onVisibilityToggleHotspot_mouseEntered() -> void:
+	if shouldHideLabelsUntilHover: %Labels.visible = true
+
+
+func onVisibilityToggleHotspot_mouseExited() -> void:
+	if shouldHideLabelsUntilHover: %Labels.visible = false
+
+#endregion
