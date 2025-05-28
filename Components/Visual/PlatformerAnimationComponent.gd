de61719d952e34c9fbe5c40c87e39ef7def5a201
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-05-28 11:17:51 +0700

    Improve PlatformerAnimationComponent performance
    * Flip only when platformerControlComponent.didChangeHorizontalDirection
    * Update per-frame only when needed

--- Components/Visual/PlatformerAnimationComponent.gd
+++ Components/Visual/PlatformerAnimationComponent.gd
@@ -9,8 +9,13 @@ extends Component
 
 
 #region Parameters
+
 ## If omitted, then the parent Entity is used if it is an [AnimatedSprite2D], or then the first matching child node of the parent Entity is used, if any.
-@export var animatedSprite: AnimatedSprite2D
+@export var animatedSprite: AnimatedSprite2D:
+	set(newValue):
+		if newValue != animatedSprite:
+			animatedSprite = newValue
+			self.set_process(isEnabled and is_instance_valid(animatedSprite))
 
 @export var idleAnimation: StringName = &"idle"
 @export var walkAnimation: StringName = &"walk"
@@ -18,7 +23,13 @@ extends Component
 @export var fallAnimation: StringName = &"fall"
 
 @export var flipWhenWalkingLeft: bool = true
-@export var isEnabled: bool = true
+
+@export var isEnabled: bool = true:
+	set(newValue):
+		if newValue != isEnabled:
+			isEnabled = newValue
+			self.set_process(isEnabled and is_instance_valid(animatedSprite)) # PERFORMANCE: Set once instead of every frame
+
 #endregion
 
 
@@ -41,30 +52,41 @@ func _ready() -> void:
 	self.body						= characterBodyComponent.body
 	self.platformerControlComponent	= coComponents.get(&"PlatformerControlComponent") # Optional
 
+	if platformerControlComponent:
+		Tools.connectSignal(platformerControlComponent.didChangeHorizontalDirection, self.onPlatformerControlComponent_didChangeHorizontalDirection)
+
+	self.set_process(isEnabled and is_instance_valid(animatedSprite)) # Apply setters because Godot doesn't on initialization
+
+
+func onPlatformerControlComponent_didChangeHorizontalDirection() -> void:
+	if not isEnabled: return
+	animatedSprite.flip_h = true if signf(platformerControlComponent.inputDirection) < 0 else false # NOTE: Check the CURRENT/most recent input, NOT the [lastInputDirection] because that would be the opposite!
+
 
 func _process(_delta: float) -> void:
-	if not isEnabled or not animatedSprite: return
-
 	# INFO: Animations are checked in order of priority: "walk" overrides "idle"
 
 	var animationToPlay: StringName
 
-	if flipWhenWalkingLeft:
-		if platformerControlComponent:
-			animatedSprite.flip_h = true if signf(platformerControlComponent.lastInputDirection) < 0.0 else false
-		else:
-			animatedSprite.flip_h = true if characterBodyComponent.previousVelocity.x < 0 else false
-		# Debug.watchList.hDirection = platformControlComponent.lastDirection
+	# If there is no PlatformerControlComponent, figure out the direction from the CharacterBodyComponent
+	if not platformerControlComponent and flipWhenWalkingLeft: # Check the rarer flag first, so we don't have to check 2
+		animatedSprite.flip_h = true if signf(characterBodyComponent.previousVelocity.x) < 0 else false
+	# if debugMode and platformerControlComponent: Debug.watchList.hDirection = platformControlComponent.lastDirection
 
+	# Check and set animation in order of lowest priority to highest. e.g. walk overrides idle
+
+	# Are we chilling?
 	if not idleAnimation.is_empty() \
 	and body.velocity.is_zero_approx():
 		animationToPlay = idleAnimation
 
+	# Are we walking?
 	if not walkAnimation.is_empty() \
 	and not body.velocity.is_zero_approx():
 		animationToPlay = walkAnimation
 
-	# Debug.watchList.onFloor = body.is_on_floor()
+	# Are we jumping or falling?
+	# if debugMode: Debug.watchList.onFloor = body.is_on_floor()
 
 	if not characterBodyComponent.isOnFloor:
 		var verticalDirection: float = signf(body.velocity.y)
@@ -76,6 +98,7 @@ func _process(_delta: float) -> void:
 		and verticalDirection > 0.0:
 			animationToPlay = fallAnimation
 
-		# Debug.watchList.vDirection = verticalDirection
+		# if debugMode: Debug.watchList.vDirection = verticalDirection
 
+	# Play the chosen animation
 	animatedSprite.play(animationToPlay)
