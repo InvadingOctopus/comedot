d65b6180cc6ab8f5bf370285e980984d79b45017
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-06-01 19:02:22 +0700

    Update AreaTest

--- Tests/AreaTest.gd
+++ Tests/AreaTest.gd
@@ -1,9 +1,9 @@
 extends Start
 
 
-@onready var area: Area2D = %TestArea
-@onready var shapeNode: CollisionShape2D = %TestShapeNode
-
+@onready var testArea: Area2D = %TestArea
+@onready var testShapeNode: CollisionShape2D = %TestShapeNode
+@onready var areaContactComponent: AreaContactComponent = %AreaContactComponent
 
 func _ready() -> void:
 	super._ready()
@@ -12,17 +12,19 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	Debug.watchList.areaPosition = area.position
-	Debug.watchList.areaGlobalPosition = area.global_position
+	Debug.watchList.areaPosition = testArea.position
+	Debug.watchList.areaGlobalPosition = testArea.global_position
 
-	Debug.watchList.shapeNodePosition = shapeNode.position
-	Debug.watchList.shapeNodeGlobalPosition = shapeNode.global_position
+	Debug.watchList.shapeNodePosition = testShapeNode.position
+	Debug.watchList.shapeNodeGlobalPosition = testShapeNode.global_position
 
-	var shapeBounds: Rect2 = Tools.getShapeBounds(area)
+	var shapeBounds: Rect2 = Tools.getShapeBounds(testArea)
 	Debug.watchList.shapeBounds = shapeBounds
 
-	var shapeBoundsInArea: Rect2 = Tools.getShapeBoundsInArea(area)
+	var shapeBoundsInArea: Rect2 = Tools.getShapeBoundsInArea(testArea)
 	Debug.watchList.shapeBoundsInArea = shapeBoundsInArea
 
-	var shapeGlobalBounds: Rect2 = Tools.getShapeGlobalBounds(area)
+	var shapeGlobalBounds: Rect2 = Tools.getShapeGlobalBounds(testArea)
 	Debug.watchList.shapeGlobalBounds = shapeGlobalBounds
+
+	Debug.watchList.displacementOutsideZone2 = Tools.getRectOffsetOutsideContainer(areaContactComponent.areaBoundsGlobal, Tools.getShapeGlobalBounds(%Zone2))
