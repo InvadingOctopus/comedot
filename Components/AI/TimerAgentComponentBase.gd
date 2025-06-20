50c5ea5525a45d4239795d6cf19413a58eaf6589
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-06-20 21:10:39 +0700

    Revert abstract classes to fix related errors
    TODO: See if this can be solved while retaining `abstract` or will Godot 4.5 fix it in a later beta.

--- Components/AI/TimerAgentComponentBase.gd
+++ Components/AI/TimerAgentComponentBase.gd
@@ -2,7 +2,7 @@
 ## e.g. to have a random chance to attack every N seconds and so on.
 ## @experimental
 
-abstract class_name TimerAgentComponentBase
+class_name TimerAgentComponentBase # NOTE: TBD: Cannot set as `abstract` because it causes an error in Spawner.gd @99: `var newSpawn: Node2D = sceneResource.instantiate()` "Cannot set object script."
 extends Component
 
 
