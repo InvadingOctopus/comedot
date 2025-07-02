9d741c35c577111427a1898302580def6e1d8906
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-07-02 09:48:01 +0700

    Update ChaseComponent
    * Use InputComponent
    * Improve performance

--- Components/Movement/ChaseComponent.gd
+++ Components/Movement/ChaseComponent.gd
@@ -1,22 +1,30 @@
-## Moves the parent entity to chase after another [Node2D] by manipulating the entity's [member OverheadPhysicsComponent.inputDirection].
+## Moves the parent entity to chase after another [Node2D] by manipulating the entity's [member InputComponent.movementDirection].
 ## Speed, acceleration and friction are determined by the [OverheadPhysicsComponent] and its [OverheadMovementParameters].
 ## NOTE: Set [member CharacterBodyComponent.shouldResetVelocityIfZeroMotion] to `false`
 ## TIP:  For more complex pathfinding based on Godot's Navigation nodes, use [NavigationComponent]
 ## Requirements: BEFORE [OverheadPhysicsComponent]
 
 class_name ChaseComponent
-extends CharacterBodyDependentComponentBase
+extends Component
 
 
 #region Parameters
 
 ## If not specified and [member shouldChasePlayerIfUnspecified], then the first [PlayerEntity] from [member GameState.players] will be chased.
-@export var nodeToChase: Node2D
+@export var nodeToChase: Node2D:
+	set(newValue):
+		if newValue != nodeToChase:
+			nodeToChase = newValue
+			self.set_physics_process(isEnabled and is_instance_valid(nodeToChase))
 
 ## If `true` amd [member nodeToChase] is `null`, the first [PlayerEntity] from [member GameState.players] will be chased.
 @export var shouldChasePlayerIfUnspecified: bool = true
 
-@export var isEnabled: bool = true
+@export var isEnabled: bool = true:
+	set(newValue):
+		if newValue != isEnabled:
+			isEnabled = newValue
+			self.set_physics_process(isEnabled and is_instance_valid(nodeToChase))
 
 #endregion
 
@@ -27,11 +35,13 @@ var recentChaseDirection: Vector2
 
 
 #region Dependencies
-@onready var overheadPhysicsComponent: OverheadPhysicsComponent = coComponents.OverheadPhysicsComponent # TBD: Static or dynamic?
+@onready var characterBodyComponent: CharacterBodyComponent = coComponents.CharacterBodyComponent
+@onready var inputComponent: InputComponent = parentEntity.findFirstComponentSubclass(InputComponent)
 
 func getRequiredComponents() -> Array[Script]:
-	return [CharacterBodyComponent, OverheadPhysicsComponent] # Cannot easily join with `super.getRequiredComponents()` :(
+	return [CharacterBodyComponent, InputComponent]
 #endregion
+
 
 
 func _ready() -> void:
@@ -42,20 +52,16 @@ func _ready() -> void:
 	if not nodeToChase and shouldChasePlayerIfUnspecified:
 		nodeToChase = GameState.getPlayer(0)
 
+	self.set_physics_process(isEnabled and is_instance_valid(nodeToChase)) # Apply setters because Godot doesn't on initialization
+
 
 func _physics_process(_delta: float) -> void:
 	# THANKS: GDQuest@YouTube https://www.youtube.com/watch?v=GwCiGixlqiU
-
-	# Check for presence of self and target to account for destroyed entities.
-	if not isEnabled or not self.body or not nodeToChase: return
+	# Check for presence of target to account for destroyed entities.
+	if not is_instance_valid(nodeToChase): return # isEnabled checked by property setter
 
 	self.recentChaseDirection = parentEntity.global_position.direction_to(nodeToChase.global_position).normalized()
-	overheadPhysicsComponent.inputDirection = self.recentChaseDirection
+	inputComponent.movementDirection = self.recentChaseDirection
 
-	# characterBodyComponent.shouldMoveThisFrame = true # Unneeded; will be called by OverheadPhysicsComponent
-	if debugMode: showDebugInfo()
-
-
-func showDebugInfo() -> void:
-	if not debugMode: return
-	Debug.watchList.set(parentEntity.name + "." + self.name, recentChaseDirection)
+	if debugMode:
+		Debug.watchList.set(parentEntity.name + "." + self.name, recentChaseDirection)
