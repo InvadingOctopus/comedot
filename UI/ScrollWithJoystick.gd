136ef04ca3a75db983c295d94d9811bda79bb13c
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-07-02 09:02:21 +0700

    Add Page Up & Down with Gamepad L2 & R2

--- UI/ScrollWithJoystick.gd
+++ UI/ScrollWithJoystick.gd
@@ -5,7 +5,8 @@ extends ScrollContainer
 
 
 #region Parameters
-@export var scrollSpeed: float   = 400.0 ## Pixels per second
+@export var scrollSpeed: float  = 400 ## Pixels per second
+@export var verticalPage:  int	= 400 ## The vertical scroll distance to "jump" when pressing "Page Up" or "Page Down"
 
 ## The amount to multiply each input axis with. 0 = axis disabled. Negative values = inverted scrolling.
 ## TIP: May be used to disable horizontal scrolling etc.
@@ -26,13 +27,26 @@ var scrollDirection: Vector2
 
 
 func _unhandled_input(event: InputEvent) -> void:
+	# TBD: Focus & set_input_as_handled()
+	
 	if event is InputEventJoypadMotion:
 		self.scrollDirection = Input.get_vector(
 			GlobalInput.Actions.aimLeft,
 			GlobalInput.Actions.aimRight,
 			GlobalInput.Actions.aimUp,
 			GlobalInput.Actions.aimDown)
-		self.isScrolling = not self.scrollDirection.is_zero_approx()
+		self.isScrolling  = not self.scrollDirection.is_zero_approx()
+		if self.isScrolling and self.has_focus(): self.get_viewport().set_input_as_handled()
+
+	if event.is_action(&"ui_page_up") \
+	and Input.is_action_just_pressed(&"ui_page_up"):
+		self.scroll_vertical -= verticalPage
+		if self.has_focus(): self.get_viewport().set_input_as_handled()
+
+	elif event.is_action(&"ui_page_down") \
+	and Input.is_action_just_pressed(&"ui_page_down"):
+		self.scroll_vertical += verticalPage
+		if self.has_focus(): self.get_viewport().set_input_as_handled()
 
 
 func _process(delta: float) -> void:
