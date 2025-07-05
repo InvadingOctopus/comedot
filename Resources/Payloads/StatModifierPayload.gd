6056bfc440c5e9816c5eb5328a12dc355f04d427
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-07-06 05:50:18 +0700

    Add StatModifierPayload.shouldDenyIfStatMax

--- Resources/Payloads/StatModifierPayload.gd
+++ Resources/Payloads/StatModifierPayload.gd
@@ -1,5 +1,6 @@
 ## A [Payload] that modifies a set of [Stat]s when executed.
 ## NOTE: The `source` and `target` are ignored/irrelevant; Stats are modified regardless.
+## TIP: Use [method Tools.checkResult] to verify this [Payload]'s success or failure depending on whether any [Stat]s were modified, including skipped [Stat]s because of [member shouldDenyIfStatMax] or 0 modifiers.
 
 class_name StatModifierPayload
 extends Payload
@@ -7,7 +8,8 @@ extends Payload
 
 #region Parameters
 ## A [Dictionary] where the keys are [Stat]s and their values are the positive or negative modifiers to add or subtract from each Stat.
-@export var statsAndModifiers: Dictionary[Stat, int]
+@export var statsAndModifiers:   Dictionary[Stat, int]
+@export var shouldDenyIfStatMax: bool = true
 #endregion
 
 
@@ -24,7 +26,11 @@ func executeImplementation(source: Variant, target: Variant) -> Array[Stat]:
 	var modifiedStats: Array[Stat]
 	for stat in statsAndModifiers:
 		if not statsAndModifiers[stat] == 0:
-			stat.value += statsAndModifiers[stat]
-			modifiedStats.append(stat)
+			if statsAndModifiers[stat] >= 1 and shouldDenyIfStatMax and stat.value >= stat.max:
+				# TODO: Emit bubbles for max Stats
+				continue # Skip if a stat is already maxed
+			else:
+				stat.value += statsAndModifiers[stat]
+				modifiedStats.append(stat)
 
-	return modifiedStats
+	return modifiedStats # NOTE: AWESOME: Thanks to being smart in Tools.checkResult() any empty Arrays will be regarded as a failed Payload :)
