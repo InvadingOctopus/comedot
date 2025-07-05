9574f93d1ada3ec82596be4715acf7c5b60065ec
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-07-05 07:26:07 +0700

    Update notes

--- Components/Control/InputDependentComponentBase.gd
+++ Components/Control/InputDependentComponentBase.gd
@@ -1,5 +1,5 @@
 ## Abstract base class for components whose functionality is dependent on player inout or AI control provided by an [InputComponent] or one of its subclasses.
-## IMPORTANT: [method _ready] MUST be called via `super._ready()` if overridden in a subclass.
+## [method _ready] should be called via `super._ready()` if overridden in a subclass, to automatically setup signals.
 ## TIP: Components where input control is optional such as [PlatformerPhysicsComponent], or components that don't have to handle all input signals, do not need to inherit from this class.
 ## Requirements: BEFORE (above) [InputComponent] or its subclasses, because input events propagate from the BOTTOM of the Scene Tree nodes list UPWARD.
 
@@ -15,12 +15,12 @@ func getRequiredComponents() -> Array[Script]:
 #endregion
 
 
-## IMPORTANT: MUST be called via super._ready() to setup signals
+## Should be called via super._ready() to setup signals.
 func _ready() -> void:
 	# DESIGN: _enter_tree() cannot be used because components that depend on InputComponent may enter the tree before InputComponent
 	if inputComponent:
 		Tools.connectSignal(inputComponent.didProcessInput, self.onInputComponent_didProcessInput)
-		Tools.connectSignal(inputComponent.didUpdateInputActionsList, self.onInputComponent_didUpdateInputActionsList)
+		Tools.connectSignal(inputComponent.didUpdateInputActionsList, self.onInputComponent_didUpdateInputActionsList) # Optional
 	else:
 		printError(str("Missing InputComponent in ", parentEntity.logFullName)) # If a component inherits this class then it means so a missing dependency is an ERROR!
 
