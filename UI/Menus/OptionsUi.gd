4041f6602fb0b3cc6f38af9d3c4fc6483f38fea9
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-07-10 08:21:47 +0700

    Disable SkipMusicButton if no music files

--- UI/Menus/OptionsUi.gd
+++ UI/Menus/OptionsUi.gd
@@ -4,5 +4,22 @@
 extends Container
 
 
+@onready var skipMusicButton: Button = %SkipMusicButton
+
+
+func _ready() -> void:
+	updateSkipMusicButton()
+	Tools.connectSignal(GlobalSonic.musicPlayerDidPlay, self.updateSkipMusicButton)
+	Tools.connectSignal(GlobalSonic.musicPlayerDidStop, self.updateSkipMusicButton)
+
+
+#region Music
+
+func updateSkipMusicButton(_musicFileName: String = "") -> void:
+	skipMusicButton.disabled = GlobalSonic.musicFiles.is_empty()
+
+
 func onSkipMusicButton_pressed() -> void:
 	GlobalSonic.skipMusic()
+
+#endregion
