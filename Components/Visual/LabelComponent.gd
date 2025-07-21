bb0e551ba40d0b4f93991855ae0ae858cb489ba8
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-07-21 08:55:28 +0500

    Overhaul LabelComponent
    - Delete AnimationPlayer
    * Animate via code
    * Change Component root node to Label
    + Add LabelComponent Theme

--- Components/Visual/LabelComponent.gd
+++ Components/Visual/LabelComponent.gd
@@ -1,32 +1,38 @@
-## Displays a text label atop the Entity node with an animation.
+## Displays a text [Label] atop the Entity node with an animation.
+## @experimental
+
 class_name LabelComponent
 extends Component
 
-
-## If unspecified then the parent [Entity]'s node will be used.
-@export var parentNodeOverride: Node2D = null
+# TODO: FIXME: Center the Label alignment on Sprite entities etc.
 
 
-@onready var label: Label = %Label
+#region Parameters
+@export var shouldAnimateOnReady: bool = false ## If `true` then the [Label] is initially hidden then animated as soon as this component is [method _ready]
+#endregion
+
+
+#region State
+@onready var label: Label = self.get_node(^".") as Label
+var tween: Tween
+#endregion
+
 
 func _ready() -> void:
-	# Just in case we left it visible in the editor.
-	label.visible = false
+	if shouldAnimateOnReady:
+		label.visible = false
 
 
-func display(text: String, animation: StringName = Animations.Names.blink) -> void:
+func display(text: String, animationFunction: Callable = Animations.blink) -> void:
+	# TODO: Allow custom arguments for the `animationFunction`
 	label.text = text
-	playAnimation(animation)
+	playAnimation(animationFunction)
 
 
-func blink(text: String) -> void:
-	label.text = text
-	playAnimation("blink")
-
-
-func playAnimation(animationName: StringName) -> void:
-	%AnimationPlayer.stop()
+func playAnimation(animationFunction: Callable) -> void:
+	# TODO: Allow custom arguments for the `animationFunction`
+	if tween: tween.kill()	
 	label.visible = true # Just in case
-	%AnimationPlayer.play(animationName)
-	await %AnimationPlayer.animation_finished
+	tween = animationFunction.call(label)
+	await tween.finished
 	label.visible = false # Just in case
