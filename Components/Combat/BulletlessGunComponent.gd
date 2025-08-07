fe489c240a85d8820ae8dfafd8d6713438b4183c
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-08-08 03:18:37 +0500

    Add BulletlessGunComponent

---
+++ Components/Combat/BulletlessGunComponent.gd
@@ -0,0 +1,68 @@
+## Represents a weapon which does not emit "bullets" or other projectiles, instead causes instant damage at a target position.
+## Other components/scripts may be used to draw "fake" projectiles as a visual indicator of the weapon firing.
+## IMPORTANT: This is NOT a standalone component; it must used with other components such as [DamageComponent] to represent the area of attack,
+## and an [AimingCursorComponent] to control the targetting (or a [TetherComponent] + [PositionControlComponent]).
+## Those components may be combined to make a specialized "GunEntity" etc.
+## @experimental
+
+class_name BulletlessGunComponent
+extends Component
+
+# TODO: Add ammo
+
+
+#region Parameters
+
+## The node to reposition the [DamageComponent] to on each frame.
+## If unspecified, an [AimingCursorComponent] is used, if available.
+@export var targetingNode: Node2D:
+	set(newValue):
+		if newValue != targetingNode:
+			targetingNode = newValue
+			self.set_physics_process(isEnabled and is_instance_valid(targetingNode))
+
+@export var isEnabled: bool = true:
+	set(newValue):
+		if newValue != isEnabled:
+			isEnabled = newValue
+			if not isEnabled: isFiring = false
+			self.set_physics_process(isEnabled and is_instance_valid(targetingNode))
+
+#endregion
+
+
+#region State
+var isFiring: bool = false:
+	set(newValue):
+		if newValue != isFiring:
+			isFiring = newValue
+			damageComponent.isEnabled = self.isFiring
+#endregion
+
+
+#region Dependencies
+@onready var inputComponent:  InputComponent  = parentEntity.findFirstComponentSubclass(InputComponent)
+@onready var damageComponent: DamageComponent = coComponents.DamageComponent
+#endregion
+
+
+func _ready() -> void:
+	if not targetingNode:
+		targetingNode = coComponents.AimingCursorComponent.get_node(^".") as Node2D
+		if not targetingNode: printWarning("targetingNode not set!")
+
+	Tools.connectSignal(inputComponent.didProcessInput, self.onInputComponent_didProcessInput)
+	# Apply setters because Godot doesn't on _ready()
+	damageComponent.isEnabled = self.isFiring
+	self.set_physics_process(isEnabled and is_instance_valid(targetingNode))
+
+
+func onInputComponent_didProcessInput(event: InputEvent) -> void:
+	self.isFiring = isEnabled and event.is_action_pressed(GlobalInput.Actions.fire)
+
+
+func _physics_process(_delta: float) -> void:
+	damageComponent.global_position = targetingNode.global_position
+	damageComponent.reset_physics_interpolation() # CHECK: Necessary?
+
+	if debugMode: targetingNode.modulate = Color.RED if isFiring else Color.GRAY
