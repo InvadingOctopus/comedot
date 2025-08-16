a5af10fa098ebd8a13f9a7f5bcd41df9fa693999
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-08-16 18:59:17 +0500

    Rename skipEmptyCells to shouldSkipEmptyCells

--- Scripts/Visual/RandomizeTileMapCells.gd
+++ Scripts/Visual/RandomizeTileMapCells.gd
@@ -9,15 +9,15 @@ extends TileMapLayer
 ## Overrides [member cellRegionStart] & [member cellRegionEnd].
 ## WARNING: If there are NO "painted" cells in the map, then the size of the map will be (0,0) which means NO cells will be modified!
 ## TIP: To quickly set the "size" of a [TileMapLayer], just place 1 transparent tile on the bottom-right cell.
-@export var shouldUseEntireMap: bool = false
+@export var shouldUseEntireMap: 	bool = false
 
-@export var cellRegionStart:	Vector2i ## The upper-left corner of the rectangle region in the [TileMapLayer] MAP to modify the CELLS in.
-@export var cellRegionEnd:		Vector2i ## The bottom-right corner of the rectangle region in the [TileMapLayer] MAP to modify the CELLS in.
-@export var atlasCoordinatesMin:Vector2i ## The upper-left corner of the rectangle region in the [TileSet] atlas to choose the TILES from.
-@export var atlasCoordinatesMax:Vector2i ## The bottom-right corner of the rectangle region in the [TileSet] atlas to choose the TILES from.
+@export var cellRegionStart:		Vector2i ## The upper-left corner of the rectangle region in the [TileMapLayer] MAP to modify the CELLS in.
+@export var cellRegionEnd:			Vector2i ## The bottom-right corner of the rectangle region in the [TileMapLayer] MAP to modify the CELLS in.
+@export var atlasCoordinatesMin:	Vector2i ## The upper-left corner of the rectangle region in the [TileSet] atlas to choose the TILES from.
+@export var atlasCoordinatesMax:	Vector2i ## The bottom-right corner of the rectangle region in the [TileSet] atlas to choose the TILES from.
 
+@export var shouldSkipEmptyCells:	bool = true ## Leave "unpainted" cells empty?
 @export_range(0, 1.0, 0.01) var modificationChance:	float = 1.0 ## THe chance to modify rolled separately for each cell in the [member cellRegionStart] to [member cellRegionEnd] rectangle.
-@export var skipEmptyCells:		bool = true ## Leave "unpainted" cells empty?
 #endregion
 
 
@@ -31,4 +31,4 @@ func _ready() -> void:
 		else:
 			Debug.printWarning(str("RandomizeTileMapCells.gd: shouldUseEntireMap: Map has cells = no area!"), self)
 
-	Tools.randomizeTileMapCells(self, cellRegionStart, cellRegionEnd, atlasCoordinatesMin, atlasCoordinatesMax, modificationChance, skipEmptyCells)
+	Tools.randomizeTileMapCells(self, cellRegionStart, cellRegionEnd, atlasCoordinatesMin, atlasCoordinatesMax, modificationChance, shouldSkipEmptyCells)
