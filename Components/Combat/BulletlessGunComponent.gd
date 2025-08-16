7c19ca0532fa27150313f925d7550f37836b80f4
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-08-16 06:55:56 +0500

    Overhaul BulletlessGunComponent
    * Inherit from CooldownComponent
    * Implement & improve cooldown & firing
    * Rename ammoCostPerTick to ammoCost
    * Improve ammo usage implementation
    - Remove DamageRepeatingComponent dependency
    + Add dontShootIfNoTargets
    + Add didFire, didDepleteAmmo, ammoInsufficient
    + Add fire(), useAmmo(), finishCooldown()
    - Remove secondElapsed & unused code

--- Components/Combat/BulletlessGunComponent.gd
+++ Components/Combat/BulletlessGunComponent.gd
@@ -1,17 +1,23 @@
 ## Represents a weapon which does not emit "bullets" or other projectiles, instead causes instant damage at a target position.
 ## Other components/scripts may be used to draw "fake" projectiles as a visual indicator of the weapon firing.
 ## IMPORTANT: This is NOT a standalone component; it must used with other components such as [DamageComponent] to represent the area of attack,
-## a [DamageRepeatingComponent] to toggle the [DamageComponent] on & off, 
 ## and an [AimingCursorComponent] to control the targetting (or a [TetherComponent] + [PositionControlComponent]).
 ## Those components may be combined to make a specialized "GunEntity" etc.
-## Requirements: [DamageComponent], [DamageRepeatingComponent], BEFORE [InputComponent]
+## Requirements: [DamageComponent]. BEFORE [InputComponent]
 ## @experimental
 
 class_name BulletlessGunComponent
-extends Component
+extends CooldownComponent
 
 
 #region Parameters
+
+@export var isEnabled: bool = true:
+	set(newValue):
+		if newValue != isEnabled:
+			isEnabled = newValue
+			if not isEnabled: isFiring = false
+			self.set_physics_process(isEnabled and is_instance_valid(targetingNode))
 
 ## The node to reposition the [DamageComponent] to on each frame.
 ## If unspecified, an [AimingCursorComponent] is used, if available.
@@ -21,24 +27,19 @@ extends Component
 			targetingNode = newValue
 			self.set_physics_process(isEnabled and is_instance_valid(targetingNode))
 
-@export var isEnabled: bool = true:
-	set(newValue):
-		if newValue != isEnabled:
-			isEnabled = newValue
-			if not isEnabled: isFiring = false
-			self.set_physics_process(isEnabled and is_instance_valid(targetingNode))
+## If `true` then [method fire] fails if [member DamageComponent.damageReceivingComponentsInContact] is empty.
+@export var dontShootIfNoTargets: bool = false
+
 
 @export_group("Ammo")
-
 @export var ammo: Stat ## The [Stat] Resource to use as the ammo. If omitted, no ammo is required to fire the gun.
-@export var ammoCostPerTick: int = 1 ## The ammo used when initating the firing and then per tick of the [member DamageRepeatingComponent.timer]. 0 == Unlimited ammo. NOTE: A negative number will INCREASE the ammo when firing.
-@export var ammoDepletedMessage: String = "AMMO DEPLETED" ## The text to display via the Entity's [LabelComponent] when the [member ammo] [Stat] reaches 0 after firing.
+@export var ammoCost: int = 1 ## The ammo used when initating the firing and then per tick of the [member DamageRepeatingComponent.timer]. 0 == Unlimited ammo. NOTE: A negative number will INCREASE the ammo when firing.
+@export var ammoDepletedMessage: String = "AMMO DEPLETED" ## The text to display as a [TextBubble] when the [member ammo] [Stat] reaches 0 AFTER firing.
 
 #endregion
 
 
 #region State
-
 var isFiring: bool = false:
 	set(newValue):
 		if newValue != isFiring:
@@ -46,22 +47,25 @@ var isFiring: bool = false:
 				Debug.printChange("isFiring", isFiring, newValue, self.debugModeTrace) # logAsTrace
 				emitDebugBubble("isFiring" if newValue else "!isFiring", randomDebugColor, true) # emitFromEntity
 			isFiring = newValue
-			damageComponent.isEnabled = self.isFiring
-			if isFiring: damageRepeatingComponent.timer.start()
-			else: damageRepeatingComponent.timer.stop()
+			# UNUSED: damageComponent.isEnabled = self.isFiring # Don't disable Area2D events
+			if isFiring and cooldownTimer.is_stopped() and not is_zero_approx(cooldownTimer.time_left):
+				cooldownTimer.start()
+#endregion
 
-var secondElapsed: float = 0 # TBD @export_storage?
 
+#region Signals
+signal didFire(damageReceivingComponentsInContact: Array[DamageReceivingComponent])
+signal didDepleteAmmo  ## Emitted when [member ammo] goes below 1 after firing the gun.
+signal ammoInsufficient ## Emitted when attempt to fire the gun while [member ammo] is < 1
 #endregion
 
 
 #region Dependencies
+@onready var damageComponent: DamageComponent = coComponents.DamageComponent # TBD: Include subclasses?
 @onready var inputComponent:  InputComponent  = parentEntity.findFirstComponentSubclass(InputComponent)
-@onready var damageComponent: DamageComponent = coComponents.DamageComponent
-@onready var damageRepeatingComponent: DamageRepeatingComponent = coComponents.DamageRepeatingComponent
 
 func getRequiredComponents() -> Array[Script]:
-	return [DamageComponent, DamageRepeatingComponent, InputComponent]
+	return [DamageComponent, InputComponent]
 #endregion
 
 
@@ -74,20 +78,24 @@ func _ready() -> void:
 
 	# Apply setters because Godot doesn't on _ready()
 
-	damageComponent.isEnabled = self.isFiring
+	# UNUSED: damageComponent.isEnabled = self.isFiring # Don't disable Area2D events
+	damageComponent.shouldDamageOnCollision = false # Damage only when isFiring
 
-	if isFiring: damageRepeatingComponent.timer.start()
-	else: damageRepeatingComponent.timer.stop()
+	if isFiring and cooldownTimer.is_stopped():
+		cooldownTimer.start()
 
 	self.set_physics_process(isEnabled and is_instance_valid(targetingNode))
 
-	Tools.connectSignal(inputComponent.didProcessInput,   self.onInputComponent_didProcessInput)
-	Tools.connectSignal(damageRepeatingComponent.didTick, self.onDamageRepeatingComponent_didTick)
+	Tools.connectSignal(inputComponent.didProcessInput, self.onInputComponent_didProcessInput)
+	Tools.connectSignal(damageComponent.didCollideReceiver, self.onDamageComponent_didCollideReceiver)
 
+
+#region Input
 
 func onInputComponent_didProcessInput(event: InputEvent) -> void:
 	if event.is_action(GlobalInput.Actions.fire):
 		self.isFiring = isEnabled and event.is_action_pressed(GlobalInput.Actions.fire)
+		if self.isFiring and not self.isOnCooldown: fire() # Fire when clicking.
 
 
 func _physics_process(_delta: float) -> void:
@@ -96,10 +104,63 @@ func _physics_process(_delta: float) -> void:
 
 	if debugMode: targetingNode.modulate = Color.RED if isFiring else Color.GRAY
 
+#endregion
 
-func onDamageRepeatingComponent_didTick(_damageReceivingComponentsInContact: Array[DamageReceivingComponent]) -> void:
-	# The number of hurt boxes in contact does not matter
-	if not self.isFiring: return
 
-	if ammo and not is_zero_approx(ammoCostPerTick):
-		ammo.value -= ammoCostPerTick
+#region Boom Boom
+
+func onDamageComponent_didCollideReceiver(_damageReceivingComponent: DamageReceivingComponent) -> void:
+	if not self.isEnabled or not self.isFiring or isOnCooldown: return
+	fire()
+
+
+func fire() -> bool:
+	if isOnCooldown \
+	or (dontShootIfNoTargets and damageComponent.damageReceivingComponentsInContact.is_empty()):
+		return false
+
+	if useAmmo():
+		if debugMode: emitDebugBubble(str("HIT ", damageComponent.damageReceivingComponentsInContact.size()))
+		damageComponent.causeDamageToAllReceivers()
+		self.didFire.emit(damageComponent.damageReceivingComponentsInContact)
+		self.startCooldown()
+		return true
+	else:
+		if debugMode: emitDebugBubble("LOW AMMO", damageComponent.damageReceivingComponentsInContact.size())
+		ammoInsufficient.emit()
+		return false
+
+
+## Deducts the [member ammoCost] from the [member ammo] [Stat].
+## If no [member ammo] [Stat] Resource is specified, no ammo is needed and the result is always `true`.
+## Returns `false` if [member ammo] is specified but there is not enough ammo.
+func useAmmo() -> bool:
+	# If no ammo resource is specified, no ammo is needed!
+	if not self.ammoCost or not self.ammo: return true
+
+	# Do we have enough ammo?
+
+	if ammo.value < ammoCost:
+		if debugMode: printDebug("Not enough ammo")
+		ammoInsufficient.emit()
+		return false
+
+	ammo.value -= ammoCost
+
+	# Did we just deplete the ammo with this shot?
+
+	if ammo.previousValue > 0 and ammo.value <= 0:
+		if debugMode: printDebug("ammo depleted")
+		didDepleteAmmo.emit()
+		if not self.ammoDepletedMessage.is_empty(): TextBubble.create(self.ammoDepletedMessage)
+
+	return true
+
+
+func finishCooldown() -> void:
+	super.finishCooldown()
+	if not self.isEnabled or not self.isFiring: return
+	fire()
+	if cooldownTimer.one_shot: cooldownTimer.start() # Keep firing
+
+#endregion
