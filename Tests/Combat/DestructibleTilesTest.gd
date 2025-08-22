337d31715e299126e1747f6d0f65e7b39da9cdc4
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-08-23 01:53:15 +0500

    Update DestructibleTilesTest

--- Tests/Combat/DestructibleTilesTest.gd
+++ Tests/Combat/DestructibleTilesTest.gd
@@ -3,4 +3,9 @@ extends Start
 
 func _ready() -> void:
 	super._ready()
-	Tools.randomizeTileMapCells($TileMapLayerWithCellData, Vector2i(2, 2), Vector2i(20, 20), Vector2i(42, 14), Vector2i(47, 15), 0.2, false) # not skipEmptyCells
+	Tools.randomizeTileMapCells($TileMapLayerWithCellData,
+		Tools.findRandomTileMapCells($TileMapLayerWithCellData,
+			0.1, true, true,
+			Vector2i(2, 2), Vector2i(20, 20)),
+	Vector2i(42, 14),
+	Vector2i(47, 15))
