d9fd5cd1f2f9bb2e2d56a4be239c6227281c0c35
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2026-01-31 09:32:37 +0700

    Overhaul TileMovementTest

--- Tests/TileMovementTest.gd
+++ Tests/TileMovementTest.gd
@@ -4,28 +4,33 @@ extends Start
 
 
 @onready var mapA: TileMapLayer = %TileMapA
-@onready var mapB: TileMapLayer = $TileMapB
-@onready var mapC: TileMapLayer = $TileMapC
+@onready var mapB: TileMapLayer = %TileMapB
+@onready var mapC: TileMapLayer = %TileMapC
 
-@onready var player: PlayerEntity = $"Player-TileBased"
-@onready var tileBasedPositionComponent: TileBasedPositionComponent = $"Player-TileBased/TileBasedPositionComponent"
+@onready var player: PlayerEntity = %"Player-TileBased"
+@onready var tileBasedPositionComponent: TileBasedPositionComponent = %"Player-TileBased/TileBasedPositionComponent"
 
 
 func onButtonACell_pressed() -> void:
 	tileBasedPositionComponent.setMapAndKeepCoordinates(mapA)
-
+	updateParameters()
 
 func onButtonBCell_pressed() -> void:
 	tileBasedPositionComponent.setMapAndKeepCoordinates(mapB)
-
+	updateParameters()
 
 func onButtonBPixel_pressed() -> void:
 	tileBasedPositionComponent.setMapAndKeepPosition(mapB)
-
+	updateParameters()
 
 func onButtonCCell_pressed() -> void:
 	tileBasedPositionComponent.setMapAndKeepCoordinates(mapC)
-
+	updateParameters()
 
 func onButtonCPixel_pressed() -> void:
 	tileBasedPositionComponent.setMapAndKeepPosition(mapC)
+	updateParameters()
+
+
+func updateParameters() -> void:
+	tileBasedPositionComponent.shouldSnapPositionEveryFrame = tileBasedPositionComponent.tileMap == mapC
