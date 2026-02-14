0adf2b410d8948a8923ed9a27825a08141890d4a
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2026-02-14 13:28:22 +0700

    Improve OverheadPhysicsComponent input flow and deceleration behavior
    ! Allow operation without InputComponent; reset movement to 0
    ! Apply non-frictional decleration only when input is non-zero
    * Fix movementDirection zero check
    * Fix OverheadMovementParameters steps
    * Fix debugInfo()
    * Update comments

--- Components/Physics/OverheadPhysicsComponent.gd
+++ Components/Physics/OverheadPhysicsComponent.gd
@@ -1,7 +1,7 @@
 ## Processes the friction and other physics for overhead-view (i.e. "top-down") movement for the parent [Entity]'s [CharacterBodyComponent].
 ## NOTE: Does NOT handle player input. Control is provided by [InputComponent] and/or AI agents etc.
 ## This component will still process friction even if no input source is present.
-## Requirements: BEFORE [CharacterBodyComponent] & [InputComponent]
+## Requirements: BEFORE [CharacterBodyComponent] & [InputComponent], AFTER components that manipulate [member InputComponent.movementDirection] etc.
 
 class_name OverheadPhysicsComponent
 extends CharacterBodyDependentComponentBase
@@ -50,26 +50,37 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	# DEBUG: printLog("_physics_process()")
 
-	self.movementDirection = inputComponent.movementDirection
+	# NOTE: Even if there is no InputComponent, we may still process friction
+	self.movementDirection = inputComponent.movementDirection if inputComponent else Vector2.ZERO
 	processMovement(delta)
 	characterBodyComponent.shouldMoveThisFrame = true
 
+	if debugMode: showDebugInfo() # NOTE: Show debug info BEFORE resetting the `movementDirection`!
+
 	# Clear the input from carrying over to the next frame
 	movementDirection = Vector2.ZERO # TBD: Should the "no input" state just be a `0` or some other flag?
-
-	if debugMode: showDebugInfo()
 
 
 ## Get the input direction and handle the movement/deceleration.
 func processMovement(delta: float) -> void:
 	if not isEnabled: return
 
+	# DESIGN: Crash if no [CharacterBody2] 
 	# Input is provided by [InputComponent] or an AI
 
-	if parameters.shouldApplyAcceleration:
-		body.velocity = body.velocity.move_toward(movementDirection * parameters.speed, parameters.acceleration * delta)
-	else:
-		body.velocity = movementDirection * parameters.speed
+	# Velocity
+
+	if not movementDirection.is_zero_approx(): # TBD: Should the velocity decay to 0 if there is no input, or should deceleration only happen during the friction pass?
+
+		if parameters.shouldApplyAcceleration:
+			body.velocity = body.velocity.move_toward(movementDirection * parameters.speed, parameters.acceleration * delta)
+		else:
+			body.velocity = movementDirection * parameters.speed
+
+	elif not parameters.shouldApplyFriction: 
+		# Stop abruptly if there is no input and no friction (gradual deceleration)
+		# TIP: To keep moving indefinitely, use `shouldMaintainMinimumVelocity`
+		body.velocity = Vector2.ZERO
 
 	# TODO: Compare setting vector components separately vs together
 
@@ -78,17 +89,18 @@ func processMovement(delta: float) -> void:
 	if parameters.shouldApplyFriction:
 
 		if is_zero_approx(movementDirection.x):
-			body.velocity.x = move_toward(body.velocity.x, 0.0, parameters.friction * delta)
+			body.velocity.x = move_toward(body.velocity.x, 0, parameters.friction * delta)
 
 		if is_zero_approx(movementDirection.y):
-			body.velocity.y = move_toward(body.velocity.y, 0.0, parameters.friction * delta)
+			body.velocity.y = move_toward(body.velocity.y, 0, parameters.friction * delta)
 
 	# Disable friction by maintaining velcoty from the previous frame?
 
-	if parameters.shouldMaintainPreviousVelocity and not movementDirection:
+	if parameters.shouldMaintainPreviousVelocity and movementDirection.is_zero_approx():
 		body.velocity = characterBodyComponent.previousVelocity
 
 	# Minimum velocity?
+	# TBD: Should there be a `lastNonZeroDirection` to recover from a complete stop if both `velocity` and `previousVelocity` are 0?
 
 	if parameters.shouldMaintainMinimumVelocity:
 		if body.velocity.length() < parameters.minimumSpeed:
--- Resources/Parameters/OverheadMovementParameters.gd
+++ Resources/Parameters/OverheadMovementParameters.gd
@@ -8,22 +8,23 @@ extends Resource
 
 @export_group("Movement")
 
-@export_range(0, 1000, 4) var speed:			float = 320
+@export_range(0, 1000, 4) var speed:		float = 320
 
-@export var shouldApplyAcceleration:			bool  = true
-@export_range(0, 2000, 4) var acceleration:		float = 800
+@export var shouldApplyAcceleration:		bool  = true
+@export_range(0, 2000, 4) var acceleration:	float = 800
 
 ## Completely disables slowdown from friction by reapplying the velocity from the previous frame.
 ## Use for scenarios like slippery surfaces such as ice.
-@export var shouldMaintainPreviousVelocity:		bool  = false
+@export var shouldMaintainPreviousVelocity:	bool  = false
 
-@export var shouldMaintainMinimumVelocity:		bool  = false
-@export_range(8, 1000, 48) var minimumSpeed:	float = 96
+@export var shouldMaintainMinimumVelocity:	bool  = false
+@export_range(8, 1000, 4) var minimumSpeed:	float = 96
 
 @export_group("Friction")
 
-## Slow the velocity down each frame.
-@export var shouldApplyFriction:				bool  = true
-@export_range(8, 2000, 48) var friction:		float = 1000
+## Gradually slows the velocity down each frame. If `false` then the movement stops abruptly when there is no input,
+## unless [member shouldMaintainPreviousVelocity] or [member shouldMaintainMinimumVelocity] is `true`.
+@export var shouldApplyFriction:			bool  = true
+@export_range(8, 2000, 4) var friction:		float = 1000
 
 #endregion
