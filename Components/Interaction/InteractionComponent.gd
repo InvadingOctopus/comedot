0260485f7758a48a66240534edbe17636a4ff0c1
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2026-03-10 02:12:10 +0700

    Overhaul InteractionControlComponent
    ! Inherit AreaContactComponent
    + Add allowNoPayload, controllersInContactCount, setIsEnabled()
    + Add shouldIncludeArea() hook
    ! Rework return result semantics: null if no payload, or true if allowNoPayload, didPerformInteraction always has raw result
    * Fix performAutomaticInteraction(): Use InteractionControlComponent.interact() ignoreRange
    * Fix indicator visibility when re-enabling during contact
    * Update comments & logging
    ! TODO: Update dependents: InteractionControlComponent, InteractionWithCooldownComponent, InteractionWithCostComponent etc.

--- Components/Interaction/InteractionComponent.gd
+++ Components/Interaction/InteractionComponent.gd
@@ -1,26 +1,21 @@
 ## Represents an area where an interaction may occur when the player inputs the interaction action.
 ## The initiator of an interaction is an [Entity]'s [InteractionControlComponent].
 ## For interactions that have a cooldown and a [Stat] cost on the object's side, use [InteractionWithCostComponent]
+## Uses [AreaContactComponent] as a parent class for collision logic and contact-tracking.
 
 class_name InteractionComponent
-extends Component
+extends AreaContactComponent
+
+# TODO: Add shouldSucceedIfNoPayload
+# TBD: Inherit scene from AreaContactComponent.tscn too?
+# CHECK: PERFORMANCE: Will too many inheritance levels impact performance?
 
 
 #region Parameters
 
-@export var isEnabled: bool = true:
-	set(newValue):
-		isEnabled = newValue
-		# AVOID: self.visible = isEnabled # Don't hide self in case some child visual effect nodes are present!
-		if interactionIndicator: interactionIndicator.visible = isEnabled
-		if  selfAsArea:
-			# NOTE: Cannot set flags directly because Godot error: "Function blocked during in/out signal"
-			selfAsArea.set_deferred(&"monitoring",  isEnabled)
-			selfAsArea.set_deferred(&"monitorable", isEnabled)
-
 ## The effect of the interaction, where this [InteractionComponent] is passed as the `source` for [method Payload.execute], and the [InteractionControlComponent]'s parent [Entity] is the `target`.
-## DESIGN: Interactions may succeed even if there is no payload; this allows special [InteractionControlComponent] subclasses to perform specific effects without a payload.
 ## See [Payload] for explanation and available options.
+## TIP: Interactions may succeed if [member allowNoPayload] even if there is no Payload; for example [TextInteractionComponent] performs its effects by itself.
 @export var payload: Payload
 
 ## If `true` then [InteractionControlComponent]s will not enter a cooldown when they interact with this object.
@@ -30,13 +25,19 @@ extends Component
 ## Initiate an interaction automatically as soon as any [InteractionControlComponent] comes in contact.
 ## Calls [method InteractionControlComponent.interact] on an [Area2D] collision event.
 ## Example: Portals or traps etc.
-## NOTE: Does not repeat interaction after the cooldown resets.
-@export var isAutomatic: bool = false
+## ALERT: [InteractionControlComponent] may skip if on cooldown; Does not repeat interaction attempt after the cooldown resets.
+@export var isAutomatic:	bool = false
+
+## Allows [method performInteraction] to return `true` if [member payload] is missing.
+## This allows components like [TextInteractionComponent] be their own payload.
+@export var allowNoPayload:	bool
 
 
 @export_group("UI")
 
-@export var interactionIndicator: CanvasItem ## A [Node2D] or [Control] to display when this [InteractionComponent] is in collisioncontact with an [InteractionControlComponent].
+## A [Node2D] or [Control] such as [Label] to display when this [InteractionComponent] is in collision contact with an [InteractionControlComponent].
+# ALERT: If multiple InteractionComponents use the same indicator, the most recent component to run [method updateIndicator] will modify this label.
+@export var interactionIndicator: CanvasItem
 
 @export var shouldAlwaysShowIndicator:  bool ## Always show the indicator even when there is no [InteractionControlComponent] in collision.
 
@@ -62,10 +63,9 @@ extends Component
 
 
 #region State
-var selfAsArea: Area2D:
-	get:
-		if not selfAsArea: selfAsArea = self.get_node(^".") as Area2D
-		return selfAsArea
+## The number of InteractionControlComponent in collision contact.
+var controllersInContactCount: int:
+	get: return areasInContact.size()
 #endregion
 
 
@@ -74,61 +74,85 @@ signal didEnterInteractionArea	(entity: Entity, interactionControlComponent: Int
 signal didExitInteractionArea	(entity: Entity, interactionControlComponent: InteractionControlComponent)
 signal didDenyInteraction		(interactorEntity: Entity)
 signal willPerformInteraction	(interactorEntity: Entity)
-signal didPerformInteraction	(result: Variant)
+signal didPerformInteraction	(result: Variant) ## Contains the result of the [Payload] or `null` if no Payload.
+#endregion
+
+
+#region Property Get/Set
+func setIsEnabled(newValue: bool) -> void:
+	# TBD: Toggle `Area2D.monitoring`?
+	super.setIsEnabled(newValue)
+	updateIndicator()
 #endregion
 
 
 func _ready() -> void:
+	self.shouldMonitorBodies = false # TBD: Just let it be customizable from the scene?
+
+	super._ready() # Prep the AreaContactComponent stuff
+
 	# Set the initial state of the indicator
+	# NOTE: Update content even if not `isEnabled` and hidden, just in case
 	if interactionIndicator:
 		if interactionIndicator is Control: interactionIndicator.tooltip_text = self.description
-		interactionIndicator.visible = shouldAlwaysShowIndicator # Start invisible if false
+		interactionIndicator.visible = isEnabled and (shouldAlwaysShowIndicator or controllersInContactCount > 0) # Start invisible if false
 
 		if interactionIndicator is Label:
 			# NOTE: If our `text` property is empty, save any existing text as the default, so we can restore it after any temporary modifications such as by [InteractionWithCooldownComponent] etc.
 			if self.text.is_empty(): self.text = interactionIndicator.text
 			else: updateIndicator() # Otherwise set the UI to our string
 
-	if  selfAsArea: # Apply setter because Godot doesn't on initialization
-		selfAsArea.monitoring  = isEnabled
-		selfAsArea.monitorable = isEnabled
 
-
+## IMPORTANT: Subclasses that override this method to add extra functionality MUST also update the visibility or call super
 func updateIndicator() -> void:
-	## If the [interactionIndicator] is a [Label], display our [member text] parameter.
-	## NOTE: Do not check text.is_empty() so we can allow the UI to be cleared.
+	# TBD: Check if any InteractionControlComponent is in contact before showing the indicator? but get_overlapping_areas() may be too expensive..
+	if not interactionIndicator: return
+
+	interactionIndicator.visible = isEnabled and (shouldAlwaysShowIndicator or controllersInContactCount > 0)
+
+	## If the [interactionIndicator] is a [Label], display our [member text]
+	## NOTE: Do not check text.is_empty() so an empty string may be used to clear the UI
+	## IMPORTANT: Update the text whether it's visible or not, in case it's needed elsewhere
 	if  interactionIndicator is Label:
 		interactionIndicator.text = self.text
 
 
 #region Area Collision Events
 
-func onArea_entered(area: Area2D) -> void:
-	if not isEnabled: return
-	var interactionControlComponent: InteractionControlComponent = area.get_node(^".") as InteractionControlComponent # HACK: Find better way to cast self?
+## Checks if an [Area2D] is an [InteractionControlComponent]
+## Subclasses may override this function to specify different conditions.
+## ALERT: PERFORMANCE: The default implementation does NOT check [member shouldMonitorAreas] or [isEnabled] or duplicate areas already in [areasInContact]
+func shouldIncludeArea(areaToCheck: Area2D) -> bool:
+	# NOTE: Don't check isEnabled so we can still allow exits
+	return  is_instance_of(areaToCheck, InteractionControlComponent) \
+			and not (areaToCheck == parentEntity or parentEntity.is_ancestor_of(areaToCheck)) \
+			and (groupToInclude.is_empty() or areaToCheck.is_in_group(groupToInclude))
+
+
+func shouldIncludeBody(_bodyToCheck: Node2D) -> bool:
+	return false # We don't deal in [PhysicsBody2D] or [TileMapLayer] collisions
+
+
+## Handles collisions with [InteractionControlComponent]
+func onCollide(collidingNode: Node2D) -> void:
+	var interactionControlComponent: InteractionControlComponent = collidingNode.get_node(^".") as InteractionControlComponent # HACK: Find better way to cast self?
 	if not interactionControlComponent: return
-
-	if debugMode: printDebug(str("onArea_entered(): ", area, ", interactionControlComponent: ", interactionControlComponent.logNameWithEntity if interactionControlComponent else "null", ", isAutomatic: ", isAutomatic))
-
-	# Display the indicators and labels, if any.
-	if interactionIndicator:
-		updateIndicator()
-		interactionIndicator.visible = true
-
+	if debugMode: printDebug(str("onAreaEntered(): ", collidingNode, ", interactionControlComponent: ", interactionControlComponent.logNameWithEntity if interactionControlComponent else "null", ", isAutomatic: ", isAutomatic))
+	
+	if self.interactionIndicator: updateIndicator() # Display the indicators and labels, if any
 	didEnterInteractionArea.emit(interactionControlComponent.parentEntity, interactionControlComponent)
+	if self.isAutomatic: performAutomaticInteraction(interactionControlComponent) # Separate method so subclasses may override it
 
-	if self.isAutomatic: performAutomaticInteraction(interactionControlComponent) # A separate method so subclasses may override it.
 
-
-func onArea_exited(area: Area2D) -> void:
+## Handles collisions with [InteractionControlComponent]
+func onExit(exitingNode: Node2D) -> void:
 	# NOTE: Exits should not check isEnabled to ensure cleanups are always performed.
-	var interactionControlComponent: InteractionControlComponent = area.get_node(^".") as InteractionControlComponent # HACK: Find better way to cast self?
+
+	var interactionControlComponent: InteractionControlComponent = exitingNode.get_node(^".") as InteractionControlComponent # HACK: Find better way to cast self?
 	if not interactionControlComponent: return
+	if debugMode: printDebug(str("onAreaExited(): ", exitingNode, ", interactionControlComponent: ", interactionControlComponent.logNameWithEntity if interactionControlComponent else "null"))
 
-	# Hide the indicators and labels.
-	if interactionIndicator and not shouldAlwaysShowIndicator:
-		interactionIndicator.visible = false
-
+	if self.interactionIndicator: updateIndicator() # Hide the indicators and labels.
 	didExitInteractionArea.emit(interactionControlComponent.parentEntity, interactionControlComponent)
 
 #endregion
@@ -152,26 +176,40 @@ func requestToInteract(interactorEntity: Entity, interactionControlComponent: In
 		return false
 
 
-## Executes the [member payload], passing this [InteractionComponent] as the `source` of the [Payload], and the [param interactorEntity] as the `target`.
+## Executes the [member payload], passing this [InteractionComponent] as the `source` parameter of the [Payload], and the [param interactorEntity] as the `target`.
 ## May be overridden by a subclass to perform custom actions.
-## Returns: The result of [method Payload.execute] or `false` if the [member payload] is missing.
+## NOTE: The return value of this method may be different than the "raw" result of the Payload included in [signal didPerformInteraction]
+## Returns: The result of [method Payload.execute] or `null` if the [member payload] is missing, or `true` if no Payload but [member allowNoPayload] is enabled.
 func performInteraction(interactorEntity: Entity, interactionControlComponent: InteractionControlComponent) -> Variant:
-	if debugMode: printDebug(str("performInteraction() interactorEntity: ", interactorEntity, "interactionControlComponent: ", interactionControlComponent, ", isEnabled: ", isEnabled))
+	if debugMode: printDebug(str("performInteraction() interactorEntity: ", interactorEntity, "interactionControlComponent: ", interactionControlComponent, ", payload: ", (payload.logName if payload else "null"), ", isEnabled: ", isEnabled, ", allowNoPayload: ", allowNoPayload))
 	if not isEnabled: return false
 
-	self.willPerformInteraction.emit(interactorEntity)
-	var result: Variant = payload.execute(self, interactorEntity) if payload else false
-	self.didPerformInteraction.emit(result)
+	# DESIGN: The value in the signal and this function's return value are not the same thing;
+	# the signal contains the "raw" result of the Payload;
+	# but this function may be the result, `true` if missing & `allowNoPayload`, or `null`
+	var result: Variant = null
 
-	return result
+	if payload or allowNoPayload: # TBD: Emit signals even if no `payload` & no `allowNoPayload`?
+		self.willPerformInteraction.emit(interactorEntity)
+		result = payload.execute(self, interactorEntity) if payload else null # NOTE: Keep `null` and NOT `true`; the `true` is for the function result if `allowNoPayload`
+		self.didPerformInteraction.emit(result) # TBD: Make it the same as the function result, i.e. `true` if missing & `allowNoPayload`?
+
+	# DESIGN: Return `true` if missing & allowNoPayload, to let components like [TextInteractionComponent] be their own payload.
+	if   payload:		 return result
+	elif allowNoPayload: return true
+	else:				 return null # TBD: Return `false` if missing? `null` makes more sense; non-existence or "not-even-wrong"
 
 
 ## Called by [method onArea_entered] if [member isAutomatic].
 ## Implemented as a separate method so that subclasses mat override it.
 ## NOTE: Does NOT check [member isAutomatic]; must be checked by caller.
-## HEADSUP: Remember to set [member previousInteractor] = [member interactionControlComponent] if using [method startCooldown].
+## NOTE: HEADSUP: Remember to set [member previousInteractor] = [member interactionControlComponent] if using [method startCooldown].
 func performAutomaticInteraction(interactionControlComponent: InteractionControlComponent) -> void:
-	interactionControlComponent.interact(self) # Interact only with me senpai!
+	# TODO: Handle InteractionControlComponent cooldown
+	# NOTE: If InteractionComponent.onArea_entered() runs before InteractionControlComponent's collision events,
+	# then the InteractionControlComponent will not have this component in `interactionsInRange` yet
+	# so we must set `ignoreRange` when calling interact()
+	interactionControlComponent.interact(self, true) # ignoreRange # Interact only with me senpai!
 
 #endregion
 
