33c00f1bd62419fb05eda2db54db0337b65e5546
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2026-03-14 02:25:58 +0700

    Fix & update InteractionWithCostComponent
    * Adopt CooldownTimer
    * Fix & improve cooldown logic
    * Respect canSkipCurrentCooldown
    * Fix & improve Stat cost payment
    * Treat missing/invalid cost as free & skip zero-cost payment
    * Implement refunds: Revert the actual change in Stat value

--- Components/Interaction/InteractionWithCostComponent.gd
+++ Components/Interaction/InteractionWithCostComponent.gd
@@ -9,7 +9,7 @@ extends InteractionWithCooldownComponent
 
 
 #region Parameters
-@export var cost: StatCost ## The [Stat] cost.
+@export var cost: StatCost ## The [Stat] cost. If missing or invalid, then the interaction is considered free to use.
 #endregion
 
 
@@ -19,12 +19,13 @@ extends InteractionWithCooldownComponent
 ## When the player presses the Interact button, the [InteractionControlComponent] checks its conditions then calls this method on the [InteractionComponent](s) in range.
 ## Then this [InteractionWithCostComponent] checks its own conditions (such as whether the player has key to open a door, or the energy to chop a tree).
 func requestToInteract(interactorEntity: Entity, interactionControlComponent: InteractionControlComponent) -> bool:
-	if not isEnabled or not is_zero_approx(cooldownTimer.time_left): return false
+	if  not isEnabled \
+	or (not canSkipCurrentCooldown and isOnCooldown): return false # Refuse if already on cooldown, unless we can skip once
 
 	var statsComponent:	 StatsComponent = interactorEntity.getComponent(StatsComponent) as StatsComponent
 	var isStatsComponentValidated: bool
 
-	if self.cost:
+	if self.cost and cost.hasCost: # Make sure `StatDependentResourceBase.costStat` is valid etc.
 		isStatsComponentValidated = cost.validateStatsComponent(statsComponent) if statsComponent else false
 		if not isStatsComponentValidated:
 			var bubble: GameplayResourceBubble = GameplayResourceBubble.create(self.cost.costStat, " LOW", interactorEntity)
@@ -50,44 +51,51 @@ func requestToInteract(interactorEntity: Entity, interactionControlComponent: In
 ## Returns: The result of [method Payload.execute] or `false` if the [member payload] is missing.
 func performInteraction(interactorEntity: Entity, interactionControlComponent: InteractionControlComponent) -> Variant:
 	if debugMode: printDebug(str("performInteraction() interactorEntity: ", interactorEntity, "interactionControlComponent: ", interactionControlComponent, ", isEnabled: ", isEnabled, ", cost: ", cost, ", cooldown: ", cooldownTimer.time_left))
-	if not isEnabled or not is_zero_approx(cooldownTimer.time_left): return false
+	if  not isEnabled \
+	or (not canSkipCurrentCooldown and isOnCooldown): return false # Refuse if already on cooldown, unless we can skip once
 
 	# NOTE: DESIGN: AWESOME: Sometimes there may be interactions that fail, such as a [StatModifierPayload] skipping a [Stat] that is already maxed out.
 	# But we cannot know the `result` of a Payload before it executes.
 	# In that case, the Stat cost must be refunded.
 
-	var statsComponent: StatsComponent = interactorEntity.getComponent(StatsComponent) as StatsComponent
-	var didPayCost:  bool
-	var paymentStat: Stat
+	var statsComponent:	StatsComponent = interactorEntity.getComponent(StatsComponent) as StatsComponent
+	var paymentStat:	Stat
+	var didFulfillCost:	bool
+	var statChange:		int
 
-	if self.cost:
-		paymentStat = cost.getPaymentStatFromStatsComponent(statsComponent)
+	if self.cost and self.cost.hasCost: # Make sure `StatDependentResourceBase.costStat` is valid etc.
+		paymentStat = cost.getPaymentStatFromStatsComponent(statsComponent) if statsComponent else null
 		if not paymentStat:
 			if debugMode: printDebug(str("performInteraction() cant find ", cost.costStatName, " in ", statsComponent))
 			return false
 
-		# NOTE: DESIGN: We have to deduct the [Stat] before executing the Payload, in case the Payload depends on the actual new value of the [Stat],
+		# DESIGN: We have to deduct the [Stat] before executing the Payload, in case the Payload depends on the actual new value of the [Stat],
 		# EXAMPLE: A card that costs mana to play but adds extra effects if the mana becomes 0.
 		# so we CANNOT just "proxy" the cost and delay mutating the [Stat] until only when the Payload's result is a success :')
 		# TODO: Find a way to suppress superfluous [StatsVisualComponent] animations etc. in case there is a refund later.
 		# TRIED: WITHOUT "skipping signals" as a previous implementation tried; it was too confusing and inelegant.
-		didPayCost = cost.deductCostFromStat(paymentStat) if statsComponent else false
-	else: # If there is no cost, any offer is valid!
-		didPayCost = true
 
-	if didPayCost:
-		var paidCost:	int = cost.cost
+		# First, check if we can pay the cost
+		if cost.validateOfferedStat(paymentStat):
+			statChange = cost.deductCostFromStat(paymentStat) # ALERT: Clamped `Stat.min` may prevent a full deduction, in case of buffs or "god mode" etc.
+			didFulfillCost = true # Assume a success even if statChange == 0 (in case of clamping or whatever)
+
+	else: # If there is no cost, any offer is valid!
+		didFulfillCost = true
+
+	if didFulfillCost:
 		var result: Variant = super.performInteraction(interactorEntity, interactionControlComponent)
 
 		if  Tools.checkResult(result):
 			previousInteractor = interactionControlComponent # TBD: Update only on successful interaction or always?
-			startCooldown()
+			if not isOnCooldown: self.startCooldown()
 
 		elif paymentStat: # Refund the cost if the interaction failed
-			if debugMode: printDebug(str("Payload: ", payload, ", result: ", result, " failed; refunding ", paidCost, " → ", paymentStat.logName))
-			# TODO: Suppress superfluous [StatsVisualComponent] animations etc. in case of a refund
-			paymentStat.value += paidCost
-			if shouldCooldownOnFailure: startCooldown(cooldownOnFailure)
+			# NOTE: REVERSE the stat's previous change i.e. turn a negative deduction to a positive number!
+			if debugMode: printDebug(str("Payload: ", payload, ", result: ", result, " failed; refunding ", -statChange, " → ", paymentStat.logName))
+			# TODO: Suppress superfluous [StatsVisualComponent] animations etc. during a refund, e.g. a "-10" and then a "+10" bubble
+			paymentStat.value += -statChange
+			if not isOnCooldown and shouldCooldownOnFailure: self.startCooldown(cooldownOnFailure)
 
 		updateIndicator()
 		return result
