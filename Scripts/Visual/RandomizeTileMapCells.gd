63472308526b4eb157b93ab8bb1c9a72dddc713f
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2026-04-24 19:16:00 +0700

    Add RandomizeTileMapCells
    * Replace shouldUseEntireMap with shouldUseRegion

--- Scripts/Visual/RandomizeTileMapCells.gd
+++ Scripts/Visual/RandomizeTileMapCells.gd
@@ -7,24 +7,25 @@ extends TileMapLayer
 
 #region Parameters
 
-## Overrides [member cellRegionStart] & [member cellRegionEnd].
-## WARNING: If there are NO "painted" cells in the map, then the size of the map will be (0,0) which means NO cells will be modified!
-## TIP: To quickly set the "size" of a [TileMapLayer], just place 1 transparent tile on the bottom-right cell.
-@export var shouldUseEntireMap:		 bool = false
+@export_range(0, 1.0, 0.01) var modificationChance:	float = 1.0 ## THe chance to modify rolled separately for each cell in the [member cellRegionStart] to [member cellRegionEnd] rectangle.
 
 @export var shouldIncludeUsedCells:  bool = true ## Modify already "painted" cells?
-@export var shouldIncludeEmptyCells: bool = true ## Leave "unpainted" cells empty?
+@export var shouldIncludeEmptyCells: bool = true ## If `false`, leave "unpainted" cells empty.
 
+@export_group("TileMap Region")
+
+## If `false` then [param cellRegionStart] & [param cellRegionEnd] are ignored, and the entire grid containing all the "painted" cells of the TileMap is used.
+## WARNING: If there are NO "painted" cells in the map, then the size of the map will be (0,0) which means NO cells will be modified!
+## TIP: To quickly set the "size" of a [TileMapLayer], just place 1 transparent tile on the bottom-right cell.
+@export_custom(PROPERTY_HINT_GROUP_ENABLE, "") var shouldUseRegion:	bool
 @export var cellRegionStart:		Vector2i ## The upper-left corner of the rectangle region in the [TileMapLayer] MAP to modify the CELLS in.
 @export var cellRegionEnd:			Vector2i ## The bottom-right corner of the rectangle region in the [TileMapLayer] MAP to modify the CELLS in.
 
+@export_group("TileSet Atlas")
+
 @export var atlasCoordinatesMin:	Vector2i ## The upper-left corner of the rectangle region in the [TileSet] atlas to choose the TILES from.
 @export var atlasCoordinatesMax:	Vector2i ## The bottom-right corner of the rectangle region in the [TileSet] atlas to choose the TILES from.
-
 @export var atlasSourceID:			int = 0  ## A [TileSetSource] identifier. See [method TileSet.set_source_id]
-
-
-@export_range(0, 1.0, 0.01) var modificationChance:	float = 1.0 ## THe chance to modify rolled separately for each cell in the [member cellRegionStart] to [member cellRegionEnd] rectangle.
 
 #endregion
 
@@ -47,7 +48,7 @@ func repaintCells() -> Array[Vector2i]:
 		Debug.printWarning("modificationChance <= 0", self)
 		return []
 
-	if cellRegionEnd < cellRegionStart:
+	if shouldUseRegion and cellRegionEnd < cellRegionStart:
 		Debug.printWarning("cellRegionEnd < cellRegionStart", self)
 		return []
 
@@ -55,17 +56,10 @@ func repaintCells() -> Array[Vector2i]:
 		Debug.printWarning("atlasCoordinatesMax < atlasCoordinatesMin", self)
 		return []
 
-	if shouldUseEntireMap:
-		var mapArea: Rect2i = self.get_used_rect()
-		if mapArea.has_area():
-			Debug.printDebug(str("RandomizeTileMapCells.gd: shouldUseEntireMap: Map area: ", mapArea), self)
-			cellRegionStart = mapArea.position
-			cellRegionEnd   = mapArea.grow(-1).end # Make `end` 0-based
-		else:
-			Debug.printWarning(str("RandomizeTileMapCells.gd: shouldUseEntireMap: Map has cells = no area!"), self)
-
 	recentlyModifiedCells = TileMapTools.findRandomTileMapCells(self,
 		modificationChance, shouldIncludeUsedCells, shouldIncludeEmptyCells,
 		shouldUseRegion, cellRegionStart, cellRegionEnd)
+
 	TileMapTools.randomizeTileMapCells(self, recentlyModifiedCells, atlasCoordinatesMin, atlasCoordinatesMax, atlasSourceID)
+
 	return recentlyModifiedCells
