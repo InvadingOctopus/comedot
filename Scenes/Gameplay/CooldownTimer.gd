7423fdd3f7bcf317be49cf51417ea49fd56f90bc
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2026-08-29 21:27:53 +0500

    Fix & improve CooldownTimer
    ! Fix & restore startCooldown() wait_time override
    * Rename finishCooldown() → onTimeout()
    * Return time_left & errors from startCooldown()
    * Reject time < 0.05
    * Fix cancellation, skips & signals
    + Add cancelCooldown()

--- Scenes/Gameplay/CooldownTimer.gd
+++ Scenes/Gameplay/CooldownTimer.gd
@@ -1,7 +1,7 @@
 ## Adds common "cooldown" functionality to a [Timer] for any actions a player may not perform too quickly, such as firing a gun or mining resources.
 ## Used by [CooldownComponent], [InteractionWithCooldownComponent] etc.
 ## IMPORTANT: ALWAYS use [method startCooldown]: Do NOT use the standard [method Timer.start] because the default Godot [Timer] is not aware of [member cooldownMilliseconds] or [member minimumCooldown] etc.
-## IMPORTANT: ALWAYS check [member cooldownSeconds] instead of [member Timer.wait_time] because it resolves the [member cooldownMilliseconds] [member Stat.value]
+## WARNING: Calling [method Timer.stop] will NOT emit [signal Timer.timeout] or [signal CooldownTimer.didFinishCooldown] — Call [method cancelCooldown] to manually cancel a cooldown.
 
 class_name CooldownTimer
 extends Timer
@@ -22,7 +22,13 @@ const godotMinimumTime: float = 0.05
 
 ## The minimum value that will be enforced for [method Timer.start] during [method startCooldown]
 ## Supersedes [member cooldownMilliseconds] if that value is lower than this minimum.
-@export_range(godotMinimumTime, 120, 0.05, "suffix:seconds", "or_greater") var minimumCooldown: float = 0.05
+## NOTE: [method startCooldown] rejects time values < [constant godotMinimumTime] 0.05
+@export_range(godotMinimumTime, 120, 0.05, "suffix:seconds", "or_greater") var minimumCooldown: float = 0.05:
+	set(newValue):
+		newValue = maxf(godotMinimumTime, newValue)
+		if newValue != minimumCooldown:
+			minimumCooldown = newValue
+
 
 @export var debugMode: bool
 
@@ -45,7 +51,7 @@ var isOnCooldown: bool:
 ## IMPORTANT: This flag is always reset in [method startCooldown], so it must be set AFTER starting a cooldown.
 @export_storage var shouldSkipNextCooldown:		 bool
 
-## Enabled by [method startCooldown] to make sure repeated calls to [method finishCooldown] don't emit redundant [signal didFinishCooldown] signals.
+## Enabled by [method startCooldown] to make sure repeated calls to [method onTimeout] don't emit redundant [signal didFinishCooldown] signals.
 @export_storage var shouldEmitDidFinishCooldown: bool
 
 #endregion
@@ -53,13 +59,14 @@ var isOnCooldown: bool:
 
 #region Signals
 signal didStartCooldown(time: float) ## ALERT: NOT emitted if [method Timer.start] is called manually!
-signal didFinishCooldown
+signal didFinishCooldown ## ALERT: NOT emitted if [method Timer.stop] or [method cancelCooldown] is called! TIP: Use [method onTimeout] to stop AND emit.
 #endregion
 
 
 func _ready() -> void:
 	# Also connect the signal via code to make it more convenient for subclasses which don't inherit the .tscn scene, such as GunComponent
-	Tools.connectSignal(self.timeout, self.finishCooldown)
+	if not self.timeout.is_connected(self.onTimeout):
+		self.timeout.connect(self.onTimeout, CONNECT_PERSIST)
 
 
 #region Cooldown
@@ -67,47 +74,68 @@ func _ready() -> void:
 ## Starts the cooldown delay, using [member cooldownSeconds] as the default for [param overrideTime] and emits [signal didStartCooldown].
 ## If this [Timer] is already running and [member Timer.paused], this method unpauses the [Timer].
 ## If [param restartIfOnCooldown] then an ongoing OR pause cooldown is restarted.
+## Returns the [member Timer.time_left] if a cooldown was started or already running.
+## Returns 0 if [member shouldSkipNextCooldown] or if [member cooldownSeconds] or [param overrideTime] is < [constant godotMinimumTime] 0.05 then this call is ignored.
+## Returns -1 if the state is invalid.
 ## TIP: Check [member isOnCooldown] before calling this method to avoid redundant processing or restarts.
 ## IMPORTANT: Do NOT use the standard [method Timer.start] because the default Godot [Timer] is of course unware of [member cooldownMilliseconds]
-func startCooldown(overrideTime: float = self.cooldownSeconds, restartIfOnCooldown: bool = false) -> void:
+func startCooldown(overrideTime: float = self.cooldownSeconds, restartIfOnCooldown: bool = false) -> float:
 	# TBD: PERFORMANCE: Do we really need all this crap just for a simple Timer.start()?
 	# Or could the `didStartCooldown` signal be helpful in chaining with other components e.g. for animations etc.?
 
-	if debugMode:
+	if overrideTime < godotMinimumTime: return 0
+
+	if  not self.is_node_ready() or not self.is_inside_tree():
+		Debug.printDebug("startCooldown(): Timer node not ready", self)
+		return -1 # TBD: Use different values?
+
+	if  debugMode:
 		Debug.printDebug(str("startCooldown() previous wait_time: ", self.wait_time, " → overrideTime: ", overrideTime, " + cooldownMilliseconds: ", (cooldownMilliseconds.logName if cooldownMilliseconds else "null"), ", minimumCooldown: ", minimumCooldown, ", shouldSkipNextCooldown: ", shouldSkipNextCooldown), self)
 
-	if  shouldSkipNextCooldown: 
-		shouldSkipNextCooldown = false
-		return
-
 	# Check for an ongoing cooldown to avoid unintended resets or redundant processing
-	if self.isOnCooldown:
+	if  self.isOnCooldown:
 		if debugMode: Debug.printDebug(str("CooldownTimer already isOnCooldown: ", self.time_left, " paused: ", self.paused), self)
 		if self.paused: self.paused = false # Unpause because running a Timer would be the behavior expected by the caller
-		if not restartIfOnCooldown: return
+		if not restartIfOnCooldown: return self.time_left
 
-	var clampedTime: float = maxf(minimumCooldown, overrideTime)
+	# IMPORTANT: Check for skip AFTER checking for a running cooldown,
+	# because "consuming" `shouldSkipNextCooldown` and then cancelling would prevent the ACTUAL next cooldown from being skipped.
+	if  shouldSkipNextCooldown:
+		shouldSkipNextCooldown = false
+		return 0 # TBD: Use different values?
 
-	# UNUSED: TBD: var previousTime: float = self.wait_time # Save the "actual" cooldown because Timer.start(overrideTime) modifies Timer.wait_time
+	var  clampedTime:  float = maxf(minimumCooldown, overrideTime)
+
+	var  previousTime: float = self.wait_time # Save the "actual" cooldown because Timer.start(overrideTime) modifies `Timer.wait_time`
 	self.wait_time = clampedTime
 	self.start()
 	self.shouldEmitDidFinishCooldown = true
-	# UNUSED: TBD: self.wait_time = previousTime # Restore the default cooldown?
+	self.wait_time = previousTime # Restore the default cooldown
+
+	shouldSkipNextCooldown = false # Don't skip the NEXT next cooldown! # NOTE: Set BEFORE `didStartCooldown` so handlers can change it if they want
 	didStartCooldown.emit(clampedTime)
+
 	# TBD: If the new time is too low, just run straight to the finish?
-	# UNUSED: finishCooldown()
-	
-	shouldSkipNextCooldown = false # Don't skip the NEXT next cooldown!
+	return self.time_left
 
 
-## Connected to [signal Timer.timeout] to be called when the cooldown [Timer] is over and emits [signal didFinishCooldown]
-## May be called manually to call [method Timer.stop] and emit the signal.
-func finishCooldown() -> void:
+## Stops this [Timer] and disables [member shouldEmitDidFinishCooldown]
+## NOTE: Does NOT emit [signal Timer.timeout] or [signal didFinishCooldown]
+func cancelCooldown() -> void:
+	if  debugMode: Debug.printDebug(str("cancelCooldown() time_left: ", time_left), self)
+	stop() # GODOT: Calling stop() does not emit the `timeout` signal
+	shouldEmitDidFinishCooldown = false
+
+
+## Connected to [signal Timer.timeout] to be called when the cooldown [Timer] is over.
+## Emits [signal didFinishCooldown] if [member shouldEmitDidFinishCooldown] is enabled.
+## NOTE: Does NOT emit [signal Timer.timeout] if called manually.
+func onTimeout() -> void:
 	# TBD: Check shouldSkipNextCooldown on finish?
-	if debugMode: Debug.printDebug(str("finishCooldown() shouldEmitDidFinishCooldown: ", shouldEmitDidFinishCooldown), self)
-	self.stop() # GODOT: Calling stop() does not emit the `timeout` signal
-	if  self.shouldEmitDidFinishCooldown:
-		self.shouldEmitDidFinishCooldown = false # IMPORTANT: Clear BEFORE emitting because handlers may immediately start a new cooldown
+	if  debugMode: Debug.printDebug(str("onTimeout() shouldEmitDidFinishCooldown: ", shouldEmitDidFinishCooldown, ", time_left: ", time_left), self)
+	stop() # GODOT: Calling stop() does not emit the `timeout` signal
+	if  shouldEmitDidFinishCooldown:
+		shouldEmitDidFinishCooldown = false # IMPORTANT: Clear BEFORE emitting because handlers may immediately start a new cooldown
 		didFinishCooldown.emit() # TBD: Emit only if we were actually on cooldown? But that won't work for the "natural" `Timer.timeout` signal which is emitted after the Timer stops
 
 #endregion
